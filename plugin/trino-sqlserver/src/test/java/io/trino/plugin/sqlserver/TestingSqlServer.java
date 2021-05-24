33ec2f9c37ceb31cd915853d85756bd91d04d6ee
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-05-24 09:20:28 +0200

    Retry SQL Server setup for all SQLException cases
    Example failure this is geared towards:
    
        [ERROR] init(io.trino.plugin.sqlserver.TestSqlServerWithSnapshotIsolation)  Time elapsed: 3.682 s  <<< FAILURE!
        java.lang.RuntimeException: Failed to execute statement: ALTER DATABASE database_dde669de2304413a89e83a012a553560 SET ALLOW_SNAPSHOT_ISOLATION ON
                at io.trino.plugin.sqlserver.TestingSqlServer.execute(TestingSqlServer.java:94)
                at io.trino.plugin.sqlserver.TestingSqlServer.lambda$setUpDatabase$4(TestingSqlServer.java:131)
                at net.jodah.failsafe.Functions.lambda$toSupplier$10(Functions.java:240)
                at net.jodah.failsafe.Functions.lambda$get$0(Functions.java:48)
                at net.jodah.failsafe.RetryPolicyExecutor.lambda$supply$0(RetryPolicyExecutor.java:62)
                at net.jodah.failsafe.Execution.executeSync(Execution.java:129)
                at net.jodah.failsafe.FailsafeExecutor.call(FailsafeExecutor.java:376)
                at net.jodah.failsafe.FailsafeExecutor.run(FailsafeExecutor.java:210)
                at io.trino.plugin.sqlserver.TestingSqlServer.setUpDatabase(TestingSqlServer.java:131)
                at io.trino.plugin.sqlserver.TestingSqlServer.start(TestingSqlServer.java:107)
                at io.trino.plugin.sqlserver.BaseSqlServerTransactionIsolationTest.createQueryRunner(BaseSqlServerTransactionIsolationTest.java:37)
                at io.trino.testing.AbstractTestQueryFramework.init(AbstractTestQueryFramework.java:90)
                at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
                at java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
                at java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
                at java.base/java.lang.reflect.Method.invoke(Method.java:566)
                at org.testng.internal.MethodInvocationHelper.invokeMethod(MethodInvocationHelper.java:104)
                at org.testng.internal.Invoker.invokeConfigurationMethod(Invoker.java:515)
                at org.testng.internal.Invoker.invokeConfigurations(Invoker.java:217)
                at org.testng.internal.Invoker.invokeConfigurations(Invoker.java:144)
                at org.testng.internal.TestMethodWorker.invokeBeforeClassMethods(TestMethodWorker.java:169)
                at org.testng.internal.TestMethodWorker.run(TestMethodWorker.java:108)
                at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1128)
                at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:628)
                at java.base/java.lang.Thread.run(Thread.java:829)
        Caused by: com.microsoft.sqlserver.jdbc.SQLServerException: ALTER DATABASE statement failed.
                at com.microsoft.sqlserver.jdbc.SQLServerException.makeFromDatabaseError(SQLServerException.java:262)
                at com.microsoft.sqlserver.jdbc.SQLServerStatement.getNextResult(SQLServerStatement.java:1632)
                at com.microsoft.sqlserver.jdbc.SQLServerStatement.doExecuteStatement(SQLServerStatement.java:872)
                at com.microsoft.sqlserver.jdbc.SQLServerStatement$StmtExecCmd.doExecute(SQLServerStatement.java:767)
                at com.microsoft.sqlserver.jdbc.TDSCommand.execute(IOBuffer.java:7418)
                at com.microsoft.sqlserver.jdbc.SQLServerConnection.executeCommand(SQLServerConnection.java:3272)
                at com.microsoft.sqlserver.jdbc.SQLServerStatement.executeCommand(SQLServerStatement.java:247)
                at com.microsoft.sqlserver.jdbc.SQLServerStatement.executeStatement(SQLServerStatement.java:222)
                at com.microsoft.sqlserver.jdbc.SQLServerStatement.execute(SQLServerStatement.java:743)
                at io.trino.plugin.sqlserver.TestingSqlServer.execute(TestingSqlServer.java:91)
                ... 24 more

--- plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestingSqlServer.java
+++ plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestingSqlServer.java
@@ -42,8 +42,7 @@ public final class TestingSqlServer
             .withBackoff(1, 5, ChronoUnit.SECONDS)
             .withMaxRetries(5)
             .handleIf(throwable -> getCausalChain(throwable).stream()
-                            .filter(SQLException.class::isInstance)
-                            .anyMatch(exception -> exception.getMessage().contains("Rerun the transaction.")))
+                    .anyMatch(SQLException.class::isInstance))
             .onRetry(event -> log.warn(
                     "Query failed on attempt %s, will retry. Exception: %s",
                     event.getAttemptCount(),
