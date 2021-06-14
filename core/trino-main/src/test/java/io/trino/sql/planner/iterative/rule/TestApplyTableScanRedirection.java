44559c1beeb2e683823b2ceff6645702cfa149d4
Piotr Findeisen <piotr.findeisen@gmail.com>
2021-06-14 22:28:40 +0200

    Encode unbounded varchar value without a CAST

--- core/trino-main/src/test/java/io/trino/sql/planner/iterative/rule/TestApplyTableScanRedirection.java
+++ core/trino-main/src/test/java/io/trino/sql/planner/iterative/rule/TestApplyTableScanRedirection.java
@@ -224,7 +224,7 @@ public class TestApplyTableScanRedirection
                     .withSession(MOCK_SESSION)
                     .matches(
                             filter(
-                                    "DEST_COL = CAST('foo' AS varchar)",
+                                    "DEST_COL = VARCHAR 'foo'",
                                     tableScan(
                                             equalTo(new MockConnectorTableHandle(DESTINATION_TABLE)),
                                             TupleDomain.all(),
@@ -244,7 +244,7 @@ public class TestApplyTableScanRedirection
                             project(
                                     ImmutableMap.of("expr", expression("DEST_COL_B")),
                                     filter(
-                                            "DEST_COL_A = CAST('foo' AS varchar)",
+                                            "DEST_COL_A = VARCHAR 'foo'",
                                             tableScan(
                                                     equalTo(new MockConnectorTableHandle(DESTINATION_TABLE)),
                                                     TupleDomain.all(),
