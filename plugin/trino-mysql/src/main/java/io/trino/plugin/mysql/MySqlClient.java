2d14b83ae725031f4e63ca4f65fed6f71a4d6a1a
Yuya Ebihara <yuya.ebihara@starburstdata.com>
2021-11-19 13:41:47 +0900

    Disable predicates pushdown to MySQL REAL type
    Previously, it returned incorrect result:
    
    CREAT TABLE test (c1 real);
    INSERT INTO test VALUES (real '3.14');
    SELECT * FROM test; -- returns 3.14
    SELECT * FROM test WHERE c1 = real '3.14'; -- no result
    
    (cherry picked from commit 75878e0dbe67daca9dfd84d2bb92ac6890d2f76c)

--- plugin/trino-mysql/src/main/java/io/trino/plugin/mysql/MySqlClient.java
+++ plugin/trino-mysql/src/main/java/io/trino/plugin/mysql/MySqlClient.java
@@ -99,7 +99,6 @@ import static io.trino.plugin.jdbc.StandardColumnMappings.doubleWriteFunction;
 import static io.trino.plugin.jdbc.StandardColumnMappings.integerColumnMapping;
 import static io.trino.plugin.jdbc.StandardColumnMappings.integerWriteFunction;
 import static io.trino.plugin.jdbc.StandardColumnMappings.longDecimalWriteFunction;
-import static io.trino.plugin.jdbc.StandardColumnMappings.realColumnMapping;
 import static io.trino.plugin.jdbc.StandardColumnMappings.realWriteFunction;
 import static io.trino.plugin.jdbc.StandardColumnMappings.shortDecimalWriteFunction;
 import static io.trino.plugin.jdbc.StandardColumnMappings.smallintColumnMapping;
@@ -124,6 +123,7 @@ import static io.trino.spi.type.TimestampType.TIMESTAMP_MILLIS;
 import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MILLIS;
 import static io.trino.spi.type.TinyintType.TINYINT;
 import static io.trino.spi.type.VarbinaryType.VARBINARY;
+import static java.lang.Float.floatToRawIntBits;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static java.lang.String.format;
@@ -275,7 +275,13 @@ public class MySqlClient
                 return Optional.of(bigintColumnMapping());
 
             case Types.REAL:
-                return Optional.of(realColumnMapping());
+                // Disable pushdown because floating-point values are approximate and not stored as exact values,
+                // attempts to treat them as exact in comparisons may lead to problems
+                return Optional.of(ColumnMapping.longMapping(
+                        REAL,
+                        (resultSet, columnIndex) -> floatToRawIntBits(resultSet.getFloat(columnIndex)),
+                        realWriteFunction(),
+                        DISABLE_PUSHDOWN));
 
             case Types.DOUBLE:
                 return Optional.of(doubleColumnMapping());
