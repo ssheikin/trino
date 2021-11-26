1ef95be071741ce5c382a92b5e39692b1fbfd27f
Raunaq Morarka <raunaqmorarka@gmail.com>
2021-11-26 14:34:15 +0530

    Fix ordering of reads in HdfsParquetDataSource#planRead
    If planRead encounters a large disk range followed by a small disk range
    for a column, then it will return ChunkReaders for those ranges in
    reversed order. This causes ParquetColumnChunk to read pages from
    the wrong slice and fail the query or give wrong results.
    
    (cherry picked from commit 5807657491b5f3d1cc9694e56963797a2c82f610)

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/TestParquetPageSkipping.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/TestParquetPageSkipping.java
@@ -37,7 +37,11 @@ public class TestParquetPageSkipping
             throws Exception
     {
         return HiveQueryRunner.builder()
-                .setHiveProperties(ImmutableMap.of("parquet.use-column-index", "true"))
+                .setHiveProperties(
+                        ImmutableMap.of(
+                                "parquet.use-column-index", "true",
+                                // Small max-buffer-size allows testing mix of small and large ranges in HdfsParquetDataSource#planRead
+                                "parquet.max-buffer-size", "400B"))
                 .build();
     }
 
