92673ba9ddacaade760c6e1aa8ca4a4ecbc44120
Yuya Ebihara <ebyhry@gmail.com>
2022-08-27 07:09:37 +0900

    Extract toColumnNameInSql method in BaseConnectorTest
    (cherry picked from commit f6e621da5e1d7ac33186b2950882a7432a47cfc6)

--- testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
+++ testing/trino-testing/src/main/java/io/trino/testing/BaseConnectorTest.java
@@ -2946,10 +2946,7 @@ public abstract class BaseConnectorTest
 
     protected void testColumnName(String columnName, boolean delimited)
     {
-        String nameInSql = columnName;
-        if (delimited) {
-            nameInSql = "\"" + columnName.replace("\"", "\"\"") + "\"";
-        }
+        String nameInSql = toColumnNameInSql(columnName, delimited);
         String tableName = "tcn_" + nameInSql.toLowerCase(ENGLISH).replaceAll("[^a-z0-9]", "") + randomTableSuffix();
 
         try {
@@ -2979,6 +2976,15 @@ public abstract class BaseConnectorTest
         finally {
             assertUpdate("DROP TABLE " + tableName);
         }
+    }
+
+    private static String toColumnNameInSql(String columnName, boolean delimited)
+    {
+        String nameInSql = columnName;
+        if (delimited) {
+            nameInSql = "\"" + columnName.replace("\"", "\"\"") + "\"";
+        }
+        return nameInSql;
     }
 
     protected boolean isColumnNameRejected(Exception exception, String columnName, boolean delimited)
