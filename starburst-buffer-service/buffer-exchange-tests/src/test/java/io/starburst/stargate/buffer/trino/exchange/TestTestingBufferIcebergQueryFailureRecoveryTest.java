339b1666ae1e9aff7f330366a2220163c84af876
Łukasz Osipiuk <lukasz@osipiuk.net>
2022-09-26 15:35:52 +0200

    [BUFFER] Add Iceberg fault-tolerant tests on buffer service exchange
    (cherry picked from commit cef5d9449e15788c5ba28f88b440f5222b2b41d7)

---
+++ starburst-buffer-service/buffer-exchange-tests/src/test/java/io/starburst/stargate/buffer/trino/exchange/TestTestingBufferIcebergQueryFailureRecoveryTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright Starburst Data, Inc. All rights reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STARBURST DATA.
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ */
+package io.starburst.stargate.buffer.trino.exchange;
+
+import com.google.common.collect.ImmutableMap;
+import io.airlift.log.Logger;
+import io.airlift.units.DataSize;
+import io.starburst.stargate.buffer.testing.TestingBufferService;
+import io.trino.faulttolerant.iceberg.BaseIcebergFailureRecoveryTest;
+import io.trino.operator.RetryPolicy;
+import io.trino.plugin.iceberg.IcebergQueryRunner;
+import io.trino.testing.QueryRunner;
+import io.trino.tpch.TpchTable;
+import org.testng.annotations.AfterClass;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+public class TestTestingBufferIcebergQueryFailureRecoveryTest
+        extends BaseIcebergFailureRecoveryTest
+{
+    protected TestTestingBufferIcebergQueryFailureRecoveryTest()
+    {
+        super(RetryPolicy.TASK);
+    }
+
+    private static final Logger log = Logger.get(TestTestingBufferDistributedFaultTolerantEngineOnlyQueries.class);
+
+    private TestingBufferService bufferService;
+
+    @Override
+    protected QueryRunner createQueryRunner(List<TpchTable<?>> requiredTpchTables, Map<String, String> configProperties, Map<String, String> coordinatorProperties)
+            throws Exception
+    {
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        long memoryHeadroom = (long) (0.7 * maxMemory);
+        int dataServersCount = 3;
+        log.info("Starting testing buffer service with %d data nodes; each with %s memory", dataServersCount, DataSize.succinctBytes(maxMemory - memoryHeadroom));
+
+        bufferService = TestingBufferService
+                .builder()
+                .withDiscoveryServerBuilder(
+                        builder -> builder.setConfigProperty("buffer.discovery.start-grace-period", "3s"))
+                .withDataServerBuilder(
+                        builder -> builder
+                                .setConfigProperty("memory.heap-headroom", DataSize.succinctBytes(memoryHeadroom).toString())
+                                .setConfigProperty("exchange.staleness-threshold", "2h")) //tmp
+                .setDataServersCount(dataServersCount)
+                .build();
+        URI discoveryServerUri = bufferService.getDiscoveryServer().getBaseUri();
+
+        ImmutableMap<String, String> exchangeManagerProperties = ImmutableMap.<String, String>builder()
+                .put("exchange.buffer-discovery.uri", discoveryServerUri.toString())
+                .buildOrThrow();
+
+        return IcebergQueryRunner.builder()
+                .setInitialTables(requiredTpchTables)
+                .setCoordinatorProperties(coordinatorProperties)
+                .setExtraProperties(configProperties)
+                .setIcebergProperties(Map.of("iceberg.experimental.extended-statistics.enabled", "true"))
+                .setAdditionalSetup(runner -> {
+                    runner.installPlugin(new BufferExchangePlugin());
+                    runner.loadExchangeManager("buffer", exchangeManagerProperties);
+                })
+                .build();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void destroy()
+            throws Exception
+    {
+        if (bufferService != null) {
+            bufferService.close();
+            bufferService = null;
+        }
+    }
+}
