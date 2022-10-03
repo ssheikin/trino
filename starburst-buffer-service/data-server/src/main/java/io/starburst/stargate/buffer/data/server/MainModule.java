0b9e960365a68f87501639f26819b5ba8cf75efc
Zebing Lin <linzebing1995@gmail.com>
2022-10-03 15:48:54 -0400

    [BUFFER] Rename PagesResponseWriter to ChunkDataResponseWriter
    (cherry picked from commit 2b4696d80543fc685d94ecad2724f20d7c5ea0c1)

--- starburst-buffer-service/data-server/src/main/java/io/starburst/stargate/buffer/data/server/MainModule.java
+++ starburst-buffer-service/data-server/src/main/java/io/starburst/stargate/buffer/data/server/MainModule.java
@@ -64,7 +64,7 @@ public class MainModule
         configBinder(binder).bindConfig(MemoryAllocatorConfig.class);
         configBinder(binder).bindConfig(DataServerConfig.class);
         jaxrsBinder(binder).bind(DataResource.class);
-        jaxrsBinder(binder).bind(PagesResponseWriter.class);
+        jaxrsBinder(binder).bind(ChunkDataResponseWriter.class);
         jaxrsBinder(binder).bind(PagesResponseReader.class);
         binder.bind(MemoryAllocator.class).in(SINGLETON);
         binder.bind(Long.class).annotatedWith(BufferNodeId.class).toInstance(bufferNodeId);
