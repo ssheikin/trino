7b3a2c521f50b843860179800dbf5dd5efb644f9
Łukasz Osipiuk <lukasz@osipiuk.net>
2022-10-07 11:21:10 +0200

    [BUFFER] Bump Trino dependency to 401
    This moves execution to even based scheduler
    
    (cherry picked from commit 87a46844e0c11144318efbf1cae4a817d7e77b78)

--- starburst-buffer-service/buffer-exchange-tests/src/test/java/io/starburst/stargate/buffer/trino/exchange/TestBufferExchangeSpecialCases.java
+++ starburst-buffer-service/buffer-exchange-tests/src/test/java/io/starburst/stargate/buffer/trino/exchange/TestBufferExchangeSpecialCases.java
@@ -40,7 +40,7 @@ import static io.starburst.stargate.buffer.testing.MockDataNodeStats.Key.SUCCESS
 import static io.starburst.stargate.buffer.testing.MockDataNodeStats.Key.SUCCESSFUL_GET_CHUNK_DATA_FROM_DRAINED_STORAGE_REQUEST_COUNT;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Test(enabled = false) // requires event based scheduler
+@Test
 public class TestBufferExchangeSpecialCases
 {
     private final ExecutorService executor = Executors.newCachedThreadPool();
