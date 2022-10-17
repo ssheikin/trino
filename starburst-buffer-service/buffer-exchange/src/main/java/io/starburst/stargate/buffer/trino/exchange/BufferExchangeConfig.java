700010d6c325aeb7078d8ce9fc19108203738e73
Łukasz Osipiuk <lukasz@osipiuk.net>
2022-10-17 13:31:17 +0200

    [BUFFER] Remove extranous NonNull annotation
    (cherry picked from commit f244d7a919f783d4985cd5ac30ae3d9dca9b4789)

--- starburst-buffer-service/buffer-exchange/src/main/java/io/starburst/stargate/buffer/trino/exchange/BufferExchangeConfig.java
+++ starburst-buffer-service/buffer-exchange/src/main/java/io/starburst/stargate/buffer/trino/exchange/BufferExchangeConfig.java
@@ -123,7 +123,6 @@ public class BufferExchangeConfig
         return dataIntegrityVerificationEnabled;
     }
 
-    @NotNull
     public int getSourceHandleTargetChunksCount()
     {
         return sourceHandleTargetChunksCount;
