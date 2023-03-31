8057aa1eef9b87e797202bc315c3c3254074f63b
Łukasz Osipiuk <lukasz@osipiuk.net>
2023-03-31 18:45:34 +0200

    [BUFFER] Add sanity checks
    (cherry picked from commit d676664227b93abc855ffd6a9a4f6dad293c46e6)

--- starburst-buffer-service/buffer-exchange/src/main/java/io/starburst/stargate/buffer/trino/exchange/BufferExchangeSinkInstanceHandle.java
+++ starburst-buffer-service/buffer-exchange/src/main/java/io/starburst/stargate/buffer/trino/exchange/BufferExchangeSinkInstanceHandle.java
@@ -13,7 +13,12 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.trino.spi.exchange.ExchangeSinkInstanceHandle;
 
+import java.util.List;
+import java.util.Map;
+
 import static com.google.common.base.MoreObjects.toStringHelper;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.util.Objects.requireNonNull;
 
 public class BufferExchangeSinkInstanceHandle
@@ -23,7 +28,32 @@ public class BufferExchangeSinkInstanceHandle
     private final int taskAttemptId;
     private final PartitionNodeMapping partitionNodeMapping;
 
+    public BufferExchangeSinkInstanceHandle create(
+            BufferExchangeSinkHandle sinkHandle,
+            int taskAttemptId,
+            PartitionNodeMapping partitionNodeMapping)
+    {
+        requireNonNull(partitionNodeMapping, "partitionNodeMapping is null");
+
+        List<Integer> partitionsWithNoNodes = partitionNodeMapping.getMapping().asMap().entrySet().stream()
+                .filter(entry -> entry.getValue().isEmpty())
+                .map(Map.Entry::getKey)
+                .collect(toImmutableList());
+        checkArgument(partitionsWithNoNodes.isEmpty(), "No buffer nodes assigned for partitions %s", partitionsWithNoNodes);
+
+        if (sinkHandle.isPreserveOrderWithinPartition()) {
+            List<Integer> partitionsWithManyNodes = partitionNodeMapping.getMapping().asMap().entrySet().stream()
+                    .filter(entry -> entry.getValue().size() > 1)
+                    .map(Map.Entry::getKey)
+                    .collect(toImmutableList());
+            checkArgument(partitionsWithNoNodes.isEmpty(), "Multiple buffer nodes assigned for %s when isPreserveOrderWithinPartition is set to true", partitionsWithManyNodes);
+        }
+
+        return new BufferExchangeSinkInstanceHandle(sinkHandle, taskAttemptId, partitionNodeMapping);
+    }
+
     @JsonCreator
+    @Deprecated
     public BufferExchangeSinkInstanceHandle(
             @JsonProperty("sinkHandle") BufferExchangeSinkHandle sinkHandle,
             @JsonProperty("taskAttemptId") int taskAttemptId,
