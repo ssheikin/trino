cf4980e5d7402b1fbb5ec23038e24f69e9c1e48f
Łukasz Osipiuk <lukasz@osipiuk.net>
2023-06-06 22:00:48 +0200

    [BUFFER] Add dummy non-test class to buffer-exchange-tests
    Since airbase 139 maven complains if there are just
    test classes in the module
    
    (cherry picked from commit 0577220f6bcebed59c2c9ba14c3dcbfbcefad17a)

---
+++ starburst-buffer-service/buffer-exchange-tests/src/main/java/io/starburst/stargate/buffer/trino/exchange/Dummy.java
@@ -0,0 +1,15 @@
+package io.starburst.stargate.buffer.trino.exchange;
+/*
+ * Copyright Starburst Data, Inc. All rights reserved.
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STARBURST DATA.
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ *
+ * Redistribution of this material is strictly prohibited.
+ */
+
+public class Dummy
+{
+    private Dummy() {}
+}
