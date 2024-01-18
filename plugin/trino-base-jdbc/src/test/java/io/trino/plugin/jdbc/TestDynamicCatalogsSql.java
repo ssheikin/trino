643a61733f0454270a33f1fea9af264aa51c534f
Sasha Sheikin <myminitrue@gmail.com>
2024-01-18 12:30:06 +0100

    Add `CREATE CATALOG LIKE` implementation
    (cherry picked from commit c4b96d25f093f9e09d0882cfc34a9b87092e1f8c)
    And resolved conflicts with base branch
    (cherry picked from commit e738be45d59e35cd0d77b9a8c90b01a02045d4ea)

--- plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/TestDynamicCatalogsSql.java
+++ plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/TestDynamicCatalogsSql.java
@@ -65,6 +65,39 @@ public class TestDynamicCatalogsSql
     }
 
     @Test
+    public void testCreateCatalogLike()
+    {
+        String createCatalogSql = """
+                CREATE CATALOG %s USING base_jdbc
+                WITH (
+                   "bootstrap.quiet" = 'true',
+                   "connection-url" = '%s'
+                )""";
+
+        String oldCatalog = "catalog_" + randomNameSuffix();
+        String oldConnectionUrl = createH2ConnectionUrl();
+        assertUpdate(createCatalogSql.formatted(oldCatalog, oldConnectionUrl));
+        assertThat((String) computeActual("SHOW CREATE CATALOG " + oldCatalog).getOnlyValue())
+                .isEqualTo(createCatalogSql.formatted(oldCatalog, oldConnectionUrl));
+
+        String catalog = "catalog_" + randomNameSuffix();
+        String newConnectionUrl = createH2ConnectionUrl();
+        assertUpdate("""
+                CREATE CATALOG %s LIKE %s
+                WITH (
+                   "bootstrap.quiet" = 'true',
+                   "connection-url" = '%s'
+                )
+                """
+                .formatted(catalog, oldCatalog, newConnectionUrl));
+        assertThat((String) computeActual("SHOW CREATE CATALOG " + catalog).getOnlyValue())
+                .isEqualTo(createCatalogSql.formatted(catalog, newConnectionUrl));
+
+        assertUpdate("DROP CATALOG " + catalog);
+        assertUpdate("DROP CATALOG " + oldCatalog);
+    }
+
+    @Test
     public void testShowCreateCatalog()
     {
         String createCatalogSql = """
