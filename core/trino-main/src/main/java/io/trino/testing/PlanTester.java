1517f9091999899bda41000ef627746e82ac5888
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-03-25 23:22:27 -0700

    Remove unsafe generics from PlanNodeSearcher
    Let the caller do the cast explicitly if needed, avoid heap pollution.
    
    (cherry picked from commit 8ed9eb77d1f6cf5a2db93c6a56814c142596a1b3)
    (cherry picked from commit a02e053e4f70c22dc28829a36d7f298a168548b7)

--- core/trino-main/src/main/java/io/trino/testing/PlanTester.java
+++ core/trino-main/src/main/java/io/trino/testing/PlanTester.java
@@ -232,6 +232,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static io.airlift.concurrent.MoreFutures.getFutureValue;
 import static io.airlift.concurrent.Threads.daemonThreadsNamed;
@@ -1048,6 +1049,8 @@ public class PlanTester
     {
         return searchFrom(node)
                 .where(TableScanNode.class::isInstance)
-                .findAll();
+                .findAll().stream()
+                .map(TableScanNode.class::cast)
+                .collect(toImmutableList());
     }
 }
