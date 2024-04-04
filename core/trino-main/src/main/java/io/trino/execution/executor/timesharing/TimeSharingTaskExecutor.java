33754e1b78d13150ade05ed640ea93ad580d387b
Łukasz Osipiuk <lukasz@osipiuk.net>
2024-04-04 14:32:30 +0200

    Force split failure on unexpected exception
    Fix exception handling TimeSharingTaskExecutor.
    We just logged an error and marked the PrioritizedSplitRunner as
    finished. The task for which the PrioritizedSplitRunner was
    executing remained RUNNING and query hang indefinitelly.
    
    Now we ensure that finished future obtained from
    TimeSharingTaskExecutor is completed with relevant exception.
    
    (cherry picked from commit ffff03aa62e5f8a4f0860a5c9cd29b55a37d3881)

--- core/trino-main/src/main/java/io/trino/execution/executor/timesharing/TimeSharingTaskExecutor.java
+++ core/trino-main/src/main/java/io/trino/execution/executor/timesharing/TimeSharingTaskExecutor.java
@@ -602,6 +602,7 @@ public class TimeSharingTaskExecutor
                                 log.error(t, "Error processing %s", split.getInfo());
                             }
                         }
+                        split.markFailed(t);
                         splitFinished(split);
                     }
                     finally {
