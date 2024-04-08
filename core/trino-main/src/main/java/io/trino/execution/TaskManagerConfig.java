504817f36c71d1a3cfeb13852bfa0ae35d4e0fc6
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-04-08 10:33:26 +0200

    Remove long deprecated configs
    Remove configs deprecated in 2022 or earlier.
    
    (cherry picked from commit 1164e9e2667c168331015a4960d5b3ed11136c41)
    (cherry picked from commit 06b939ccf6b62dece2f2a95f1d468fb027822caf)

--- core/trino-main/src/main/java/io/trino/execution/TaskManagerConfig.java
+++ core/trino-main/src/main/java/io/trino/execution/TaskManagerConfig.java
@@ -45,6 +45,8 @@ import static java.math.BigDecimal.TWO;
         "task.level-absolute-priority",
         "task.max-memory",
         "task.operator-pre-allocated-memory",
+        "task.shard.max-threads",
+        "task.verbose-stats",
 })
 public class TaskManagerConfig
 {
@@ -174,7 +176,6 @@ public class TaskManagerConfig
         return perOperatorCpuTimerEnabled;
     }
 
-    @LegacyConfig("task.verbose-stats")
     @Config("task.per-operator-cpu-timer-enabled")
     public TaskManagerConfig setPerOperatorCpuTimerEnabled(boolean perOperatorCpuTimerEnabled)
     {
@@ -290,7 +291,6 @@ public class TaskManagerConfig
         return maxWorkerThreads;
     }
 
-    @LegacyConfig("task.shard.max-threads")
     @Config("task.max-worker-threads")
     public TaskManagerConfig setMaxWorkerThreads(String maxWorkerThreads)
     {
