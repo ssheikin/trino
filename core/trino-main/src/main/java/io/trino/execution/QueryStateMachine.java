179e4333676c29d355a9e3c8814370e9182e9a49
Łukasz Osipiuk <lukasz@osipiuk.net>
2024-04-08 11:10:48 +0200

    Move comment to proper place
    (cherry picked from commit 5d1077e9192941561ac8fe25b12733450a1b5afa)
    (cherry picked from commit 90f070162027ff164245498073e48bb74cf00110)

--- core/trino-main/src/main/java/io/trino/execution/QueryStateMachine.java
+++ core/trino-main/src/main/java/io/trino/execution/QueryStateMachine.java
@@ -1530,8 +1530,8 @@ public class QueryStateMachine
                 queryStats.getFailedPhysicalWrittenDataSize(),
                 queryStats.getStageGcStatistics(),
                 queryStats.getDynamicFiltersStats(),
-                ImmutableList.of(),
-                ImmutableList.of()); // Remove the operator summaries as OperatorInfo (especially DirectExchangeClientStatus) can hold onto a large amount of memory
+                ImmutableList.of(), // Remove the operator summaries as OperatorInfo (especially DirectExchangeClientStatus) can hold onto a large amount of memory
+                ImmutableList.of());
     }
 
     private QueryOutputManager getOutputManager()
