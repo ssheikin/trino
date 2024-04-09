07d7f5ba5c9c2e06931fdf7fb5904eb184f38b25
Zhang JianAo <zhangjianao@gmail.com>
2024-04-09 12:10:57 +0800

    Add mongodb dynamic filter support
    (cherry picked from commit bfadbb23f2f7c64c1a49d8c9a8bb356bea91eec6)
    (cherry picked from commit 3e6d0eb8c6697bf88d2744e66e3ec89f5fac2a8a)

--- plugin/trino-mongodb/src/main/java/io/trino/plugin/mongodb/MongoPageSourceProvider.java
+++ plugin/trino-mongodb/src/main/java/io/trino/plugin/mongodb/MongoPageSourceProvider.java
@@ -23,14 +23,19 @@ import io.trino.spi.connector.ConnectorSplit;
 import io.trino.spi.connector.ConnectorTableHandle;
 import io.trino.spi.connector.ConnectorTransactionHandle;
 import io.trino.spi.connector.DynamicFilter;
+import io.trino.spi.connector.EmptyPageSource;
+import io.trino.spi.predicate.TupleDomain;
 
 import java.util.List;
 
+import static io.trino.plugin.mongodb.TypeUtils.isPushdownSupportedType;
 import static java.util.Objects.requireNonNull;
 
 public class MongoPageSourceProvider
         implements ConnectorPageSourceProvider
 {
+    private static final int MONGO_DOMAIN_COMPACTION_THRESHOLD = 1000;
+
     private final MongoSessionProvider mongoSessionProvider;
 
     @Inject
@@ -49,12 +54,36 @@ public class MongoPageSourceProvider
             DynamicFilter dynamicFilter)
     {
         MongoTableHandle tableHandle = (MongoTableHandle) table;
+        MongoSession mongoSession = mongoSessionProvider.getMongoSession(session.getIdentity());
 
         ImmutableList.Builder<MongoColumnHandle> handles = ImmutableList.builder();
         for (ColumnHandle handle : requireNonNull(columns, "columns is null")) {
             handles.add((MongoColumnHandle) handle);
         }
 
-        return new MongoPageSource(mongoSessionProvider.getMongoSession(session.getIdentity()), tableHandle, handles.build());
+        TupleDomain<MongoColumnHandle> dynamicPredicate = dynamicFilter
+                .getCurrentPredicate()
+                .transformKeys(MongoColumnHandle.class::cast)
+                .filter((mongoColumnHandle, domain) -> isPushdownSupportedType(mongoColumnHandle.type()));
+
+        MongoTableHandle newTableHandle;
+
+        if (dynamicFilter == DynamicFilter.EMPTY || tableHandle.limit().isPresent()) {
+            newTableHandle = tableHandle;
+        }
+        else {
+            TupleDomain<ColumnHandle> newDomain = tableHandle
+                    .constraint()
+                    .intersect(dynamicPredicate)
+                    .simplify(MONGO_DOMAIN_COMPACTION_THRESHOLD);
+
+            newTableHandle = tableHandle.withConstraint(newDomain);
+        }
+
+        if (newTableHandle.constraint().isNone()) {
+            return new EmptyPageSource();
+        }
+
+        return new MongoPageSource(mongoSession, newTableHandle, handles.build());
     }
 }
