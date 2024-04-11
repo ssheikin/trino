fcaf892f0b1cfb93bdb1d9d5be08d5f8e00c0d74
Semion Paramonow <semion.paramonow@starburstdata.com>
2024-04-11 14:29:32 +0200

    Add char->varchar type coercion in Iceberg CTAS
    Similar to #13981, extend type coercion support to char type.
    
    (cherry picked from commit 6e104f2453e6ed922e4f1bf9a902e3f63bc465f3)
    (cherry picked from commit 755e429525fec42e586761104513d048410ea8ac)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
@@ -104,6 +104,7 @@ import io.trino.spi.statistics.ColumnStatisticMetadata;
 import io.trino.spi.statistics.ComputedStatistics;
 import io.trino.spi.statistics.TableStatistics;
 import io.trino.spi.statistics.TableStatisticsMetadata;
+import io.trino.spi.type.CharType;
 import io.trino.spi.type.LongTimestamp;
 import io.trino.spi.type.LongTimestampWithTimeZone;
 import io.trino.spi.type.TimeType;
@@ -287,6 +288,7 @@ import static io.trino.spi.type.TimeType.TIME_MICROS;
 import static io.trino.spi.type.TimestampType.TIMESTAMP_MICROS;
 import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_TZ_MICROS;
 import static io.trino.spi.type.Timestamps.MICROSECONDS_PER_MILLISECOND;
+import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.lang.Math.floorDiv;
 import static java.lang.String.format;
 import static java.util.Locale.ENGLISH;
@@ -936,6 +938,9 @@ public class IcebergMetadata
         }
         if (type instanceof TimeType) {
             return Optional.of(TIME_MICROS);
+        }
+        if (type instanceof CharType) {
+            return Optional.of(VARCHAR);
         }
         return Optional.empty();
     }
