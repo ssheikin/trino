98f536a932fe879eb6ce6b4c897e9f40f2e421ee
shlomi.alfasi@starburstdata.com <shlomi.alfasi@starburstdata.com>
2024-04-15 15:45:11 +0300

    [WARP-SPEED] Sync warp 26-04
    (cherry picked from commit 24070e5dcf44e2f4fae158070c63d0577afa588f)

--- plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/varada/dispatcher/DispatcherMetadata.java
+++ plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/varada/dispatcher/DispatcherMetadata.java
@@ -594,6 +594,13 @@ public class DispatcherMetadata
     }
 
     @Override
+    public void createView(ConnectorSession session, SchemaTableName viewName, ConnectorViewDefinition definition, Map<String, Object> viewProperties, boolean replace)
+    {
+        proxiedConnectorMetadata.createView(session, viewName, definition, viewProperties, replace);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
     public void createView(ConnectorSession session, SchemaTableName viewName, ConnectorViewDefinition definition, boolean replace)
     {
         proxiedConnectorMetadata.createView(session, viewName, definition, replace);
@@ -633,6 +640,12 @@ public class DispatcherMetadata
     public Optional<ConnectorViewDefinition> getView(ConnectorSession session, SchemaTableName viewName)
     {
         return proxiedConnectorMetadata.getView(session, viewName);
+    }
+
+    @Override
+    public Map<String, Object> getViewProperties(ConnectorSession session, SchemaTableName viewName)
+    {
+        return proxiedConnectorMetadata.getViewProperties(session, viewName);
     }
 
     @Override
