0e4f07e90199a85b5d7146bd34b643b8f4c59ea9
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-04-16 14:16:09 +0200

    Convert TableMetadata to record
    (cherry picked from commit 62f7a0bf21e1a4ff8e2d336a4fdc255cb7ab4d5c)
    (cherry picked from commit 7dac124bda0b069216fe211aaba8e61d403c8cfb)

--- core/trino-main/src/main/java/io/trino/sql/rewrite/ShowQueriesRewrite.java
+++ core/trino-main/src/main/java/io/trino/sql/rewrite/ShowQueriesRewrite.java
@@ -700,7 +700,7 @@ public final class ShowQueriesRewrite
 
                 QualifiedObjectName targetTableName = redirection.redirectedTableName().orElse(objectName);
                 accessControl.checkCanShowCreateTable(session.toSecurityContext(), targetTableName);
-                ConnectorTableMetadata connectorTableMetadata = metadata.getTableMetadata(session, tableHandle).getMetadata();
+                ConnectorTableMetadata connectorTableMetadata = metadata.getTableMetadata(session, tableHandle).metadata();
 
                 Collection<PropertyMetadata<?>> allColumnProperties = columnPropertyManager.getAllProperties(tableHandle.getCatalogHandle());
 
