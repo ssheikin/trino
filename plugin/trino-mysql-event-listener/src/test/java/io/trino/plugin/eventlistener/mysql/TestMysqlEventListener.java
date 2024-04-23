4d4eb475eb70de7b506738640cd6f92356b97d1d
Łukasz Osipiuk <lukasz@osipiuk.net>
2024-04-23 11:54:46 +0200

    Add StageTaskStatistics field to QueryStatistics
    Add StageTaskStatistics to capture various task related statistics
    per-stage manner in query competion event.
    Initially only put cpu time distribution there rendering specialized
    QueryStatistics.cpuTimeDistribution obsolete.
    
    (cherry picked from commit 2e1759a24c8bb74dbda86b9a44cc12114277e8d0)
    (cherry picked from commit 5e2e155c649b9de0b6e9147a6d915db864b02914)

--- plugin/trino-mysql-event-listener/src/test/java/io/trino/plugin/eventlistener/mysql/TestMysqlEventListener.java
+++ plugin/trino-mysql-event-listener/src/test/java/io/trino/plugin/eventlistener/mysql/TestMysqlEventListener.java
@@ -130,6 +130,8 @@ public class TestMysqlEventListener
             // not stored
             Collections.emptyList(),
             // not stored
+            Collections.emptyList(),
+            // not stored
             List.of("{operator: \"operator1\"}", "{operator: \"operator2\"}"),
             // not stored
             Collections.emptyList(),
@@ -284,6 +286,8 @@ public class TestMysqlEventListener
             Collections.emptyList(),
             130,
             false,
+            // not stored
+            Collections.emptyList(),
             // not stored
             Collections.emptyList(),
             // not stored
