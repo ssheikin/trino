ff65b630bb98cbdcb584011a4fcf6cdfd0d6b847
Yuya Ebihara <ebyhry@gmail.com>
2024-04-29 14:40:29 +0900

    Convert HiveBucketHandle to record
    (cherry picked from commit 74237c7524213cd81ca8689209fa5069d2e4f183)
    (cherry picked from commit a47ee1d2a1ba5af5acaad69af2b0f80677a0a435)

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/BaseHiveConnectorTest.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/BaseHiveConnectorTest.java
@@ -3839,7 +3839,7 @@ public abstract class BaseHiveConnectorTest
 
     private int getBucketCount(String tableName)
     {
-        return (int) getHiveTableProperty(tableName, table -> table.getBucketHandle().get().getTableBucketCount());
+        return (int) getHiveTableProperty(tableName, table -> table.getBucketHandle().get().tableBucketCount());
     }
 
     @Test
