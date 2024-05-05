39247a01e5a0a595b2f169cce0c1282486fd7cb4
Grzegorz Kokosiński <grzegorz@starburstdata.com>
2024-05-05 15:01:18 +0200

    [TROUBLESHOOTING] Use ClosingBinder
    There is no logic change here. This commit only uses existing
    ClosingBinder to replace its alternatives.
    
    (cherry picked from commit 0a7e8f2ec3645f24f17b4dc387f4a0d4ee9e0be3)

--- core/trino-main/src/main/java/io/starburst/server/troubleshooting/TroubleshootingModule.java
+++ core/trino-main/src/main/java/io/starburst/server/troubleshooting/TroubleshootingModule.java
@@ -26,23 +26,20 @@ import io.starburst.server.troubleshooting.tracing.SpanSerializer;
 import io.starburst.server.troubleshooting.tracing.TroubleshootingSpanProcessor;
 import io.starburst.server.troubleshooting.tracing.TroubleshootingTraceResource;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
-import io.airlift.log.Logger;
 import io.airlift.units.DataSize;
 import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.trino.SystemSessionPropertiesProvider;
 import io.trino.server.ServerConfig;
-import jakarta.annotation.PreDestroy;
 import jdk.jfr.FlightRecorder;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
 import static io.airlift.concurrent.Threads.daemonThreadsNamed;
 import static io.airlift.configuration.ConfigBinder.configBinder;
 import static io.airlift.jaxrs.JaxrsBinder.jaxrsBinder;
+import static io.trino.plugin.base.ClosingBinder.closingBinder;
 import static io.trino.server.InternalCommunicationHttpClientModule.internalHttpClientModule;
-import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newScheduledThreadPool;
 
 public class TroubleshootingModule
@@ -78,7 +75,6 @@ public class TroubleshootingModule
         ScheduledExecutorService troubleshootingExecutor = newScheduledThreadPool(4, daemonThreadsNamed("query-troubleshooting-%s"));
         binder.bind(ScheduledExecutorService.class).annotatedWith(ForTroubleshooting.class)
                 .toInstance(troubleshootingExecutor);
-        binder.bind(ExecutorServiceCleaner.class).toInstance(new ExecutorServiceCleaner(troubleshootingExecutor));
         binder.bind(FullQueryInfoProvider.class).to(FullQueryInfoProviderDispatchManager.class).in(Scopes.SINGLETON);
         binder.bind(TroubleshootingArchiver.class).in(Scopes.SINGLETON);
         binder.bind(RemoteTroubleshootingTraceClient.class).in(Scopes.SINGLETON);
@@ -89,23 +85,8 @@ public class TroubleshootingModule
         setBinder.addBinding().to(JmxTroubleshootingProvider.class);
         setBinder.addBinding().to(QueryJsonProvider.class);
         setBinder.addBinding().to(OpenTelemetryTraceProvider.class);
-    }
 
-    public static class ExecutorServiceCleaner
-    {
-        private static final Logger log = Logger.get(ExecutorServiceCleaner.class);
-        private final ExecutorService executorService;
-
-        ExecutorServiceCleaner(ExecutorService executorService)
-        {
-            this.executorService = requireNonNull(executorService, "executor is null");
-        }
-
-        @PreDestroy
-        public void cleanNow()
-        {
-            log.info("cleanNow %s", executorService);
-            executorService.shutdownNow();
-        }
+        closingBinder(binder)
+                .registerExecutor(ScheduledExecutorService.class, ForTroubleshooting.class);
     }
 }
