cad59e092fb979b36a5a5a8f96dd8e09665b995d
Yuya Ebihara <ebyhry@gmail.com>
2024-05-22 11:01:48 +0900

    Replace QueryAssert.assertThat with Assertions.assertThat
    (cherry picked from commit 79768d38d062e16986eb0adeb9ed7db0a8bcf56b)
    (cherry picked from commit d401dcc402ab970805969ddb60adc8190480079c)

--- testing/trino-product-tests/src/main/java/io/trino/tests/product/hive/TestHiveMerge.java
+++ testing/trino-product-tests/src/main/java/io/trino/tests/product/hive/TestHiveMerge.java
@@ -28,7 +28,6 @@ import java.util.stream.IntStream;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.trino.tempto.assertions.QueryAssert.Row.row;
 import static io.trino.tempto.assertions.QueryAssert.assertQueryFailure;
-import static io.trino.tempto.assertions.QueryAssert.assertThat;
 import static io.trino.tests.product.TestGroups.HIVE_TRANSACTIONAL;
 import static io.trino.tests.product.TestGroups.PROFILE_SPECIFIC_TESTS;
 import static io.trino.tests.product.hive.BucketingType.BUCKETED_V2;
@@ -40,6 +39,7 @@ import static io.trino.tests.product.utils.QueryExecutors.onHive;
 import static io.trino.tests.product.utils.QueryExecutors.onTrino;
 import static java.lang.String.format;
 import static java.util.Locale.ENGLISH;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestHiveMerge
         extends HiveProductTest
