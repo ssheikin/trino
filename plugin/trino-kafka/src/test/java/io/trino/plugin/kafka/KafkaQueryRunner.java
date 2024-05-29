4cf2dd4f6156bcd2a4096d2496f906395f2123bb
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-05-29 11:54:27 +0200

    Enable warning/error logs from libraries in tests
    Warning and errors are rarely seen by engineers, but still it's better
    not to hide them, they may (now or in the future) contain some
    actionable information. When a library over-verbosely logs some
    warnings/errors, let's suppress these selectively.
    
    (cherry picked from commit 29fcd50d936e538f908a009e2ffbaa97c5ce7b8c)
    (cherry picked from commit 85a38a19b6d3b16e85f95e46b4ef77b8e40832ff)

--- plugin/trino-kafka/src/test/java/io/trino/plugin/kafka/KafkaQueryRunner.java
+++ plugin/trino-kafka/src/test/java/io/trino/plugin/kafka/KafkaQueryRunner.java
@@ -63,7 +63,7 @@ public final class KafkaQueryRunner
 
     static {
         Logging logging = Logging.initialize();
-        logging.setLevel("org.apache.kafka", Level.OFF);
+        logging.setLevel("org.apache.kafka", Level.WARN);
     }
 
     private static final Logger log = Logger.get(KafkaQueryRunner.class);
