222492a8cb452955821e75aeaf5f2301beac7f40
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-05-29 14:37:18 +0200

    Revert "Enable warning/error logs from libraries in tests"
    This reverts commit 29fcd50d936e538f908a009e2ffbaa97c5ce7b8c.
    
    (cherry picked from commit cd126e791614f64425cb7e20bf306048e63aad16)
    (cherry picked from commit 817186e9aad6ad5b69ed5868c329f2e6fd5e7a1b)

--- plugin/trino-kafka/src/test/java/io/trino/plugin/kafka/KafkaQueryRunner.java
+++ plugin/trino-kafka/src/test/java/io/trino/plugin/kafka/KafkaQueryRunner.java
@@ -63,7 +63,7 @@ public final class KafkaQueryRunner
 
     static {
         Logging logging = Logging.initialize();
-        logging.setLevel("org.apache.kafka", Level.WARN);
+        logging.setLevel("org.apache.kafka", Level.OFF);
     }
 
     private static final Logger log = Logger.get(KafkaQueryRunner.class);
