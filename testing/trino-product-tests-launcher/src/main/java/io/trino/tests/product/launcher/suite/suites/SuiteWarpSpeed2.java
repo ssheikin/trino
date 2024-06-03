1b04f5b81c35a1b53d8da63ca7dce2418b4369a6
Assaf Bern <assaf.bern@starburstdata.com>
2024-06-03 14:05:48 +0300

    Revert "Revert "Revert "[WARP-SPEED] disable cache flaky tests"""
    This reverts commit 7946b16545fba96f75f3a886c7ca4f751a601d38.
    
    (cherry picked from commit 416a063544b44560d03c20d6e5a7f7cf98bbce5c)

--- testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteWarpSpeed2.java
+++ testing/trino-product-tests-launcher/src/main/java/io/trino/tests/product/launcher/suite/suites/SuiteWarpSpeed2.java
@@ -16,12 +16,14 @@ package io.trino.tests.product.launcher.suite.suites;
 import com.google.common.collect.ImmutableList;
 import io.trino.tests.product.launcher.env.EnvironmentConfig;
 import io.trino.tests.product.launcher.env.environment.EnvMultinodeWarpGlueHive;
+import io.trino.tests.product.launcher.env.environment.EnvMultinodeWarpHiveCache;
 import io.trino.tests.product.launcher.env.environment.EnvMultinodeWarpSpeedMinio;
 import io.trino.tests.product.launcher.suite.Suite;
 import io.trino.tests.product.launcher.suite.SuiteTestRun;
 
 import java.util.List;
 
+import static io.trino.tests.product.TestGroups.WARP_SPEED_CACHE;
 import static io.trino.tests.product.TestGroups.WARP_SPEED_HIVE_2;
 import static io.trino.tests.product.TestGroups.WARP_SPEED_MINIO;
 import static io.trino.tests.product.launcher.suite.SuiteTestRun.testOnEnvironment;
@@ -36,9 +38,9 @@ public class SuiteWarpSpeed2
                 testOnEnvironment(EnvMultinodeWarpGlueHive.class)
                         .withGroups(WARP_SPEED_HIVE_2)
                         .build(),
-//                testOnEnvironment(EnvMultinodeWarpHiveCache.class)
-//                        .withGroups(WARP_SPEED_CACHE)
-//                        .build(),
+                testOnEnvironment(EnvMultinodeWarpHiveCache.class)
+                        .withGroups(WARP_SPEED_CACHE)
+                        .build(),
                 testOnEnvironment(EnvMultinodeWarpSpeedMinio.class)
                         .withGroups(WARP_SPEED_MINIO)
                         .build());
