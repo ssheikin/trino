1c1498740595977b65dd505fad8e610449db8414
Raunaq Morarka <raunaqmorarka@gmail.com>
2024-06-04 01:30:15 +0530

    Populate split offsets when writing orc/parquet files in iceberg
    Offsets can be used by readers to align splits with row-group/stripe boundaries
    
    (cherry picked from commit 4cc2421a723aa916811896b0c384e9d363a54d24)
    (cherry picked from commit f66a92cca97f515d0be233a2158789895fd89f96)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
@@ -1147,6 +1147,7 @@ public class IcebergMetadata
                     .withFileSizeInBytes(task.fileSizeInBytes())
                     .withFormat(table.fileFormat().toIceberg())
                     .withMetrics(task.metrics().metrics());
+            task.fileSplitOffsets().ifPresent(builder::withSplitOffsets);
 
             if (!icebergTable.spec().fields().isEmpty()) {
                 String partitionDataJson = task.partitionDataJson()
@@ -2443,6 +2444,7 @@ public class IcebergMetadata
                             .ofPositionDeletes()
                             .withFileSizeInBytes(task.fileSizeInBytes())
                             .withMetrics(task.metrics().metrics());
+                    task.fileSplitOffsets().ifPresent(deleteBuilder::withSplitOffsets);
                     if (!partitionSpec.fields().isEmpty()) {
                         String partitionDataJson = task.partitionDataJson()
                                 .orElseThrow(() -> new VerifyException("No partition data for partitioned table"));
