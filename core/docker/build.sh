9e7b9c8095220d3d422b9adca7481dad9843bfcc
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-06-26 11:35:25 +0200

    Verify ppc64le docker image
    (cherry picked from commit d6b6db77e6302fa2975810ede71e7ef20602f49b)
    (cherry picked from commit 10ca16731564904a32cfc930a94caffb19e529f0)

--- core/docker/build.sh
+++ core/docker/build.sh
@@ -132,9 +132,6 @@ echo "🏃 Testing built images"
 source container-test.sh
 
 for arch in "${ARCHITECTURES[@]}"; do
-    # TODO: remove when https://github.com/multiarch/qemu-user-static/issues/128 is fixed
-    if [[ "$arch" != "ppc64le" ]]; then
-        test_container "${TAG_PREFIX}-$arch" "linux/$arch"
-    fi
+    test_container "${TAG_PREFIX}-$arch" "linux/$arch"
     docker image inspect -f '🚀 Built {{.RepoTags}} {{.Id}}' "${TAG_PREFIX}-$arch"
 done
