beb76fe2ea8318e629805a4b7c2ed7f291328fd4
Yuya Ebihara <ebyhry@gmail.com>
2024-07-08 10:14:57 +0900

    Follow test guideline in MySQL event listener
    (cherry picked from commit 671eb922fe0132a0412fc00a7fc56e9e9b160655)
    (cherry picked from commit aef8a69c7ce808c69b028caa4eefc0245a97b874)

--- plugin/trino-mysql-event-listener/src/test/java/io/trino/plugin/eventlistener/mysql/TestMysqlEventListener.java
+++ plugin/trino-mysql-event-listener/src/test/java/io/trino/plugin/eventlistener/mysql/TestMysqlEventListener.java
@@ -66,7 +66,7 @@ import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
 @TestInstance(PER_CLASS)
 @Execution(CONCURRENT)
-public class TestMysqlEventListener
+final class TestMysqlEventListener
 {
     private static final QueryMetadata FULL_QUERY_METADATA = new QueryMetadata(
             "full_query",
@@ -343,7 +343,7 @@ public class TestMysqlEventListener
     private JsonCodecFactory jsonCodecFactory;
 
     @BeforeAll
-    public void setup()
+    void setup()
     {
         mysqlContainer = new MySQLContainer<>("mysql:8.0.36");
         mysqlContainer.start();
@@ -354,7 +354,7 @@ public class TestMysqlEventListener
     }
 
     @AfterAll
-    public void teardown()
+    void teardown()
     {
         if (mysqlContainer != null) {
             mysqlContainer.close();
@@ -374,7 +374,7 @@ public class TestMysqlEventListener
     }
 
     @Test
-    public void testFull()
+    void testFull()
             throws SQLException
     {
         eventListener.queryCompleted(FULL_QUERY_COMPLETED_EVENT);
@@ -458,7 +458,7 @@ public class TestMysqlEventListener
     }
 
     @Test
-    public void testMinimal()
+    void testMinimal()
             throws SQLException
     {
         eventListener.queryCompleted(MINIMAL_QUERY_COMPLETED_EVENT);
