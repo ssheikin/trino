dc7933f34854dcf674ba11c4a6850096d18d6c4c
David Phillips <david@acz.org>
2024-07-16 16:30:24 -0700

    Move PartitionStatistics to metastore package
    (cherry picked from commit 175ed9d341e419ae64500ccdcc93e63337024387)
    (cherry picked from commit 907ecd55d6344c848981a1bdec01f0c8ea3689e2)

--- plugin/trino-hive/src/test/java/io/trino/plugin/hive/metastore/recording/RecordingHiveMetastore.java
+++ plugin/trino-hive/src/test/java/io/trino/plugin/hive/metastore/recording/RecordingHiveMetastore.java
@@ -14,7 +14,6 @@
 package io.trino.plugin.hive.metastore.recording;
 
 import io.trino.plugin.hive.HiveType;
-import io.trino.plugin.hive.PartitionStatistics;
 import io.trino.plugin.hive.acid.AcidTransaction;
 import io.trino.plugin.hive.metastore.Database;
 import io.trino.plugin.hive.metastore.DatabaseFunctionKey;
@@ -27,6 +26,7 @@ import io.trino.plugin.hive.metastore.HivePrivilegeInfo;
 import io.trino.plugin.hive.metastore.HivePrivilegeInfo.HivePrivilege;
 import io.trino.plugin.hive.metastore.HiveTableName;
 import io.trino.plugin.hive.metastore.Partition;
+import io.trino.plugin.hive.metastore.PartitionStatistics;
 import io.trino.plugin.hive.metastore.PartitionWithStatistics;
 import io.trino.plugin.hive.metastore.PrincipalPrivileges;
 import io.trino.plugin.hive.metastore.StatisticsUpdateMode;
