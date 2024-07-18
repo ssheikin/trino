6634762ce3940432fbb5075aed005ffbdabc2618
Mateusz "Serafin" Gajewski <mateusz.gajewski@gmail.com>
2024-07-18 15:51:28 +0200

    Require source in ClientSession
    (cherry picked from commit a6d4207a00dce3ddaede8acc9b43527e8f7daaea)
    (cherry picked from commit 12fc8614a76bc2bfa4243f8f566b9f425848b170)

--- client/trino-client/src/main/java/io/trino/client/uri/TrinoUri.java
+++ client/trino-client/src/main/java/io/trino/client/uri/TrinoUri.java
@@ -481,7 +481,7 @@ public class TrinoUri
         return uri.getScheme().equals("https") || (uri.getScheme().equals("trino") && uri.getPort() == 443);
     }
 
-    public ClientSession toClientSession()
+    public ClientSession.Builder toClientSessionBuilder()
     {
         return ClientSession.builder()
                 .server(getHttpUri())
@@ -501,8 +501,7 @@ public class TrinoUri
                 .credentials(getExtraCredentials())
                 .transactionId(null)
                 .resourceEstimates(getResourceEstimates())
-                .compressionDisabled(isCompressionDisabled())
-                .build();
+                .compressionDisabled(isCompressionDisabled());
     }
 
     protected static Set<ConnectionProperty<?, ?>> allProperties()
