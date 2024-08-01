f7bc960bbe919c62e15316d17dd0018a1c0b8972
Karol Sobczak <napewnotrafi@gmail.com>
2024-08-01 15:44:42 +0200

    Prevent too many concurrent query completed notifications
    queryCompleted notifications are handled by unbounded
    executor, hence it could cause all sort of issues when
    there is spike in query traffic and event listener
    is slow to report events.
    
    (cherry picked from commit e700260d33c1d210cb3c258f51ea281283db9927)
    (cherry picked from commit 1a04c1429800c0201b666da715ed81831adc1ca2)

--- core/trino-main/src/main/java/io/trino/eventlistener/EventListenerManager.java
+++ core/trino-main/src/main/java/io/trino/eventlistener/EventListenerManager.java
@@ -18,6 +18,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import io.airlift.configuration.secrets.SecretsResolver;
 import io.airlift.log.Logger;
+import io.airlift.stats.CounterStat;
 import io.airlift.stats.TimeStat;
 import io.trino.spi.classloader.ThreadContextClassLoader;
 import io.trino.spi.eventlistener.EventListener;
@@ -39,6 +40,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -56,10 +58,13 @@ public class EventListenerManager
     private static final File CONFIG_FILE = new File("etc/event-listener.properties");
     private static final String EVENT_LISTENER_NAME_PROPERTY = "event-listener.name";
     private final List<File> configFiles;
+    private final int maxConcurrentQueryCompletedEvents;
     private final Map<String, EventListenerFactory> eventListenerFactories = new ConcurrentHashMap<>();
     private final List<EventListener> providedEventListeners = Collections.synchronizedList(new ArrayList<>());
     private final AtomicReference<List<EventListener>> configuredEventListeners = new AtomicReference<>(ImmutableList.of());
     private final AtomicBoolean loading = new AtomicBoolean(false);
+    private final AtomicInteger concurrentQueryCompletedEvents = new AtomicInteger();
+    private final CounterStat skippedQueryCompletedEvents = new CounterStat();
 
     private final TimeStat queryCreatedTime = new TimeStat(MILLISECONDS);
     private final TimeStat queryCompletedTime = new TimeStat(MILLISECONDS);
@@ -71,6 +76,7 @@ public class EventListenerManager
     {
         this.configFiles = ImmutableList.copyOf(config.getEventListenerFiles());
         this.secretsResolver = requireNonNull(secretsResolver, "secretsResolver is null");
+        this.maxConcurrentQueryCompletedEvents = config.getMaxConcurrentQueryCompletedEvents();
     }
 
     public void addEventListenerFactory(EventListenerFactory eventListenerFactory)
@@ -147,7 +153,13 @@ public class EventListenerManager
     public void queryCompleted(Function<Boolean, QueryCompletedEvent> queryCompletedEventProvider)
     {
         try (TimeStat.BlockTimer _ = queryCompletedTime.time()) {
+            if (concurrentQueryCompletedEvents.incrementAndGet() > maxConcurrentQueryCompletedEvents) {
+                concurrentQueryCompletedEvents.decrementAndGet();
+                skippedQueryCompletedEvents.update(1);
+                return;
+            }
             doQueryCompleted(queryCompletedEventProvider);
+            concurrentQueryCompletedEvents.decrementAndGet();
         }
     }
 
@@ -221,6 +233,19 @@ public class EventListenerManager
     public TimeStat getSplitCompletedTime()
     {
         return splitCompletedTime;
+    }
+
+    @Managed
+    public int getConcurrentQueryCompletedEvents()
+    {
+        return concurrentQueryCompletedEvents.get();
+    }
+
+    @Managed
+    @Nested
+    public CounterStat getSkippedQueryCompletedEvents()
+    {
+        return skippedQueryCompletedEvents;
     }
 
     @PreDestroy
