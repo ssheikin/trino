6eb57b9f945f0c38190a8898575de7fd3ff85a3d
Yuya Ebihara <ebyhry@gmail.com>
2024-09-02 22:13:00 +0900

    Migrate assertStatement in TestSqlParser.testResetSessionAuthorization
    (cherry picked from commit 351d061fa0e42694cbdb09ae53587d789bd0c639)
    (cherry picked from commit 7f6d157c9685dc6a3f84f3402a77ee0b61226e5f)

--- core/trino-parser/src/test/java/io/trino/sql/parser/TestSqlParser.java
+++ core/trino-parser/src/test/java/io/trino/sql/parser/TestSqlParser.java
@@ -6235,7 +6235,8 @@ public class TestSqlParser
     @Test
     public void testResetSessionAuthorization()
     {
-        assertStatement("RESET SESSION AUTHORIZATION", new ResetSessionAuthorization());
+        assertThat(statement("RESET SESSION AUTHORIZATION"))
+                .isEqualTo(new ResetSessionAuthorization(location(1, 1)));
     }
 
     private static QualifiedName makeQualifiedName(String tableName)
