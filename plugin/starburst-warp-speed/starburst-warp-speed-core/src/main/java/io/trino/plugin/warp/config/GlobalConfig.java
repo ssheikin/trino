1a9d302c0854fe66b4baf072fa10d7b0fe12e154
LandauAharon <landauaharon@gmail.com>
2024-09-04 11:06:06 +0300

    [WARP-SPEED] enable lazy-collect by default

--- plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/warp/config/GlobalConfig.java
+++ plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/warp/config/GlobalConfig.java
@@ -86,7 +86,7 @@ public class GlobalConfig
     private boolean enableMatchCollect = true;
     private boolean enableMappedMatchCollect = true;
     private boolean enableVarcharMappedMatchCollect;
-    private boolean enableLazyCollect;
+    private boolean enableLazyCollect = true;
     private boolean enableOrPushdown = true;
     private boolean enableRangeFilter = true;
     private int shapingLoggerThreshold = 1000;
