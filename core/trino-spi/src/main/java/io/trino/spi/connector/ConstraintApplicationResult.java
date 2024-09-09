e08235f9adbf1559ed9a0b0db7e66fa7fe7a2aa2
Alex Jo <jo.alex2144@gmail.com>
2024-09-09 12:18:05 -0600

    Remove deprecated constructor from ConstraintApplicationResult
    (cherry picked from commit 750c847b671079781cb67581f79f5f95a72d2c90)
    (cherry picked from commit f35f024159386bb1c5548656cf10b66762ff92b6)

--- core/trino-spi/src/main/java/io/trino/spi/connector/ConstraintApplicationResult.java
+++ core/trino-spi/src/main/java/io/trino/spi/connector/ConstraintApplicationResult.java
@@ -31,17 +31,6 @@ public class ConstraintApplicationResult<T>
     private final List<Alternative<T>> alternatives;
 
     /**
-     * @param precalculateStatistics Indicates whether engine should consider calculating statistics based on the plan before pushdown,
-     * as the connector may be unable to provide good table statistics for {@code handle}.
-     * @deprecated Use {@link #ConstraintApplicationResult(Object, TupleDomain, ConnectorExpression, boolean)}.
-     */
-    @Deprecated
-    public ConstraintApplicationResult(T handle, TupleDomain<ColumnHandle> remainingFilter, boolean precalculateStatistics)
-    {
-        this(false, List.of(new Alternative<>(handle, remainingFilter, Optional.empty(), precalculateStatistics)));
-    }
-
-    /**
      * @param remainingExpression the remaining expression, which will be AND-ed with {@code remainingFilter},
      * @param precalculateStatistics Indicates whether engine should consider calculating statistics based on the plan before pushdown,
      * as the connector may be unable to provide good table statistics for {@code handle}.
