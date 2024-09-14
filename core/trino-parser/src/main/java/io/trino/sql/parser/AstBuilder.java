a4c326a2f971d9c8404a782bda9246deb8aeda94
Yuya Ebihara <ebyhry@gmail.com>
2024-09-14 16:07:45 +0900

    Avoid optional NodeLocation in RefreshMaterializedView
    (cherry picked from commit dd844930c552a505f228644edec0989d76c67152)
    (cherry picked from commit d031ca1b0e69335e11ea3ce75248c22e2b6034bf)

--- core/trino-parser/src/main/java/io/trino/sql/parser/AstBuilder.java
+++ core/trino-parser/src/main/java/io/trino/sql/parser/AstBuilder.java
@@ -653,7 +653,7 @@ class AstBuilder
     public Node visitRefreshMaterializedView(SqlBaseParser.RefreshMaterializedViewContext context)
     {
         return new RefreshMaterializedView(
-                Optional.of(getLocation(context)),
+                getLocation(context),
                 new Table(getQualifiedName(context.qualifiedName())));
     }
 
