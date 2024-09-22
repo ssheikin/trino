2433beff392e0ef146b581b69bdc3fc38bf2241e
Piotr Rzysko <piotr.rzysko@gmail.com>
2024-09-22 21:56:56 +0200

    Move getQueriesQueuedOnInternal to InternalResourceGroup
    This change provides two benefits:
    * It improves encapsulation by exposing less of InternalResourceGroup's
    internal state.
    * Since getQueriesQueuedOnInternal is very similar to
    InternalResourceGroup::getWaitingQueuedQueries, it's better to keep them
     close for consistency.
    
    (cherry picked from commit 2905643dbb5641362c898da18662d81235535cd6)

--- core/trino-main/src/main/java/io/trino/execution/resourcegroups/InternalResourceGroup.java
+++ core/trino-main/src/main/java/io/trino/execution/resourcegroups/InternalResourceGroup.java
@@ -32,7 +32,6 @@ import org.weakref.jmx.Managed;
 import org.weakref.jmx.Nested;
 
 import java.time.Duration;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -317,6 +316,22 @@ public class InternalResourceGroup
             }
 
             return waitingQueuedQueries;
+        }
+    }
+
+    public int getQueriesQueuedOnInternal()
+    {
+        synchronized (root) {
+            if (subGroups.isEmpty()) {
+                return min(getQueuedQueries(), softConcurrencyLimit - getRunningQueries());
+            }
+
+            int queriesQueuedInternal = 0;
+            for (InternalResourceGroup subGroup : subGroups.values()) {
+                queriesQueuedInternal += subGroup.getQueriesQueuedOnInternal();
+            }
+
+            return queriesQueuedInternal;
         }
     }
 
@@ -993,13 +1008,6 @@ public class InternalResourceGroup
                 hardConcurrencyLimit = Math.max(1, hardConcurrencyLimit);
             }
             return runningQueries.size() + descendantRunningQueries < hardConcurrencyLimit;
-        }
-    }
-
-    public Collection<InternalResourceGroup> subGroups()
-    {
-        synchronized (root) {
-            return subGroups.values();
         }
     }
 
