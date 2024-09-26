d70da89d090ea44a9012eacbcf017561d6c37c6e
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-09-26 12:33:27 +0200

    Rename principal to user in ClientSession
    This matches the naming in other parts of the code
    
    (cherry picked from commit 517dc7f87bc751b1726437573bb4ad69781e8eb0)
    (cherry picked from commit edc3d0f166d00452aa50bb01a8f1a67016971e48)

--- client/trino-client/src/main/java/io/trino/client/uri/TrinoUri.java
+++ client/trino-client/src/main/java/io/trino/client/uri/TrinoUri.java
@@ -496,7 +496,7 @@ public class TrinoUri
     {
         return ClientSession.builder()
                 .server(getHttpUri())
-                .principal(getUser())
+                .user(getUser())
                 .path(getPath().orElse(ImmutableList.of()))
                 .clientRequestTimeout(getTimeout())
                 .sessionUser(getSessionUser())
