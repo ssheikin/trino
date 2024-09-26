2f0ec4c84e6f25fbeb11511558e828b417728b6a
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-09-26 12:31:40 +0200

    Rename user to sessionUser in ClientSession
    (cherry picked from commit 1e6ba0551071125211ee8f7ceb5f8a1f6069fa75)
    (cherry picked from commit 827c21e3b3ce93709b2a374910682c6adebacbbe)

--- client/trino-client/src/main/java/io/trino/client/uri/TrinoUri.java
+++ client/trino-client/src/main/java/io/trino/client/uri/TrinoUri.java
@@ -499,7 +499,7 @@ public class TrinoUri
                 .principal(getUser())
                 .path(getPath().orElse(ImmutableList.of()))
                 .clientRequestTimeout(getTimeout())
-                .user(getSessionUser())
+                .sessionUser(getSessionUser())
                 .clientTags(getClientTags().orElse(ImmutableSet.of()))
                 .source(getSource().orElse(null))
                 .traceToken(getTraceToken())
