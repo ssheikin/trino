34ee6558f25289413b6797e9a1c011e10dea91e3
Jianjian <jja725@gmail.com>
2024-09-30 22:23:47 -0700

    Extend wait time of Alluxio master in tests
    (cherry picked from commit ed04b0115dbfca8216065fa1f7cc4b6d18602944)
    (cherry picked from commit a7d7341fcb06d7b2045301b4212515ae19b43e46)

--- lib/trino-filesystem-alluxio/src/test/java/io/trino/filesystem/alluxio/TestAlluxioFileSystem.java
+++ lib/trino-filesystem-alluxio/src/test/java/io/trino/filesystem/alluxio/TestAlluxioFileSystem.java
@@ -14,16 +14,15 @@
 package io.trino.filesystem.alluxio;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.time.Duration;
 
 @Testcontainers
 public class TestAlluxioFileSystem
@@ -52,7 +51,9 @@ public class TestAlluxioFileSystem
                                 + "-Dalluxio.security.authorization.plugins.enabled=false ")
                 .withNetworkMode("host")
                 .withAccessToHost(true)
-                .waitingFor(Wait.forLogMessage(".*Primary started*\n", 1));
+                .waitingFor((new LogMessageWaitStrategy())
+                        .withRegEx(".*Primary started*\n")
+                        .withStartupTimeout(Duration.ofSeconds(180L)));
         return container;
     }
 
@@ -84,12 +85,5 @@ public class TestAlluxioFileSystem
         // the SSHD container will be stopped by TestContainers on shutdown
         // https://github.com/trinodb/trino/discussions/21969
         System.setProperty("ReportLeakedContainers.disabled", "true");
-    }
-
-    @Test
-    void testContainer()
-    {
-        assertThat(ALLUXIO_MASTER_CONTAINER.isRunning()).isTrue();
-        assertThat(ALLUXIO_WORKER_CONTAINER.isRunning()).isTrue();
     }
 }
