f9a2fdef3029ba7e3b7f638cb1462ff569823b8a
Łukasz Osipiuk <lukasz@osipiuk.net>
2024-10-04 17:19:43 +0200

    👷 Use OSS plan tests
    (cherry picked from commit 39f8fcb9f1cc32296fe0d15c6afba7cdf9a60074)

--- testing/trino-tests/src/test/java/io/trino/sql/planner/UpdateExpectedPlans.java
+++ testing/trino-tests/src/test/java/io/trino/sql/planner/UpdateExpectedPlans.java
@@ -35,17 +35,8 @@ public final class UpdateExpectedPlans
         List<Future<Void>> futures = ForkJoinPool.commonPool().invokeAll(
                 ImmutableList.<Callable<Void>>builder()
                         // in alphabetical order
-                        .add(runMain(TestCachingHiveTpcdsCostBasedPlan.class, noArgs))
-                        .add(runMain(TestCachingHiveTpchCostBasedPlan.class, noArgs))
-                        .add(runMain(TestHivePartitionedTpcdsCostBasedPlan.class, noArgs))
-                        .add(runMain(TestHivePartitionedTpchCostBasedPlan.class, noArgs))
-                        .add(runMain(TestHiveTpcdsCostBasedPlan.class, noArgs))
-                        .add(runMain(TestHiveTpchCostBasedPlan.class, noArgs))
-                        .add(runMain(TestIcebergPartitionedTpcdsCostBasedPlan.class, noArgs))
-                        .add(runMain(TestIcebergPartitionedTpchCostBasedPlan.class, noArgs))
-                        .add(runMain(TestIcebergTpcdsCostBasedPlan.class, noArgs))
-                        .add(runMain(TestIcebergTpchCostBasedPlan.class, noArgs))
-                        .add(runMain(TestIcebergSmallFilesTpcdsCostBasedPlan.class, noArgs))
+                        .add(runMain(TestPartitionedTpcdsCostBasedPlan.class, noArgs))
+                        .add(runMain(TestTpchCostBasedPlan.class, noArgs))
                         .build());
 
         for (Future<Void> future : futures) {
