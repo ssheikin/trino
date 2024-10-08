49dbe6bd72c35b93f4c4eed147b4df389f1b2e6f
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-10-08 12:15:46 +0200

    Return negotiated encoding to the client
    (cherry picked from commit 3882fb6c5d04f6e31444106de1aca9fc7184fff3)
    (cherry picked from commit afb1d17bc6b7b08b449dbfc3f5d6cea9aedd9cce)

--- core/trino-main/src/main/java/io/trino/server/protocol/ExecutingStatementResource.java
+++ core/trino-main/src/main/java/io/trino/server/protocol/ExecutingStatementResource.java
@@ -63,6 +63,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static io.airlift.concurrent.Threads.threadsNamed;
 import static io.airlift.units.DataSize.Unit.MEGABYTE;
+import static io.trino.client.ProtocolHeaders.TRINO_HEADERS;
 import static io.trino.server.DisconnectionAwareAsyncResponse.bindDisconnectionAwareAsyncResponse;
 import static io.trino.server.protocol.Slug.Context.EXECUTING_QUERY;
 import static io.trino.server.security.ResourceSecurity.AccessType.PUBLIC;
@@ -238,12 +239,13 @@ public class ExecutingStatementResource
         }
         ListenableFuture<QueryResultsResponse> queryResultsFuture = query.waitForResults(token, externalUriInfo, wait, targetResultSize);
 
-        ListenableFuture<Response> response = Futures.transform(queryResultsFuture, this::toResponse, directExecutor());
+        ListenableFuture<Response> response = Futures.transform(queryResultsFuture, results ->
+                toResponse(results, query.getQueryInfo().getSession().getQueryDataEncoding()), directExecutor());
 
         bindDisconnectionAwareAsyncResponse(asyncResponse, response, responseExecutor);
     }
 
-    private Response toResponse(QueryResultsResponse resultsResponse)
+    private Response toResponse(QueryResultsResponse resultsResponse, Optional<String> queryDataEncoding)
     {
         ResponseBuilder response = Response.ok(resultsResponse.queryResults());
 
@@ -292,6 +294,9 @@ public class ExecutingStatementResource
         if (!compressionEnabled) {
             response.encoding("identity");
         }
+
+        queryDataEncoding
+                .ifPresent(encoding -> response.header(TRINO_HEADERS.responseQueryDataEncoding(), encoding));
 
         return response.build();
     }
