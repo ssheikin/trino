2c375769f4f02c88a812c45d6b79f6676ce85ae1
Yuya Ebihara <ebyhry@gmail.com>
2024-10-23 18:11:27 +0900

    Replace `_` with `-` in config property names
    (cherry picked from commit 9660adfd525e5e6fc1a82169e973db220488dc79)
    (cherry picked from commit b62ca4dec6615d3d94bf9fa352917435c2b426d0)

--- core/trino-main/src/test/java/io/trino/memory/TestMemoryManagerConfig.java
+++ core/trino-main/src/test/java/io/trino/memory/TestMemoryManagerConfig.java
@@ -64,7 +64,7 @@ public class TestMemoryManagerConfig
                 .put("fault-tolerant-execution-task-memory-estimation-quantile", "0.7")
                 .put("fault-tolerant-execution-task-runtime-memory-estimation-overhead", "300MB")
                 .put("fault-tolerant-execution-memory-requirement-increase-on-worker-crash-enabled", "false")
-                .put("fault-tolerant-execution-eager-speculative-tasks-node_memory-overcommit", "21GB")
+                .put("fault-tolerant-execution-eager-speculative-tasks-node-memory-overcommit", "21GB")
                 .put("fault-tolerant-execution-task-split-memory-threshold", "6GB")
                 .put("fault-tolerant-execution-task-split-factor", "11")
                 .put("query.low-memory-killer.policy", "none")
