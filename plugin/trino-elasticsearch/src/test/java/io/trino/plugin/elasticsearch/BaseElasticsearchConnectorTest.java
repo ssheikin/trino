84fb31a8499d5bf11a9849f2d4d05a925fd91f88
striderarun <strider90arun@gmail.com>
2024-10-25 10:52:03 -0700

    Add test for column name containing special characters in Elasticsearch
    (cherry picked from commit c2cb00bc3f1647167f19e4d0ca7e2fcb43e49dc2)
    (cherry picked from commit 920ed8895af116b28b58dd78fb1ef96c88b33695)

--- plugin/trino-elasticsearch/src/test/java/io/trino/plugin/elasticsearch/BaseElasticsearchConnectorTest.java
+++ plugin/trino-elasticsearch/src/test/java/io/trino/plugin/elasticsearch/BaseElasticsearchConnectorTest.java
@@ -46,10 +46,12 @@ import static io.trino.plugin.elasticsearch.ElasticsearchQueryRunner.PASSWORD;
 import static io.trino.plugin.elasticsearch.ElasticsearchQueryRunner.TPCH_SCHEMA;
 import static io.trino.plugin.elasticsearch.ElasticsearchQueryRunner.TRUSTSTORE_PASSWORD;
 import static io.trino.plugin.elasticsearch.ElasticsearchQueryRunner.USER;
+import static io.trino.spi.StandardErrorCode.INVALID_COLUMN_REFERENCE;
 import static io.trino.spi.type.DoubleType.DOUBLE;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static io.trino.testing.MaterializedResult.resultBuilder;
 import static io.trino.testing.TestingNames.randomNameSuffix;
+import static io.trino.testing.assertions.TrinoExceptionAssert.assertTrinoExceptionThrownBy;
 import static java.lang.String.format;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.requireNonNull;
@@ -529,6 +531,23 @@ public abstract class BaseElasticsearchConnectorTest
 
         assertThat(query("SELECT name, fields.fielda, fields.fieldb FROM data"))
                 .matches("VALUES (VARCHAR 'nestfield', BIGINT '32', VARCHAR 'valueb')");
+    }
+
+    @Test
+    public void testFieldNameWithSpecialCharacters()
+            throws IOException
+    {
+        String index = "field_name_with_special_characters_" + randomNameSuffix();
+        index(index, ImmutableMap.<String, Object>builder()
+                .put("with$sign", 55)
+                .put("nested", ImmutableMap.<String, Object>builder()
+                        .put("with$sign", "few bucks")
+                        .buildOrThrow())
+                .buildOrThrow());
+
+        assertThat(query("SELECT \"with$sign\", nested.\"with$sign\" FROM " + index))
+                .skippingTypesCheck()
+                .matches("VALUES (CAST(55 AS BIGINT), 'few bucks')");
     }
 
     @Test
@@ -1236,7 +1255,23 @@ public abstract class BaseElasticsearchConnectorTest
             throws IOException
     {
         String indexName = "nested_variants";
+        @Language("JSON")
+        String properties =
+                """
+                {
+                    "properties": {
+                        "a": {
+                            "properties": {
+                                "b.c": {
+                                    "type": "text"
+                                }
+                            }
+                        }
+                    }
+                }
+                """;
 
+        createIndex(indexName, properties);
         index(indexName,
                 ImmutableMap.of("a",
                         ImmutableMap.of("b",
@@ -1258,6 +1293,10 @@ public abstract class BaseElasticsearchConnectorTest
 
         assertThat(query("SELECT a.b.c FROM nested_variants"))
                 .matches("VALUES VARCHAR 'value1', VARCHAR 'value2', VARCHAR 'value3', VARCHAR 'value4'");
+
+        assertTrinoExceptionThrownBy(() -> computeActual("SELECT a.\"b.c\" FROM nested_variants"))
+                .hasErrorCode(INVALID_COLUMN_REFERENCE)
+                .hasMessageContaining("Column reference 'a.b.c' is invalid");
     }
 
     @Test
