4aac717ea7316e81f3b3711e3c17225afb5f1829
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-10-29 16:09:25 +0100

    Move SpoolingConfig out of SpoolingManager and add new config
    (cherry picked from commit d68ab218ec1f92da8309b67acc46f6fa4944eae7)
    (cherry picked from commit fd228ef98c55c8fe5cfae23eee78b19be30f50ff)

--- core/trino-main/src/main/java/io/trino/testing/PlanTester.java
+++ core/trino-main/src/main/java/io/trino/testing/PlanTester.java
@@ -814,6 +814,7 @@ public class PlanTester
                 new GenericSpillerFactory(unsupportedSingleStreamSpillerFactory()),
                 new QueryDataEncoders(Set.of()),
                 Optional.empty(),
+                Optional.empty(),
                 unsupportedSingleStreamSpillerFactory(),
                 unsupportedPartitioningSpillerFactory(),
                 new PagesIndex.TestingFactory(false),
