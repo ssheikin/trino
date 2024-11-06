85d59fe0022735c65839f429348a24e74252ea65
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-11-06 18:21:02 +0100

    Fix Iceberg exception assertion
    (cherry picked from commit 48df0900e475fe0aeecaa9559a89f6451dbcf534)
    (cherry picked from commit 96a32b8065028f2f50debcbec5e88b89945e04f5)

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/glue/TestIcebergGlueTableOperationsInsertFailure.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/glue/TestIcebergGlueTableOperationsInsertFailure.java
@@ -134,7 +134,7 @@ public class TestIcebergGlueTableOperationsInsertFailure
                     assertThat(throwable.getCause()).isInstanceOf(Failure.class);
                     Failure failure = (Failure) throwable.getCause();
                     assertThat(failure.getMessage()).contains("Test-simulated Glue timeout exception");
-                    assertThat(failure.getFailureInfo().getType()).isEqualTo("org.apache.iceberg.exceptions.CommitStateUnknownException");
+                    assertThat(failure.getFailureInfo().getType()).isEqualTo("io.trino.spi.TrinoException");
                 });
         assertQuery("SELECT * FROM " + tableName, "VALUES 'Trino', 'rocks'");
     }
