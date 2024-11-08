6cdb7e5ee33db6f26aee163a3399c4ff5119482a
Kobi Luz <kobi.luz@starburstdata.com>
2024-11-08 09:07:47 +0100

    [WARP-SPEED] SIC-2711 add protection for warp up elements parameters in match open
    (cherry picked from commit c8025ce7e9ee1d978a93b51e9a3970fd9739fecb)

--- plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/warp/storage/read/MatchService.java
+++ plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/warp/storage/read/MatchService.java
@@ -19,6 +19,8 @@ import io.airlift.log.Logger;
 import io.trino.plugin.warp.config.GlobalConfig;
 import io.trino.plugin.warp.dispatcher.DispatcherPageSourceFactory;
 import io.trino.plugin.warp.dispatcher.model.WarmUpElement;
+import io.trino.plugin.warp.gen.constants.RecTypeCode;
+import io.trino.plugin.warp.gen.constants.WarmUpType;
 import io.trino.plugin.warp.gen.stats.DispatcherPageSourceStats;
 import io.trino.plugin.warp.gen.stats.LucenePageCacheStats;
 import io.trino.plugin.warp.juffer.BufferAllocator;
@@ -134,15 +136,23 @@ public class MatchService
             Optional<MemorySegment> luceneBitmaps = Optional.empty();
             final int luceneBitmapSizePerWE = storageEngineConstants.getPageSize() * queryArgs.numChunksInRange();
             try {
-                SequenceLayout warmUpElementAttsLayout = MemoryLayout.sequenceLayout(queryParams.getNumMatchElements(), WarmUpElement.WARM_UP_ELEMENT_ATT_LAYOUT);
-                MemorySegment warmUpElementAtts = Arena.ofAuto().allocate(warmUpElementAttsLayout.byteSize(), ValueLayout.JAVA_BYTE.byteSize());
+                // we llocate one more and fill it with invalid values for safety check that storage layer does not continue over the boundaries of the array
+                SequenceLayout warmUpElementAttsLayout = MemoryLayout.sequenceLayout(queryParams.getNumMatchElements() + 1, WarmUpElement.WARM_UP_ELEMENT_ATT_LAYOUT);
+                MemorySegment warmUpElementAtts = Arena.ofAuto().allocate(warmUpElementAttsLayout.byteSize(), ValueLayout.JAVA_SHORT.byteSize());
                 Iterator<WarmupElementMatchParams> matchParamsListItr = queryParams.getMatchElementsParamsList().iterator();
                 warmUpElementAtts.elements(WarmUpElement.WARM_UP_ELEMENT_ATT_LAYOUT)
                         .forEach(warmupElementAtt -> {
-                            WarmupElementMatchParams matchParams = matchParamsListItr.next();
-                            WarmUpElement.setRecTypeCode(warmupElementAtt, matchParams.getRecTypeCode());
-                            WarmUpElement.setRecTypeLength(warmupElementAtt, matchParams.getRecTypeLength());
-                            WarmUpElement.setWarmUpType(warmupElementAtt, matchParams.getWarmUpType());
+                            if (matchParamsListItr.hasNext()) {
+                                WarmupElementMatchParams matchParams = matchParamsListItr.next();
+                                WarmUpElement.setRecTypeCode(warmupElementAtt, matchParams.getRecTypeCode());
+                                WarmUpElement.setRecTypeLength(warmupElementAtt, matchParams.getRecTypeLength());
+                                WarmUpElement.setWarmUpType(warmupElementAtt, matchParams.getWarmUpType());
+                            }
+                            else {
+                                WarmUpElement.setRecTypeCode(warmupElementAtt, RecTypeCode.REC_TYPE_INVALID);
+                                WarmUpElement.setRecTypeLength(warmupElementAtt, 0);
+                                WarmUpElement.setWarmUpType(warmupElementAtt, WarmUpType.WARM_UP_TYPE_NUM_OF);
+                            }
                         });
 
                 if (queryParams.getNumLucene() > 0) {
