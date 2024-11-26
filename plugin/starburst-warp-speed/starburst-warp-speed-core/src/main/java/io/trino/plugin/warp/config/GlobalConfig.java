b381bc80ed20953a04f34cdf5cf08d43ed516037
LandauAharon <landauaharon@gmail.com>
2024-11-26 11:09:24 -0500

    [WARP-SPEED] enable varchar mapped match collect by default
    (cherry picked from commit 6b73f84afd75f124e197b5916fd81660f71498ed)

--- plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/warp/config/GlobalConfig.java
+++ plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/warp/config/GlobalConfig.java
@@ -70,7 +70,7 @@ public class GlobalConfig
     private boolean enableExportAppendOnCloud = true;
     private boolean enableMatchCollect = true;
     private boolean enableMappedMatchCollect = true;
-    private boolean enableVarcharMappedMatchCollect;
+    private boolean enableVarcharMappedMatchCollect = true;
     private boolean enableOrPushdown = true;
     private boolean enableRangeFilter = true;
     private boolean enableInverseWithNulls;
