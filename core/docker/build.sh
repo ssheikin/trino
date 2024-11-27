ccc8acf9e92d18035a777ebfe30b9b737eb98880
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2024-11-27 11:04:32 +0100

    Prune launcher binaries for other archs
    (cherry picked from commit 563524e8b6dd7f58012f6a639da793ae721aea25)
    (cherry picked from commit f0e4ebe87471b61b2ce68e12ecb58529dc813824)

--- core/docker/build.sh
+++ core/docker/build.sh
@@ -123,6 +123,7 @@ for arch in "${ARCHITECTURES[@]}"; do
         "${WORK_DIR}" \
         --progress=plain \
         --pull \
+        --build-arg ARCH="${arch}" \
         --build-arg JDK_VERSION="${JDK_RELEASE}" \
         --build-arg JDK_DOWNLOAD_LINK="$(jdk_download_link "${JDKS_PATH}/${JDK_RELEASE}" "${arch}")" \
         --platform "linux/$arch" \
