7a71d8ecf9bac560f586125653c4ea446f09433e
Yuya Ebihara <ebyhry@gmail.com>
2024-11-28 10:40:56 +0900

    Add $transactions system table to Delta Lake
    (cherry picked from commit 62d77f73037cba4c8abc259f78ad765a43e97a43)
    (cherry picked from commit 4978979b7f120a2f874a7506da993c23c4cbeef7)

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/DeltaLakeMetadata.java
@@ -4116,6 +4116,12 @@ public class DeltaLakeMetadata
                     fileSystemFactory,
                     transactionLogAccess,
                     typeManager));
+            case TRANSACTIONS -> Optional.of(new DeltaLakeTransactionsTable(
+                    systemTableName,
+                    tableLocation,
+                    fileSystemFactory,
+                    transactionLogAccess,
+                    typeManager));
             case PROPERTIES -> Optional.of(new DeltaLakePropertiesTable(systemTableName, tableLocation, transactionLogAccess));
             case PARTITIONS -> Optional.of(new DeltaLakePartitionsTable(session, systemTableName, tableLocation, transactionLogAccess, typeManager));
         };
