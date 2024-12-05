d7e28f03fda53ca8e3e32349457b8f7c92944f99
Kobi Luz <kobi.luz@starburstdata.com>
2024-12-05 10:42:27 +0200

    Revert "[WARP-SPEED] pass debug log to all workers for readability of worker logs"
    This reverts commit e71bde7a91d837350edb4f17b012c263952b238e.
    
    (cherry picked from commit 7e5adcd5962901abe31954fc3e9613d9b37a765b)

--- plugin/starburst-warp-speed/starburst-warp-speed-extensions/src/main/java/io/trino/plugin/warp/extension/execution/debugtools/DebugLogTask.java
+++ plugin/starburst-warp-speed/starburst-warp-speed-extensions/src/main/java/io/trino/plugin/warp/extension/execution/debugtools/DebugLogTask.java
@@ -14,27 +14,16 @@
 package io.trino.plugin.warp.extension.execution.debugtools;
 
 import com.google.inject.Inject;
-import io.airlift.http.client.HttpUriBuilder;
-import io.airlift.http.client.Request;
-import io.airlift.json.JsonCodec;
 import io.airlift.log.Logger;
-import io.trino.plugin.warp.execution.WarpClient;
 import io.trino.plugin.warp.extension.execution.TaskResource;
 import io.trino.plugin.warp.extension.execution.TaskResourceMarker;
-import io.trino.plugin.warp.util.UriUtils;
-import io.trino.spi.Node;
-import io.trino.spi.NodeManager;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import static io.airlift.http.client.FullJsonResponseHandler.createFullJsonResponseHandler;
-import static io.airlift.http.client.JsonBodyGenerator.jsonBodyGenerator;
-import static io.airlift.http.client.Request.Builder.preparePost;
 import static io.trino.plugin.warp.extension.execution.debugtools.DebugLogTask.TASK_NAME;
-import static java.util.Objects.requireNonNull;
 
 @TaskResourceMarker(shouldCheckExecutionAllowed = false)
 @Path(TASK_NAME)
@@ -45,43 +34,15 @@ public class DebugLogTask
         implements TaskResource
 {
     public static final String TASK_NAME = "debug-log";
-
     private static final Logger logger = Logger.get(DebugLogTask.class);
 
-    private static final JsonCodec<DebugLogData> jsonCodec = JsonCodec.jsonCodec(DebugLogData.class);
-
-    private final NodeManager nodeManager;
-    private final WarpClient warpClient;
-
     @Inject
-    public DebugLogTask(
-            NodeManager nodeManager,
-            WarpClient warpClient)
-    {
-        this.nodeManager = requireNonNull(nodeManager);
-        this.warpClient = requireNonNull(warpClient);
-    }
+    public DebugLogTask() {}
 
     @POST
-//@ApiOperation(value = "debug-log", extensions = {@Extension(properties = @ExtensionProperty(name = "exposing-level", value = "DEBUG"))})
+    //@ApiOperation(value = "debug-log", extensions = {@Extension(properties = @ExtensionProperty(name = "exposing-level", value = "DEBUG"))})
     public void debugLog(DebugLogData debugLogData)
     {
         logger.info(debugLogData.getLogLine());
-        if (nodeManager.getCurrentNode().isCoordinator()) {
-            nodeManager.getWorkerNodes()
-                    .stream()
-                    .filter(Node::isCoordinator) // in case of single node cluster
-                    .forEach(node -> {
-                        HttpUriBuilder uriBuilder = warpClient.getRestEndpoint(UriUtils.getHttpUri(node));
-                        uriBuilder.appendPath(DebugLogTask.TASK_NAME);
-
-                        Request request = preparePost()
-                                .setUri(uriBuilder.build())
-                                .setBodyGenerator(jsonBodyGenerator(jsonCodec, debugLogData))
-                                .setHeader("Content-Type", "application/json")
-                                .build();
-                        warpClient.sendWithRetry(request, createFullJsonResponseHandler(JsonCodec.jsonCodec(Void.class)));
-                    });
-        }
     }
 }
