dbb98379054fbb89aef5964d7f96658848edea74
kasiafi <30203062+kasiafi@users.noreply.github.com>
2024-12-10 13:59:04 +0100

    New IR -- abstractions and dialects
    (cherry picked from commit 180a82d8c39faf21c5c334b4275535fb0b20268d)

---
+++ core/trino-main/src/main/java/io/trino/sql/dialect/trino/operation/ExplainAnalyze.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.sql.dialect.trino.operation;
+
+import com.google.common.collect.ImmutableList;
+import io.trino.spi.TrinoException;
+import io.trino.spi.type.MultisetType;
+import io.trino.spi.type.RowType;
+import io.trino.sql.newir.Block;
+import io.trino.sql.newir.FormatOptions;
+import io.trino.sql.newir.Operation;
+import io.trino.sql.newir.Region;
+import io.trino.sql.newir.Value;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import static io.trino.spi.StandardErrorCode.IR_ERROR;
+import static io.trino.spi.type.EmptyRowType.EMPTY_ROW;
+import static io.trino.spi.type.RowType.anonymousRow;
+import static io.trino.spi.type.VarcharType.VARCHAR;
+import static io.trino.sql.dialect.trino.Attributes.VERBOSE;
+import static io.trino.sql.dialect.trino.RelationalProgramBuilder.relationRowType;
+import static io.trino.sql.dialect.trino.TrinoDialect.TRINO;
+import static io.trino.sql.dialect.trino.TrinoDialect.irType;
+import static io.trino.sql.dialect.trino.TrinoDialect.trinoType;
+import static io.trino.sql.dialect.trino.TypeConstraint.IS_RELATION;
+import static io.trino.sql.newir.Region.singleBlockRegion;
+import static java.util.Objects.requireNonNull;
+
+public class ExplainAnalyze
+        extends Operation
+{
+    private static final String NAME = "explain_analyze";
+
+    private final Result result;
+    private final Value input;
+    private final Region fieldSelector;
+    private final Map<AttributeKey, Object> attributes;
+
+    public ExplainAnalyze(String resultName, Value input, Block fieldSelector, boolean verbose, Map<AttributeKey, Object> sourceAttributes)
+    {
+        super(TRINO, NAME);
+        requireNonNull(resultName, "resultName is null");
+        requireNonNull(input, "input is null");
+        requireNonNull(fieldSelector, "fieldSelector is null");
+        requireNonNull(sourceAttributes, "sourceAttributes is null");
+
+        if (!IS_RELATION.test(trinoType(input.type()))) {
+            throw new TrinoException(IR_ERROR, "input to the ExplainAnalyze operation must be of relation type");
+        }
+        this.input = input;
+
+        this.result = new Result(resultName, irType(new MultisetType(anonymousRow(VARCHAR))));
+
+        if (fieldSelector.parameters().size() != 1 ||
+                !trinoType(fieldSelector.parameters().getFirst().type()).equals(relationRowType(trinoType(input.type()))) ||
+                !(trinoType(fieldSelector.getReturnedType()) instanceof RowType || trinoType(fieldSelector.getReturnedType()).equals(EMPTY_ROW))) {
+            throw new TrinoException(IR_ERROR, "invalid field selection for ExplainAnalyze operation");
+        }
+        this.fieldSelector = singleBlockRegion(fieldSelector);
+
+        this.attributes = VERBOSE.asMap(verbose);
+    }
+
+    @Override
+    public Result result()
+    {
+        return result;
+    }
+
+    @Override
+    public List<Value> arguments()
+    {
+        return ImmutableList.of(input);
+    }
+
+    @Override
+    public List<Region> regions()
+    {
+        return ImmutableList.of(fieldSelector);
+    }
+
+    @Override
+    public Map<AttributeKey, Object> attributes()
+    {
+        return attributes;
+    }
+
+    @Override
+    public String prettyPrint(int indentLevel, FormatOptions formatOptions)
+    {
+        return "pretty explain analyze";
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (ExplainAnalyze) obj;
+        return Objects.equals(this.result, that.result) &&
+                Objects.equals(this.input, that.input) &&
+                Objects.equals(this.fieldSelector, that.fieldSelector) &&
+                Objects.equals(this.attributes, that.attributes);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(result, input, fieldSelector, attributes);
+    }
+}
