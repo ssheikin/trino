1cbe866f89c669f746a1fd6acb1998249a35d3bb
David Phillips <david@acz.org>
2025-01-01 18:07:50 -0800

    Remove optional binder for IcebergPageSourceProviderFactory
    (cherry picked from commit ffe42ed032de51533ffd400b50b6af99a3556860)
    (cherry picked from commit 2e974098e45e14815f556dedfbb0d9ec2b502fc1)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergModule.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergModule.java
@@ -109,7 +109,7 @@ public class IcebergModule
         binder.bind(ConnectorSplitManager.class).annotatedWith(ForDecorator.class).to(IcebergSplitManager.class).in(Scopes.SINGLETON);
         binder.bind(ConnectorSplitManager.class).annotatedWith(ForClassLoaderSafe.class).to(DecoratingConnectorSplitManager.class).in(Scopes.SINGLETON);
         binder.bind(ConnectorSplitManager.class).to(ClassLoaderSafeConnectorSplitManager.class).in(Scopes.SINGLETON);
-        newOptionalBinder(binder, Key.get(ConnectorPageSourceProviderFactory.class, ForClassLoaderSafe.class)).setDefault().to(IcebergPageSourceProviderFactory.class).in(Scopes.SINGLETON);
+        binder.bind(ConnectorPageSourceProviderFactory.class).annotatedWith(ForClassLoaderSafe.class).to(IcebergPageSourceProviderFactory.class).in(Scopes.SINGLETON);
         binder.bind(IcebergPageSourceProviderFactory.class).in(Scopes.SINGLETON);
         binder.bind(ConnectorPageSourceProviderFactory.class).to(ClassLoaderSafeConnectorPageSourceProviderFactory.class).in(Scopes.SINGLETON);
         binder.bind(ConnectorPageSinkProvider.class).annotatedWith(ForClassLoaderSafe.class).to(IcebergPageSinkProvider.class).in(Scopes.SINGLETON);
