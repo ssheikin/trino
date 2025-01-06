f577f18b54c4f289786b93de582f1ad4f0c85b06
Mayank Vadariya <mayank.vadariya@starburstdata.com>
2025-01-06 13:13:16 -0500

    Bump DynamoDB docker image version
    (cherry picked from commit fbefb2fb6bf08e7a2c7ac93321e14012fe1331b6)

--- plugin/starburst-dynamodb/src/test/java/com/starburstdata/trino/plugin/dynamodb/TestingDynamoDbServer.java
+++ plugin/starburst-dynamodb/src/test/java/com/starburstdata/trino/plugin/dynamodb/TestingDynamoDbServer.java
@@ -33,7 +33,7 @@ public class TestingDynamoDbServer
 
     public TestingDynamoDbServer()
     {
-        dockerContainer = new GenericContainer<>("amazon/dynamodb-local:2.1.0")
+        dockerContainer = new GenericContainer<>("amazon/dynamodb-local:2.5.4")
                 .withExposedPorts(PORT)
                 .waitingFor(Wait.forLogMessage(".*Initializing DynamoDB Local with the following configuration.*", 1)
                         .withStartupTimeout(Duration.ofMinutes(5)));
