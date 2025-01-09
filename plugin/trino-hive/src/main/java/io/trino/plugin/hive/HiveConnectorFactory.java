98c4fbc7c16c6512ff5715b5040c51dfd6ee08f2
Piotr Rzysko <piotr.rzysko@starburstdata.com>
2025-01-09 10:11:13 +0100

    Expose security-sensitive properties for Hive connector

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveConnectorFactory.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveConnectorFactory.java
@@ -20,6 +20,7 @@ import com.google.inject.Scopes;
 import com.google.inject.TypeLiteral;
 import io.airlift.bootstrap.Bootstrap;
 import io.airlift.bootstrap.LifeCycleManager;
+import io.airlift.configuration.ConfigPropertyMetadata;
 import io.airlift.json.JsonModule;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
@@ -36,6 +37,7 @@ import io.trino.plugin.base.classloader.ClassLoaderSafeConnectorSplitManager;
 import io.trino.plugin.base.classloader.ClassLoaderSafeEventListener;
 import io.trino.plugin.base.classloader.ClassLoaderSafeNodePartitioningProvider;
 import io.trino.plugin.base.classloader.ClassLoaderSafeSystemTable;
+import io.trino.plugin.base.config.ConfigUtils;
 import io.trino.plugin.base.jmx.ConnectorObjectNameGeneratorModule;
 import io.trino.plugin.base.jmx.MBeanServerModule;
 import io.trino.plugin.base.session.SessionPropertiesProvider;
@@ -81,6 +83,11 @@ import static io.trino.plugin.base.Versions.checkStrictSpiVersionMatch;
 public class HiveConnectorFactory
         implements ConnectorFactory
 {
+    private static final Module DEFAULT_ADDITIONAL_MODULE = EMPTY_MODULE;
+    private static final Optional<HiveMetastore> DEFAULT_METASTORE = Optional.empty();
+    private static final Optional<TrinoFileSystemFactory> DEFAULT_FILESYSTEM_FACTORY = Optional.empty();
+    private static final Optional<DirectoryLister> DEFAULT_DIRECTORY_LISTENER = Optional.empty();
+
     @Override
     public String getName()
     {
@@ -91,7 +98,20 @@ public class HiveConnectorFactory
     public Connector create(String catalogName, Map<String, String> config, ConnectorContext context)
     {
         checkStrictSpiVersionMatch(context, this);
-        return createConnector(catalogName, config, context, EMPTY_MODULE, Optional.empty(), Optional.empty(), Optional.empty());
+        return createConnector(catalogName, config, context, DEFAULT_ADDITIONAL_MODULE, DEFAULT_METASTORE, DEFAULT_FILESYSTEM_FACTORY, DEFAULT_DIRECTORY_LISTENER);
+    }
+
+    @Override
+    public Set<String> getSecuritySensitivePropertyNames(String catalogName, Map<String, String> config, ConnectorContext context)
+    {
+        ClassLoader classLoader = HiveConnectorFactory.class.getClassLoader();
+        try (ThreadContextClassLoader _ = new ThreadContextClassLoader(classLoader)) {
+            Bootstrap app = createBootstrap(catalogName, config, context, DEFAULT_ADDITIONAL_MODULE, DEFAULT_METASTORE, DEFAULT_FILESYSTEM_FACTORY, DEFAULT_DIRECTORY_LISTENER, true);
+
+            Set<ConfigPropertyMetadata> usedProperties = app.configure();
+
+            return ConfigUtils.getSecuritySensitivePropertyNames(config, usedProperties);
+        }
     }
 
     public static Connector createConnector(
@@ -105,41 +125,9 @@ public class HiveConnectorFactory
     {
         ClassLoader classLoader = HiveConnectorFactory.class.getClassLoader();
         try (ThreadContextClassLoader _ = new ThreadContextClassLoader(classLoader)) {
-            Bootstrap app = new Bootstrap(
-                    new CatalogNameModule(catalogName),
-                    new MBeanModule(),
-                    new ConnectorObjectNameGeneratorModule("io.trino.plugin.hive", "trino.plugin.hive"),
-                    new JsonModule(),
-                    new TypeDeserializerModule(context.getTypeManager()),
-                    new HiveModule(),
-                    new CachingDirectoryListerModule(directoryLister),
-                    new HiveMetastoreModule(metastore),
-                    new HiveSecurityModule(),
-                    fileSystemFactory
-                            .map(factory -> (Module) binder -> binder.bind(TrinoFileSystemFactory.class).toInstance(factory))
-                            .orElseGet(() -> new FileSystemModule(catalogName, context.getNodeManager(), context.getOpenTelemetry(), false, false)),
-                    new HiveProcedureModule(),
-                    new MBeanServerModule(),
-                    binder -> {
-                        binder.bind(OpenTelemetry.class).toInstance(context.getOpenTelemetry());
-                        binder.bind(Tracer.class).toInstance(context.getTracer());
-                        binder.bind(NodeVersion.class).toInstance(new NodeVersion(context.getNodeManager().getCurrentNode().getVersion()));
-                        binder.bind(NodeManager.class).toInstance(context.getNodeManager());
-                        binder.bind(VersionEmbedder.class).toInstance(context.getVersionEmbedder());
-                        binder.bind(MetadataProvider.class).toInstance(context.getMetadataProvider());
-                        binder.bind(PageIndexerFactory.class).toInstance(context.getPageIndexerFactory());
-                        binder.bind(PageSorter.class).toInstance(context.getPageSorter());
-                        binder.bind(CatalogName.class).toInstance(new CatalogName(catalogName));
-                        binder.bind(LocationAccessControl.class).toInstance(context.getLocationAccessControl());
-                    },
-                    binder -> newSetBinder(binder, EventListener.class),
-                    binder -> newSetBinder(binder, SessionPropertiesProvider.class).addBinding().to(HiveSessionProperties.class).in(Scopes.SINGLETON),
-                    module);
+            Bootstrap app = createBootstrap(catalogName, config, context, module, metastore, fileSystemFactory, directoryLister, false);
 
-            Injector injector = app
-                    .doNotInitializeLogging()
-                    .setRequiredConfigurationProperties(config)
-                    .initialize();
+            Injector injector = app.initialize();
 
             LifeCycleManager lifeCycleManager = injector.getInstance(LifeCycleManager.class);
             HiveTransactionManager transactionManager = injector.getInstance(HiveTransactionManager.class);
@@ -191,5 +179,56 @@ public class HiveConnectorFactory
                     injector.getInstance(HiveConfig.class).isSingleStatementWritesOnly(),
                     classLoader);
         }
+    }
+
+    private static Bootstrap createBootstrap(
+            String catalogName,
+            Map<String, String> config,
+            ConnectorContext context,
+            Module module,
+            Optional<HiveMetastore> metastore,
+            Optional<TrinoFileSystemFactory> fileSystemFactory,
+            Optional<DirectoryLister> directoryLister,
+            boolean quietBootstrap)
+    {
+        Bootstrap app = new Bootstrap(
+                new CatalogNameModule(catalogName),
+                new MBeanModule(),
+                new ConnectorObjectNameGeneratorModule("io.trino.plugin.hive", "trino.plugin.hive"),
+                new JsonModule(),
+                new TypeDeserializerModule(context.getTypeManager()),
+                new HiveModule(),
+                new CachingDirectoryListerModule(directoryLister),
+                new HiveMetastoreModule(metastore),
+                new HiveSecurityModule(),
+                fileSystemFactory
+                        .map(factory -> (Module) binder -> binder.bind(TrinoFileSystemFactory.class).toInstance(factory))
+                        .orElseGet(() -> new FileSystemModule(catalogName, context.getNodeManager(), context.getOpenTelemetry(), false, quietBootstrap)),
+                new HiveProcedureModule(),
+                new MBeanServerModule(),
+                binder -> {
+                    binder.bind(OpenTelemetry.class).toInstance(context.getOpenTelemetry());
+                    binder.bind(Tracer.class).toInstance(context.getTracer());
+                    binder.bind(NodeVersion.class).toInstance(new NodeVersion(context.getNodeManager().getCurrentNode().getVersion()));
+                    binder.bind(NodeManager.class).toInstance(context.getNodeManager());
+                    binder.bind(VersionEmbedder.class).toInstance(context.getVersionEmbedder());
+                    binder.bind(MetadataProvider.class).toInstance(context.getMetadataProvider());
+                    binder.bind(PageIndexerFactory.class).toInstance(context.getPageIndexerFactory());
+                    binder.bind(PageSorter.class).toInstance(context.getPageSorter());
+                    binder.bind(CatalogName.class).toInstance(new CatalogName(catalogName));
+                    binder.bind(LocationAccessControl.class).toInstance(context.getLocationAccessControl());
+                },
+                binder -> newSetBinder(binder, EventListener.class),
+                binder -> newSetBinder(binder, SessionPropertiesProvider.class).addBinding().to(HiveSessionProperties.class).in(Scopes.SINGLETON),
+                module);
+
+        if (quietBootstrap) {
+            app.quiet()
+                    .skipErrorReporting();
+        }
+
+        return app
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperties(config);
     }
 }
