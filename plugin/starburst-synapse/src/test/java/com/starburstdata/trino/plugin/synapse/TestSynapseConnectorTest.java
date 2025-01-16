cebdd9e2f3ab8ddb5fdd3b695c10c01dff1c1d0a
Mayank Vadariya <mayank.vadariya@starburstdata.com>
2025-01-16 14:20:22 -0500

    Add create/drop dynamic catalog tests for Synapse connector

--- plugin/starburst-synapse/src/test/java/com/starburstdata/trino/plugin/synapse/TestSynapseConnectorTest.java
+++ plugin/starburst-synapse/src/test/java/com/starburstdata/trino/plugin/synapse/TestSynapseConnectorTest.java
@@ -27,6 +27,7 @@ import io.trino.testing.QueryRunner;
 import io.trino.testing.TestingConnectorBehavior;
 import io.trino.testing.sql.SqlExecutor;
 import io.trino.testing.sql.TestTable;
+import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -36,7 +37,11 @@ import java.util.stream.Stream;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.MoreCollectors.toOptional;
+import static com.starburstdata.trino.plugin.synapse.SynapseQueryRunner.TEST_SCHEMA;
 import static com.starburstdata.trino.plugin.synapse.SynapseQueryRunner.createSynapseQueryRunner;
+import static com.starburstdata.trino.plugin.synapse.SynapseServer.JDBC_URL;
+import static com.starburstdata.trino.plugin.synapse.SynapseServer.PASSWORD;
+import static com.starburstdata.trino.plugin.synapse.SynapseServer.USERNAME;
 import static io.trino.plugin.jdbc.JdbcWriteSessionProperties.NON_TRANSACTIONAL_INSERT;
 import static io.trino.plugin.sqlserver.SqlServerSessionProperties.BULK_COPY_FOR_WRITE;
 import static io.trino.spi.connector.ConnectorMetadata.MODIFYING_ROWS_MESSAGE;
@@ -51,6 +56,7 @@ import static org.junit.jupiter.api.Assumptions.abort;
 public class TestSynapseConnectorTest
         extends BaseSqlServerConnectorTest
 {
+    private static final String CONNECTOR_NAME = "synapse";
     public static final String CATALOG = "sqlserver";
     private SynapseServer synapseServer;
 
@@ -100,6 +106,57 @@ public class TestSynapseConnectorTest
                         "col_default BIGINT DEFAULT 43," +
                         "col_nonnull_default BIGINT NOT NULL DEFAULT 42," +
                         "col_required2 BIGINT NOT NULL)");
+    }
+
+    @Test
+    void testCreateDropDynamicCatalog()
+    {
+        String catalog = "new_catalog_" + randomNameSuffix();
+        @Language("SQL")
+        String createCatalogSql = CREATE_CATALOG_SQL_TEMPLATE.formatted(catalog, CONNECTOR_NAME, PASSWORD, JDBC_URL, USERNAME);
+        assertUpdate(createCatalogSql);
+        assertCatalogs("system", "sqlserver", "tpch", "mock_dynamic_listing", "jmx", catalog);
+
+        assertUpdate("DROP CATALOG " + catalog);
+        assertCatalogs("system", "sqlserver", "tpch", "mock_dynamic_listing", "jmx");
+        // re-add the same catalog
+        assertUpdate(createCatalogSql);
+        assertCatalogs("system", "sqlserver", "tpch", "mock_dynamic_listing", "jmx", catalog);
+
+        assertUpdate("DROP CATALOG " + catalog);
+        assertCatalogs("system", "sqlserver", "tpch", "mock_dynamic_listing", "jmx");
+    }
+
+    @Test
+    void testCreateDropMultipleCatalogs()
+    {
+        String firstCatalog = "catalog1_" + randomNameSuffix();
+        String secondCatalog = "catalog2_" + randomNameSuffix();
+        try {
+            @Language("SQL")
+            String createFirstCatalogSql = CREATE_CATALOG_SQL_TEMPLATE.formatted(firstCatalog, CONNECTOR_NAME, PASSWORD, JDBC_URL, USERNAME);
+            assertUpdate(createFirstCatalogSql);
+            assertThat((String) computeActual("SHOW CREATE CATALOG " + firstCatalog).getOnlyValue())
+                    .isEqualTo(createFirstCatalogSql);
+            assertQuerySucceeds("SHOW TABLES FROM %s.%s".formatted(firstCatalog, TEST_SCHEMA));
+
+            @Language("SQL")
+            String createSecondCatalogSql = """
+                CREATE CATALOG %s USING %s
+                WITH (
+                   "connection-password" = '%s',
+                   "connection-url" = '%s',
+                   "connection-user" = '%s',
+                   "jdbc-types-mapped-to-varchar" = 'true'
+                )""".formatted(secondCatalog, CONNECTOR_NAME, PASSWORD, JDBC_URL, USERNAME);
+            assertUpdate(createSecondCatalogSql);
+            assertThat((String) computeActual("SHOW CREATE CATALOG " + secondCatalog).getOnlyValue()).isEqualTo(createSecondCatalogSql);
+            assertQuerySucceeds("SHOW TABLES FROM %s.%s".formatted(secondCatalog, TEST_SCHEMA));
+        }
+        finally {
+            assertUpdate("DROP CATALOG IF EXISTS " + firstCatalog);
+            assertUpdate("DROP CATALOG IF EXISTS " + secondCatalog);
+        }
     }
 
     @Test
