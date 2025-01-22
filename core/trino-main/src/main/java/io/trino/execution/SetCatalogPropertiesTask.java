a475654746f7930e96bb2675ee98eb5d509262ee
Piotr Rzysko <piotr.rzysko@starburstdata.com>
2025-01-22 12:34:34 +0100

    Extract evaluateProperties in SetCatalogPropertiesTask

--- core/trino-main/src/main/java/io/trino/execution/SetCatalogPropertiesTask.java
+++ core/trino-main/src/main/java/io/trino/execution/SetCatalogPropertiesTask.java
@@ -71,7 +71,7 @@ public class SetCatalogPropertiesTask
 
         String catalogName = statement.getName().getValue();
 
-        Map<String, Optional<String>> properties = getProperties(statement, parameters, session);
+        Map<String, Optional<String>> properties = evaluateProperties(statement, session, plannerContext, accessControl, parameters);
 
         accessControl.checkCanSetCatalogProperties(session.toSecurityContext(), catalogName, properties);
 
@@ -80,7 +80,12 @@ public class SetCatalogPropertiesTask
         return immediateVoidFuture();
     }
 
-    private Map<String, Optional<String>> getProperties(SetCatalogProperties statement, List<Expression> parameters, Session session)
+    private static Map<String, Optional<String>> evaluateProperties(
+            SetCatalogProperties statement,
+            Session session,
+            PlannerContext plannerContext,
+            AccessControl accessControl,
+            List<Expression> parameters)
     {
         Map<NodeRef<Parameter>, Expression> boundParameters = bindParameters(statement, parameters);
         ImmutableMap.Builder<String, Optional<String>> propertiesBuilder = ImmutableMap.builder();
