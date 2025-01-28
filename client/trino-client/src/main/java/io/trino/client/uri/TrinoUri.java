1ad05f488a823304d46e48927b4ac5f1141ccafd
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-01-28 10:01:03 +0100

    Mark fields as final
    (cherry picked from commit 44275cdf60430b119391acc2f40d4512dc670812)
    (cherry picked from commit 5aed1d959091f946ab82da02f15d830f2d164cfd)

--- client/trino-client/src/main/java/io/trino/client/uri/TrinoUri.java
+++ client/trino-client/src/main/java/io/trino/client/uri/TrinoUri.java
@@ -757,7 +757,7 @@ public class TrinoUri
     {
         private URI uri;
         private List<PropertyName> restrictedProperties = ImmutableList.of();
-        private ImmutableMap.Builder<Object, Object> properties = ImmutableMap.builder();
+        private final ImmutableMap.Builder<Object, Object> properties = ImmutableMap.builder();
 
         private Builder() {}
 
