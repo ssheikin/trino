74d86ec81461991849ad3a874ebae6bd1baef490
James Petty <pettja@amazon.com>
2025-02-06 11:58:37 -0500

    Use compiled comparator in TopNOperator
    Switches TopNOperator to use a compiled comparator instead of using
    SimplePageWithPositionComparator.
    
    (cherry picked from commit ed84c037e88d6e7f026d2048a567563382e3eb6d)
    (cherry picked from commit c25a96a0cf376935dc0f3cca87a0b8c801b473ab)

--- core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/LocalExecutionPlanner.java
@@ -1972,10 +1972,13 @@ public class LocalExecutionPlanner
 
             List<Symbol> orderBySymbols = node.getOrderingScheme().orderBy();
 
+            List<Type> sortTypes = new ArrayList<>();
             List<Integer> sortChannels = new ArrayList<>();
             List<SortOrder> sortOrders = new ArrayList<>();
             for (Symbol symbol : orderBySymbols) {
-                sortChannels.add(source.getLayout().get(symbol));
+                int sortChannel = source.getLayout().get(symbol);
+                sortTypes.add(source.getTypes().get(sortChannel));
+                sortChannels.add(sortChannel);
                 sortOrders.add(node.getOrderingScheme().ordering(symbol));
             }
 
@@ -1984,9 +1987,7 @@ public class LocalExecutionPlanner
                     node.getId(),
                     source.getTypes(),
                     (int) node.getCount(),
-                    sortChannels,
-                    sortOrders,
-                    plannerContext.getTypeOperators());
+                    orderingCompiler.compilePageWithPositionComparator(sortTypes, sortChannels, sortOrders));
 
             return new PhysicalOperation(operator, source.getLayout(), source);
         }
