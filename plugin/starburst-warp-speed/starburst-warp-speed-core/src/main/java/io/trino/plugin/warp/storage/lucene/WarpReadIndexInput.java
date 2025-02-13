66df0397d0a04e17a4946662356075d0762436ac
shlomi.alfasi@starburstdata.com <shlomi.alfasi@starburstdata.com>
2025-02-13 16:57:12 +0200

    [WARP-SPEED] fix lucene log prefix
    (cherry picked from commit 372e3712d266ead45e18a87ba4fdbff4dc90e652)

--- plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/warp/storage/lucene/WarpReadIndexInput.java
+++ plugin/starburst-warp-speed/starburst-warp-speed-core/src/main/java/io/trino/plugin/warp/storage/lucene/WarpReadIndexInput.java
@@ -79,7 +79,7 @@ public class WarpReadIndexInput
         this.sliceOffset = sliceOffset;
         this.sliceDescription = sliceDescription;
         this.digest = new BufferedChecksum(new CRC32());
-        this.logPrefix = String.format(Locale.US, "%d(%s_%s_%d-%d)", indexUniqueIdInRowGroup, luceneFileType, sliceDescription, sliceOffset, sliceOffset + length);
+        this.logPrefix = String.format(Locale.US, "%d(%s_%s_%s-%s)", indexUniqueIdInRowGroup, luceneFileType, sliceDescription, sliceOffset, sliceOffset + length);
         this.pageSize = storageEngineConstants.getPageSize();
         currentPageIndex = (int) (sliceOffset / pageSize);
         setCurrentBuffer();
