f83f514a2ffd9e8242833926afc3122cd5c179ca
lukasz-stec <lukasz.stec@starburstdata.com>
2025-02-15 11:38:32 +0100

    Print splits count and distribution time in EXPLAIN ANALYZE
    Split distribution time can be a bottleneck in some scenarios.
    Having it in the EXPLAIN output makes it easier to diagnose without accessing query json.
    Split count is helpful to analyze query performance when there is either a big
    number of small splits or there is just one split like with basic JDBC connectors.
    The split count is visible also in the "Input rows distribution" metric but it is only
    available in the VERBOSE mode.
    
    (cherry picked from commit 819b1935110eff239b710ae540da5c0285e3c02f)
    (cherry picked from commit 3c08f5f84c3d23058e4cb279f75a8935b8103892)

--- core/trino-main/src/main/java/io/trino/sql/planner/planprinter/PlanPrinter.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/planprinter/PlanPrinter.java
@@ -534,6 +534,10 @@ public class PlanPrinter
                             stageStats.getPeakUserMemoryReservation().succinct(),
                             tasks.size(),
                             maxPeakTaskMemoryUsage.succinct()));
+            if (stageStats.getGetSplitDistribution().getTotal() > 0) {
+                builder.append(indentString(1))
+                        .append(format("Total split distribution time: %sms\n", formatDouble(stageStats.getGetSplitDistribution().getTotal() / 1_000_000)));
+            }
             Optional<DistributionSnapshot> outputBufferUtilization = stageInfo.get().getStageStats().getOutputBufferUtilization();
             if (verbose && outputBufferUtilization.isPresent()) {
                 builder.append(indentString(1))
@@ -1174,6 +1178,7 @@ public class PlanPrinter
                         formatPositions(nodeStats.getPlanNodeInputPositions()),
                         nodeStats.getPlanNodeInputDataSize().toString());
                 addPhysicalInputStats(nodeStats, inputDetailBuilder, argsBuilder);
+                addSplits(nodeStats, inputDetailBuilder, argsBuilder);
                 appendDetailsFromBuilder(nodeOutput, inputDetailBuilder, argsBuilder);
             }
             return null;
@@ -1308,6 +1313,7 @@ public class PlanPrinter
                             nodeStats.getPlanNodeInputDataSize().toString(),
                             formatDouble(filtered));
                     addPhysicalInputStats(nodeStats, inputDetailBuilder, argsBuilder);
+                    addSplits(nodeStats, inputDetailBuilder, argsBuilder);
                     appendDetailsFromBuilder(nodeOutput, inputDetailBuilder, argsBuilder);
                 }
                 List<DynamicFilterDomainStats> collectedDomainStats = dynamicFilters.stream()
@@ -1336,6 +1342,11 @@ public class PlanPrinter
 
             sourceNode.accept(this, new Context(context.isInitialPlan()));
             return null;
+        }
+
+        private void addSplits(PlanNodeStats nodeStats, StringBuilder inputDetailBuilder, ImmutableList.Builder<String> argsBuilder)
+        {
+            buildFormatString(inputDetailBuilder, argsBuilder, ", Splits: %s", String.valueOf(nodeStats.getOperatorStats().values().iterator().next().getTotalDrivers()));
         }
 
         private static void addPhysicalInputStats(PlanNodeStats nodeStats, StringBuilder inputDetailBuilder, ImmutableList.Builder<String> argsBuilder)
