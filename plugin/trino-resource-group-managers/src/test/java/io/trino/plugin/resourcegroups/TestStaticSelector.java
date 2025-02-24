55299de404a98f618605120ba078e4f4dbe87a8e
Erik Krogen <erikkrogen@gmail.com>
2025-02-24 15:55:11 -0800

    Refactor StaticSelector to simplify and optimize matching logic
    (cherry picked from commit 8c0cc3a929a84519b81c6094b1167bb60f173230)
    (cherry picked from commit 11a5f06298d349688ef4683aff76cc352b644627)

--- plugin/trino-resource-group-managers/src/test/java/io/trino/plugin/resourcegroups/TestStaticSelector.java
+++ plugin/trino-resource-group-managers/src/test/java/io/trino/plugin/resourcegroups/TestStaticSelector.java
@@ -56,6 +56,25 @@ public class TestStaticSelector
     }
 
     @Test
+    public void testUserRegexCustomGroup()
+    {
+        ResourceGroupId resourceGroupId = new ResourceGroupId(new ResourceGroupId("global"), "foo_userA_A");
+        StaticSelector selector = new StaticSelector(
+                Optional.of(Pattern.compile("user(?<suffix>.*)")),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                new ResourceGroupIdTemplate("global.foo_${USER}_${suffix}"));
+        assertThat(selector.match(newSelectionCriteria("userA", null, ImmutableSet.of(), EMPTY_RESOURCE_ESTIMATES, ""))).hasValueSatisfying(context -> {
+            assertThat(context.getResourceGroupId()).isEqualTo(resourceGroupId);
+            assertThat(context.getContext().getVariableNames()).containsExactlyInAnyOrder("suffix", "USER");
+        });
+    }
+
+    @Test
     public void testSourceRegex()
     {
         ResourceGroupId resourceGroupId = new ResourceGroupId(new ResourceGroupId("global"), "foo");
@@ -89,6 +108,24 @@ public class TestStaticSelector
         assertThat(selector.match(newSelectionCriteria("userA", "source", ImmutableSet.of("tag1"), EMPTY_RESOURCE_ESTIMATES, "select * from part"))).isEqualTo(Optional.empty());
         assertThat(selector.match(newSelectionCriteria("user", "source", ImmutableSet.of(), EMPTY_RESOURCE_ESTIMATES, "select partkey from part")).map(SelectionContext::getResourceGroupId)).isEqualTo(Optional.of(resourceGroupId));
         assertThat(selector.match(newSelectionCriteria("user", "source", ImmutableSet.of("tag1"), EMPTY_RESOURCE_ESTIMATES, "select name, partkey, brand from part")).map(SelectionContext::getResourceGroupId)).isEqualTo(Optional.of(resourceGroupId));
+    }
+
+    @Test
+    public void testSourceRegexEmptyMatch()
+    {
+        ResourceGroupId resourceGroupId = new ResourceGroupId(new ResourceGroupId("global"), "foo");
+        StaticSelector selector = new StaticSelector(
+                Optional.empty(),
+                Optional.empty(),
+                Optional.of(Pattern.compile("$^")),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                Optional.empty(),
+                new ResourceGroupIdTemplate("global.foo"));
+        assertThat(selector.match(newSelectionCriteria("user", null, ImmutableSet.of(), EMPTY_RESOURCE_ESTIMATES, "")).map(SelectionContext::getResourceGroupId)).isEqualTo(Optional.of(resourceGroupId));
+        assertThat(selector.match(newSelectionCriteria("user", "", ImmutableSet.of(), EMPTY_RESOURCE_ESTIMATES, "")).map(SelectionContext::getResourceGroupId)).isEqualTo(Optional.of(resourceGroupId));
+        assertThat(selector.match(newSelectionCriteria("user", "source", ImmutableSet.of(""), EMPTY_RESOURCE_ESTIMATES, ""))).isEqualTo(Optional.empty());
     }
 
     @Test
