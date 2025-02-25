78931c145e5bb1c1164249c0623466ae4910c08c
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-02-25 13:36:15 +0100

    Move QueryDataProducerFactory to the Query class
    (cherry picked from commit be3e35a6686ea2d985bae962a6871fe847e77355)
    (cherry picked from commit 4421d037d0dbd72463d9cc54bde7e9e842bcfed0)

--- core/trino-main/src/main/java/io/trino/server/protocol/ExecutingStatementResource.java
+++ core/trino-main/src/main/java/io/trino/server/protocol/ExecutingStatementResource.java
@@ -206,7 +206,7 @@ public class ExecutingStatementResource
                 session,
                 querySlug,
                 queryManager,
-                queryDataProducerFactory.create(session),
+                queryDataProducerFactory,
                 queryInfoUrlFactory.getQueryInfoUrl(queryId),
                 directExchangeClientSupplier,
                 exchangeManagerRegistry,
--- core/trino-main/src/main/java/io/trino/server/protocol/Query.java
+++ core/trino-main/src/main/java/io/trino/server/protocol/Query.java
@@ -198,7 +198,7 @@ class Query
             Session session,
             Slug slug,
             QueryManager queryManager,
-            QueryDataProducer queryDataProducer,
+            QueryDataProducerFactory queryDataProducerFactory,
             Optional<URI> queryInfoUrl,
             DirectExchangeClientSupplier directExchangeClientSupplier,
             ExchangeManagerRegistry exchangeManagerRegistry,
@@ -217,7 +217,7 @@ class Query
                 getRetryPolicy(session),
                 exchangeManagerRegistry);
 
-        Query result = new Query(session, slug, queryManager, queryDataProducer, queryInfoUrl, resultsCacheEntry, exchangeDataSource, dataProcessorExecutor, timeoutExecutor, blockEncodingSerde);
+        Query result = new Query(session, slug, queryManager, queryDataProducerFactory.create(session), queryInfoUrl, resultsCacheEntry, exchangeDataSource, dataProcessorExecutor, timeoutExecutor, blockEncodingSerde);
 
         result.queryManager.setOutputInfoListener(result.getQueryId(), result::setQueryOutputInfo);
 
