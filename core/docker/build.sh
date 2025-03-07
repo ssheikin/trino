8bc7b5bd89e1e3e02d7106f7e6d5c32997cb7d0b
Jan Waś <jan.was@starburstdata.com>
2025-03-07 15:31:36 +0100

    Don't require the Trino version arg in the Dockerfile
    (cherry picked from commit dbab3bc96f4b355289d7f70432e6fcb74ac6dda3)
    (cherry picked from commit 3d217e4bd559dea03b082894586c2a77f5d7154f)

--- core/docker/build.sh
+++ core/docker/build.sh
@@ -109,10 +109,11 @@ fi
 echo "🧱 Preparing the image build context directory"
 WORK_DIR="$(mktemp -d)"
 cp "$trino_server" "${WORK_DIR}/"
-cp "$trino_client" "${WORK_DIR}/"
+cp "$trino_client" "${WORK_DIR}/trino-cli.jar"
 tar -C "${WORK_DIR}" -xzf "${WORK_DIR}/trino-server-${TRINO_VERSION}.tar.gz"
 rm "${WORK_DIR}/trino-server-${TRINO_VERSION}.tar.gz"
-cp -R bin "${WORK_DIR}/trino-server-${TRINO_VERSION}"
+mv "${WORK_DIR}/trino-server-${TRINO_VERSION}" "${WORK_DIR}/trino-server"
+cp -R bin "${WORK_DIR}/trino-server"
 cp -R default "${WORK_DIR}/"
 
 TAG_PREFIX="trino:${TRINO_VERSION}"
@@ -128,8 +129,7 @@ for arch in "${ARCHITECTURES[@]}"; do
         --build-arg JDK_DOWNLOAD_LINK="$(jdk_download_link "${JDKS_PATH}/${JDK_RELEASE}" "${arch}")" \
         --platform "linux/$arch" \
         -f Dockerfile \
-        -t "${TAG_PREFIX}-$arch" \
-        --build-arg "TRINO_VERSION=${TRINO_VERSION}"
+        -t "${TAG_PREFIX}-$arch"
 done
 
 echo "🧹 Cleaning up the build context directory"
