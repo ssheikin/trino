2e3a949e28cc926801ccf8bd96258a7ac8930cae
Ashhar Hasan <ashhar.hasan@starburstdata.com>
2025-03-21 00:31:20 +0530

    Revert "Ensure queries in system.runtime.queries are redacted"
    This reverts commit 047a001b1b285d6115f87ed235dd3b9bf9613199.
    
    Redaction is best effort and it's difficult to ensure that credentials
    will not be leaked anywhere.
    
    We instead want to educate users using error messages and Trino warnings
    system that any credentials typed anywhere should be considered leaked
    and rotated.

--- core/trino-main/src/test/java/io/trino/connector/MockConnectorFactory.java
+++ core/trino-main/src/test/java/io/trino/connector/MockConnectorFactory.java
@@ -16,7 +16,6 @@ package io.trino.connector;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
 import io.trino.spi.cache.CacheColumnId;
 import io.trino.spi.cache.CacheTableId;
 import io.trino.spi.connector.AggregateFunction;
@@ -142,7 +141,6 @@ public class MockConnectorFactory
     private final Supplier<List<PropertyMetadata<?>>> columnProperties;
     private final Optional<ConnectorNodePartitioningProvider> partitioningProvider;
     private final Function<ConnectorTableFunctionHandle, ConnectorSplitSource> tableFunctionSplitsSources;
-    private final Set<String> securitySensitivePropertyNames;
 
     // access control
     private final ListRoleGrants roleGrants;
@@ -202,7 +200,6 @@ public class MockConnectorFactory
             Supplier<List<PropertyMetadata<?>>> tableProperties,
             Supplier<List<PropertyMetadata<?>>> columnProperties,
             Optional<ConnectorNodePartitioningProvider> partitioningProvider,
-            Set<String> securitySensitivePropertyNames,
             ListRoleGrants roleGrants,
             Optional<LocationAccessControl> locationAccessControl,
             Optional<ConnectorAccessControl> accessControl,
@@ -254,7 +251,6 @@ public class MockConnectorFactory
         this.tableProperties = requireNonNull(tableProperties, "tableProperties is null");
         this.columnProperties = requireNonNull(columnProperties, "columnProperties is null");
         this.partitioningProvider = requireNonNull(partitioningProvider, "partitioningProvider is null");
-        this.securitySensitivePropertyNames = requireNonNull(securitySensitivePropertyNames, "securitySensitivePropertyNames is null");
         this.roleGrants = requireNonNull(roleGrants, "roleGrants is null");
         this.locationAccessControl = requireNonNull(locationAccessControl, "locationAccessControl is null");
         this.accessControl = requireNonNull(accessControl, "accessControl is null");
@@ -341,12 +337,6 @@ public class MockConnectorFactory
                 writerScalingOptions,
                 capabilities,
                 allowSplittingReadIntoMultipleSubQueries);
-    }
-
-    @Override
-    public Set<String> getSecuritySensitivePropertyNames(String catalogName, Map<String, String> config, ConnectorContext context)
-    {
-        return Sets.intersection(securitySensitivePropertyNames, config.keySet());
     }
 
     public static MockConnectorFactory create()
@@ -488,7 +478,6 @@ public class MockConnectorFactory
         private Supplier<List<PropertyMetadata<?>>> columnProperties = ImmutableList::of;
         private Optional<ConnectorNodePartitioningProvider> partitioningProvider = Optional.empty();
         private Function<ConnectorTableFunctionHandle, ConnectorSplitSource> tableFunctionSplitsSources = handle -> null;
-        private Set<String> securitySensitivePropertyNames = ImmutableSet.of();
 
         // access control
         private boolean provideAccessControl;
@@ -880,12 +869,6 @@ public class MockConnectorFactory
             return this;
         }
 
-        public Builder withSecuritySensitivePropertyNames(Set<String> securitySensitivePropertyNames)
-        {
-            this.securitySensitivePropertyNames = securitySensitivePropertyNames;
-            return this;
-        }
-
         public MockConnectorFactory build()
         {
             Optional<ConnectorAccessControl> accessControl = Optional.empty();
@@ -936,7 +919,6 @@ public class MockConnectorFactory
                     tableProperties,
                     columnProperties,
                     partitioningProvider,
-                    securitySensitivePropertyNames,
                     roleGrants,
                     locationAccessControl,
                     accessControl,
