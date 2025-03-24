fd46c59cd8dca00f1b1fae3e4a957da8f40903a2
Yuya Ebihara <ebyhry@gmail.com>
2025-03-25 08:54:43 +0900

    Rename method in IcebergUtil
    (cherry picked from commit 6fc462374a51dd71ab5f717dcafa3da15ec274d5)
    (cherry picked from commit 65e758d3edcab9e0732a5aaec90662cc297ae5f6)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergUtil.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergUtil.java
@@ -1188,7 +1188,7 @@ public final class IcebergUtil
         return domain;
     }
 
-    public static Domain getFileModifiedTimePathDomain(TupleDomain<IcebergColumnHandle> effectivePredicate)
+    public static Domain getFileModifiedTimeDomain(TupleDomain<IcebergColumnHandle> effectivePredicate)
     {
         IcebergColumnHandle fileModifiedTimeColumn = fileModifiedTimeColumnHandle();
         Domain domain = effectivePredicate.getDomains().orElseThrow(() -> new IllegalArgumentException("Unexpected NONE tuple domain"))
