73da64320e97d1f59e688d8f626af0844aa6a200
Anu Sudarsan <anu.sudarsan@starburstdata.com>
2025-03-26 15:58:01 -0400

    Remove usages of SQLPOOL2 from Synapse tests
    In a follow up we will see if we can run tests in the Serverless pool
    
    (cherry picked from commit 8ea1aab3fa96ef77ce2b43e22c277d5cf2235604)

--- plugin/starburst-synapse/src/test/java/com/starburstdata/trino/plugin/synapse/SynapseServer.java
+++ plugin/starburst-synapse/src/test/java/com/starburstdata/trino/plugin/synapse/SynapseServer.java
@@ -19,7 +19,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
-import java.util.Random;
 import java.util.function.Function;
 
 import static java.util.Objects.requireNonNull;
@@ -34,7 +33,7 @@ public class SynapseServer
     static final String USERNAME = requireNonNull(System.getProperty("test.synapse.jdbc.user"), "test.synapse.jdbc.user is not set");
     static final String PASSWORD = requireNonNull(System.getProperty("test.synapse.jdbc.password"), "test.synapse.jdbc.password is not set");
 
-    private static final String DATABASE = new Random().nextBoolean() ? "SQLPOOL1" : "SQLPOOL2";
+    private static final String DATABASE = "SQLPOOL1";
     private static final String PORT = "1433";
 
     static final String JDBC_URL = "jdbc:sqlserver://" + ENDPOINT + ":" + PORT + ";database=" + DATABASE;
