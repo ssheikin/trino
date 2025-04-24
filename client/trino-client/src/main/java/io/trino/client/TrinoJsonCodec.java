34ed20ef62a671c8bc5698c1c4909ec9b48ba018
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-04-24 15:01:41 +0200

    Teach client libraries how to serialize spooling protocol
    (cherry picked from commit 02cb501a1fa51c6799a721d81ee16a6d63e462d9)
    (cherry picked from commit 06b211343e2107d0f14de49f6a27632793dff6a7)

--- client/trino-client/src/main/java/io/trino/client/TrinoJsonCodec.java
+++ client/trino-client/src/main/java/io/trino/client/TrinoJsonCodec.java
@@ -65,7 +65,7 @@ public class TrinoJsonCodec<T>
                 .disable(MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS)
                 .disable(StreamReadFeature.AUTO_CLOSE_SOURCE)
                 .addModule(new Jdk8Module())
-                .addModule(new QueryDataClientJacksonModule())
+                .addModule(new QueryDataJacksonModule())
                 .build();
     };
 
