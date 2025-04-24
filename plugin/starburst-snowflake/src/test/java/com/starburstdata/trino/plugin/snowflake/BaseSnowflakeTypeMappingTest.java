94ceb1a257c4de2837e03f89a5e54d9f50078172
Yuya Ebihara <ebyhry@gmail.com>
2025-04-24 13:40:38 +0900

    Fix error message pattern of Snowflake negative timestamp
    (cherry picked from commit 3bf9b83036af0290e87fc7e95da4503d05cf3679)

--- plugin/starburst-snowflake/src/test/java/com/starburstdata/trino/plugin/snowflake/BaseSnowflakeTypeMappingTest.java
+++ plugin/starburst-snowflake/src/test/java/com/starburstdata/trino/plugin/snowflake/BaseSnowflakeTypeMappingTest.java
@@ -567,11 +567,11 @@ public abstract class BaseSnowflakeTypeMappingTest
         //timestamp(9) -0001-01-01 00:00:00.000000000 has different results in jdbc and distributed connectors
         try (TestTable table = new TestTable(sqls -> getQueryRunner().execute(sqls), "test_schema_2.test_timestamp", "(c1 timestamp(6))")) {
             assertQueryFails(format("INSERT INTO %s (c1) VALUES (TIMESTAMP '-0001-01-01 00:00:00.000000')", table.getName()),
-                    "Failed to insert data: Timestamp '-0001-01-01T00:00' is not recognized");
+                    "Failed to insert data: .*Timestamp '-0001-01-01T00:00' is not recognized");
         }
         try (TestTable table = new TestTable(sqls -> getQueryRunner().execute(sqls), "test_schema_2.test_timestamp", "(c1 timestamp(3))")) {
             assertQueryFails(format("INSERT INTO %s (c1) VALUES (TIMESTAMP '-0001-01-01 00:00:00.000')", table.getName()),
-                    "Failed to insert data: Timestamp '-0001-01-01T00:00' is not recognized");
+                    "Failed to insert data: .*Timestamp '-0001-01-01T00:00' is not recognized");
         }
     }
 
