58e3712d78a1cdf419851c7254b5d380eace9130
kumiDa <kayrahul@gmail.com>
2025-04-26 11:40:37 +0530

    Fix typos in the trino-client
    (cherry picked from commit c8ed2282d3adf2e6c77f6c52b61dd990af4ab4f8)
    (cherry picked from commit 79d62f3b1ffe3a983fdbf39325b1196b64d91583)

--- client/trino-client/src/main/java/io/trino/client/StatementClientV1.java
+++ client/trino-client/src/main/java/io/trino/client/StatementClientV1.java
@@ -687,7 +687,7 @@ class StatementClientV1
     private enum State
     {
         /**
-         * submitted to server, not in terminal state (including planning, queued, running, etc)
+         * submitted to server, not in terminal state (including planning, queued, running, etc.)
          */
         RUNNING,
         CLIENT_ERROR,
