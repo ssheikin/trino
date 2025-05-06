5c4cb01f674e68b4933a545c3c21821dd53cb506
James Petty <pettja@amazon.com>
2025-05-06 12:30:21 -0400

    Remove unnecessary retainedSizeInBytes field
    (cherry picked from commit fec9790124d03faa8c81aab3dd186313cf56a6a6)
    (cherry picked from commit d43bb192655608653567e322230f512377ffbffb)

--- core/trino-main/src/main/java/io/trino/operator/project/PageProcessor.java
+++ core/trino-main/src/main/java/io/trino/operator/project/PageProcessor.java
@@ -162,7 +162,6 @@ public class PageProcessor
         private SourcePage page;
         private final Block[] previouslyComputedResults;
         private SelectedPositions selectedPositions;
-        private long retainedSizeInBytes;
 
         // remember if we need to re-use the same batch size if we yield last time
         private boolean lastComputeYielded;
@@ -280,7 +279,7 @@ public class PageProcessor
                 }
             }
 
-            retainedSizeInBytes = INSTANCE_SIZE +
+            long retainedSizeInBytes = INSTANCE_SIZE +
                     selectedPositions.getRetainedSizeInBytes() +
                     sizeOf(previouslyComputedResults) +
                     visitor.getRetainedSizeInBytes();
