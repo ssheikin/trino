a97eb7146338c6f20fe6055f59c29475a9435fed
Łukasz Osipiuk <lukasz@osipiuk.net>
2025-05-13 12:38:12 +0200

    Use better variable names
    (cherry picked from commit 7cc0a1b082c35f81e32d12e21de86d7c91ecbd6e)

--- core/trino-main/src/main/java/io/trino/execution/scheduler/MultiSourcePartitionedScheduler.java
+++ core/trino-main/src/main/java/io/trino/execution/scheduler/MultiSourcePartitionedScheduler.java
@@ -50,7 +50,7 @@ public class MultiSourcePartitionedScheduler
     private static final Logger log = Logger.get(MultiSourcePartitionedScheduler.class);
 
     private final StageExecution stageExecution;
-    private final Queue<SourceScheduler> sourceSchedulers;
+    private final Queue<SourceScheduler> partitionedSourceSchedulers;
     private final Map<InternalNode, RemoteTask> scheduledTasks = new HashMap<>();
     private final DynamicFilterService dynamicFilterService;
     private final SplitPlacementPolicy splitPlacementPolicy;
@@ -58,7 +58,7 @@ public class MultiSourcePartitionedScheduler
 
     public MultiSourcePartitionedScheduler(
             StageExecution stageExecution,
-            Map<PlanNodeId, SplitSource> splitSources,
+            Map<PlanNodeId, SplitSource> partitionedSplitSources,
             Map<PlanNodeId, Optional<QualifiedObjectName>> sourceTables,
             ScheduledSplitsPerTableTracker scheduledSplitsPerTableTracker,
             SplitPlacementPolicy splitPlacementPolicy,
@@ -67,12 +67,12 @@ public class MultiSourcePartitionedScheduler
             TableExecuteContextManager tableExecuteContextManager,
             BooleanSupplier anySourceTaskBlocked)
     {
-        requireNonNull(splitSources, "splitSources is null");
-        checkArgument(splitSources.size() > 1, "It is expected that there will be more than one split sources");
+        requireNonNull(partitionedSplitSources, "partitionedSplitSources is null");
+        checkArgument(partitionedSplitSources.size() > 1, "It is expected that there will be more than one split sources");
 
         ImmutableList.Builder<SourceScheduler> sourceSchedulers = ImmutableList.builder();
-        for (PlanNodeId planNodeId : splitSources.keySet()) {
-            SplitSource splitSource = splitSources.get(planNodeId);
+        for (PlanNodeId planNodeId : partitionedSplitSources.keySet()) {
+            SplitSource splitSource = partitionedSplitSources.get(planNodeId);
             SourceScheduler sourceScheduler = newSourcePartitionedSchedulerAsSourceScheduler(
                     stageExecution,
                     planNodeId,
@@ -89,7 +89,7 @@ public class MultiSourcePartitionedScheduler
             sourceSchedulers.add(sourceScheduler);
         }
         this.stageExecution = requireNonNull(stageExecution, "stageExecution is null");
-        this.sourceSchedulers = new ArrayDeque<>(sourceSchedulers.build());
+        this.partitionedSourceSchedulers = new ArrayDeque<>(sourceSchedulers.build());
         this.dynamicFilterService = requireNonNull(dynamicFilterService, "dynamicFilterService is null");
         this.splitPlacementPolicy = requireNonNull(splitPlacementPolicy, "splitPlacementPolicy is null");
     }
@@ -120,8 +120,8 @@ public class MultiSourcePartitionedScheduler
         Optional<ScheduleResult.BlockedReason> blockedReason = Optional.empty();
         int splitsScheduled = 0;
 
-        while (!sourceSchedulers.isEmpty()) {
-            SourceScheduler scheduler = sourceSchedulers.peek();
+        while (!partitionedSourceSchedulers.isEmpty()) {
+            SourceScheduler scheduler = partitionedSourceSchedulers.peek();
             ScheduleResult scheduleResult = scheduler.schedule();
 
             splitsScheduled += scheduleResult.getSplitsScheduled();
@@ -135,18 +135,18 @@ public class MultiSourcePartitionedScheduler
             }
 
             stageExecution.schedulingComplete(scheduler.getPlanNodeId());
-            sourceSchedulers.remove().close();
+            partitionedSourceSchedulers.remove().close();
         }
         if (blockedReason.isPresent()) {
-            return new ScheduleResult(sourceSchedulers.isEmpty(), newScheduledTasks.build(), blocked, blockedReason.get(), splitsScheduled);
+            return new ScheduleResult(partitionedSourceSchedulers.isEmpty(), newScheduledTasks.build(), blocked, blockedReason.get(), splitsScheduled);
         }
-        return new ScheduleResult(sourceSchedulers.isEmpty(), newScheduledTasks.build(), splitsScheduled);
+        return new ScheduleResult(partitionedSourceSchedulers.isEmpty(), newScheduledTasks.build(), splitsScheduled);
     }
 
     @Override
     public void close()
     {
-        for (SourceScheduler sourceScheduler : sourceSchedulers) {
+        for (SourceScheduler sourceScheduler : partitionedSourceSchedulers) {
             try {
                 sourceScheduler.close();
             }
@@ -154,7 +154,7 @@ public class MultiSourcePartitionedScheduler
                 log.warn(t, "Error closing split source");
             }
         }
-        sourceSchedulers.clear();
+        partitionedSourceSchedulers.clear();
     }
 
     private void scheduleTaskOnRandomNode()
