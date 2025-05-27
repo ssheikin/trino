e82f2a34b94a31ca92a954750d503671904c2a8a
Łukasz Osipiuk <lukasz@osipiuk.net>
2025-05-27 14:47:26 +0200

    Handle adaptive plans in TableExecuteStructureValidator
    (cherry picked from commit 1e85f95452cd4063c000a05c06ba00083dbad7f0)
    (cherry picked from commit e6e68eb8d0ff967de45d9341d0522ef6b179de8f)

--- core/trino-main/src/main/java/io/trino/sql/planner/AdaptivePlanner.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/AdaptivePlanner.java
@@ -321,7 +321,7 @@ public class AdaptivePlanner
         return upstreamNodes;
     }
 
-    private PlanNode getCurrentPlan(PlanNode node)
+    public static PlanNode getCurrentPlan(PlanNode node)
     {
         return rewriteWith(new CurrentPlanRewriter(), node);
     }
