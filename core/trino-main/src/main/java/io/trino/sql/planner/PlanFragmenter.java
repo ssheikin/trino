2576ca485365855887420eb8a8b18d0d8a804d99
Łukasz Osipiuk <lukasz@osipiuk.net>
2025-05-27 14:48:12 +0200

    Fix logic for propagating adaptive partitioning fragment property
    (cherry picked from commit a00811bdbe58fc22f205aa9678b68840e3a09072)
    (cherry picked from commit 1d3fbeb36f6904a878dc69f9233544e52f954a4c)

--- core/trino-main/src/main/java/io/trino/sql/planner/PlanFragmenter.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/PlanFragmenter.java
@@ -650,9 +650,12 @@ public class PlanFragmenter
                 Metadata metadata,
                 Session session)
         {
+            if (partitionCount.isPresent()) {
+                this.partitionCount = partitionCount;
+            }
+
             if (partitioningHandle.isEmpty()) {
                 partitioningHandle = Optional.of(distribution);
-                this.partitionCount = partitionCount;
                 return this;
             }
 
@@ -673,7 +676,6 @@ public class PlanFragmenter
 
             if (isCompatibleScaledWriterPartitioning(currentPartitioning, distribution)) {
                 this.partitioningHandle = Optional.of(distribution);
-                this.partitionCount = partitionCount;
                 return this;
             }
 
