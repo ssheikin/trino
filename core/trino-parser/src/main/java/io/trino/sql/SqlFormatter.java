1f26483726be3347ceed9b391e179d9c2c6efd1e
praveenkrishna.d <praveenkrishna@tutanota.com>
2025-06-02 20:43:45 +0530

    Add parser support for refreshing view
    (cherry picked from commit bc6df0068d3798d5c725172f6874abd61f2a2ccb)
    (cherry picked from commit 656765c2ba52134dd4fb9b05f51b5e753dba0584)

--- core/trino-parser/src/main/java/io/trino/sql/SqlFormatter.java
+++ core/trino-parser/src/main/java/io/trino/sql/SqlFormatter.java
@@ -122,6 +122,7 @@ import io.trino.sql.tree.QueryColumn;
 import io.trino.sql.tree.QueryPeriod;
 import io.trino.sql.tree.QuerySpecification;
 import io.trino.sql.tree.RefreshMaterializedView;
+import io.trino.sql.tree.RefreshView;
 import io.trino.sql.tree.Relation;
 import io.trino.sql.tree.RenameCatalog;
 import io.trino.sql.tree.RenameColumn;
@@ -1279,6 +1280,15 @@ public final class SqlFormatter
             builder.append("REFRESH MATERIALIZED VIEW ");
             builder.append(formatName(node.getName()));
 
+            return null;
+        }
+
+        @Override
+        protected Void visitRefreshView(RefreshView node, Integer indent)
+        {
+            builder.append("ALTER VIEW ");
+            builder.append(formatName(node.getName()));
+            builder.append(" REFRESH");
             return null;
         }
 
