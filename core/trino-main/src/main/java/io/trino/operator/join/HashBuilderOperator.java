7e90e687f7939fbbc7a89c29f8acfbc630e887d2
Karol Sobczak <napewnotrafi@gmail.com>
2025-06-10 14:18:03 +0200

    Update revocable memory reservation after compaction
    (cherry picked from commit 2a3df684c1c1c8943b8c08606319074c763a2457)
    (cherry picked from commit e1a96e2ba6f56c8a19789b3f4450a8d7d2de5694)

--- core/trino-main/src/main/java/io/trino/operator/join/HashBuilderOperator.java
+++ core/trino-main/src/main/java/io/trino/operator/join/HashBuilderOperator.java
@@ -366,6 +366,7 @@ public class HashBuilderOperator
             long indexSizeAfterCompaction = index.getEstimatedSize().toBytes();
             if (indexSizeAfterCompaction < indexSizeBeforeCompaction * INDEX_COMPACTION_ON_REVOCATION_TARGET) {
                 finishMemoryRevoke = Optional.of(() -> {});
+                localRevocableMemoryContext.setBytes(indexSizeAfterCompaction);
                 return immediateVoidFuture();
             }
 
