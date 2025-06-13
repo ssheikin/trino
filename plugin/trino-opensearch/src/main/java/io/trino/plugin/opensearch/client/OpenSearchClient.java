2599b14bd4697140b3f0c5f6e1a43e30c18e346c
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-06-13 10:54:16 +0200

    Update AWS SDK v2 to 2.31.63
    (cherry picked from commit aa5a65a99ece977d9593f2340ff934f25bc75ea1)
    (cherry picked from commit d499c27c410380b1921a9623167bbf36b3510032)

--- plugin/trino-opensearch/src/main/java/io/trino/plugin/opensearch/client/OpenSearchClient.java
+++ plugin/trino-opensearch/src/main/java/io/trino/plugin/opensearch/client/OpenSearchClient.java
@@ -277,7 +277,7 @@ public class OpenSearchClient
 
     private static AwsCredentialsProvider getAwsCredentialsProvider(AwsSecurityConfig config)
     {
-        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.builder().build();
 
         if (config.getAccessKey().isPresent() && config.getSecretKey().isPresent()) {
             credentialsProvider = StaticCredentialsProvider.create(AwsBasicCredentials.create(
