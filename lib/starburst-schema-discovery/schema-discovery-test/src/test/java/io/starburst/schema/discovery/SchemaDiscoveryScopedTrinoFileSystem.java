2b6ede86c9e49232f3ee56d48e22433eb0e3175d
Łukasz Osipiuk <lukasz@osipiuk.net>
2025-06-18 16:10:04 +0200

    Remove unnecessary throws declarations
    (cherry picked from commit 0f4518ed53da9d64ff91674bebe87c5fd3db114d)

--- lib/starburst-schema-discovery/schema-discovery-test/src/test/java/io/starburst/schema/discovery/SchemaDiscoveryScopedTrinoFileSystem.java
+++ lib/starburst-schema-discovery/schema-discovery-test/src/test/java/io/starburst/schema/discovery/SchemaDiscoveryScopedTrinoFileSystem.java
@@ -14,7 +14,6 @@ import io.trino.filesystem.TrinoFileSystem;
 import io.trino.filesystem.TrinoInputFile;
 import io.trino.filesystem.TrinoOutputFile;
 
-import java.io.IOException;
 import java.time.Instant;
 import java.util.Optional;
 
@@ -41,42 +40,36 @@ public interface SchemaDiscoveryScopedTrinoFileSystem
 
     @Override
     default void deleteFile(Location location)
-            throws IOException
     {
         throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
     default void deleteDirectory(Location location)
-            throws IOException
     {
         throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
     default void renameFile(Location source, Location target)
-            throws IOException
     {
         throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
     default void createDirectory(Location location)
-            throws IOException
     {
         throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
     default void renameDirectory(Location source, Location target)
-            throws IOException
     {
         throw new UnsupportedOperationException("Not implemented");
     }
 
     @Override
     default Optional<Location> createTemporaryDirectory(Location targetPath, String temporaryPrefix, String relativePrefix)
-            throws IOException
     {
         throw new UnsupportedOperationException("Not implemented");
     }
