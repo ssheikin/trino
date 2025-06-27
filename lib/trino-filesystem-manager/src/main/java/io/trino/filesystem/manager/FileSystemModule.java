d70dbe8208f0690d634b621c0d71591ffac1b027
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-06-27 11:25:48 +0200

    Detect and close I/O leaks in the native FS implementations
    It is enable by default in the CI to report detected leaks
    
    (cherry picked from commit 34b02bcaff07c583b4a5dd66a2cd3efa8939f0c1)
    (cherry picked from commit b30704d9058c639a4fc83782928147c6838ffd45)

--- lib/trino-filesystem-manager/src/main/java/io/trino/filesystem/manager/FileSystemModule.java
+++ lib/trino-filesystem-manager/src/main/java/io/trino/filesystem/manager/FileSystemModule.java
@@ -50,6 +50,7 @@ import io.trino.filesystem.s3.FileSystemS3;
 import io.trino.filesystem.s3.S3FileSystemModule;
 import io.trino.filesystem.switching.SwitchingFileSystemFactory;
 import io.trino.filesystem.tracing.TracingFileSystemFactory;
+import io.trino.filesystem.tracking.TrackingFileSystemFactory;
 import io.trino.plugin.base.security.passthrough.TokenPassThroughConfig;
 import io.trino.spi.NodeManager;
 
@@ -62,6 +63,7 @@ import static com.google.inject.multibindings.MapBinder.newMapBinder;
 import static com.google.inject.multibindings.Multibinder.newSetBinder;
 import static com.google.inject.multibindings.OptionalBinder.newOptionalBinder;
 import static io.airlift.configuration.ConfigBinder.configBinder;
+import static java.lang.System.getenv;
 import static java.util.Objects.requireNonNull;
 
 public class FileSystemModule
@@ -166,6 +168,7 @@ public class FileSystemModule
     @Provides
     @Singleton
     static TrinoFileSystemFactory createFileSystemFactory(
+            FileSystemConfig config,
             Optional<HdfsFileSystemLoader> hdfsFileSystemLoader,
             Map<String, TrinoFileSystemFactory> factories,
             Optional<TrinoFileSystemCache> fileSystemCache,
@@ -183,6 +186,13 @@ public class FileSystemModule
 
         TrinoFileSystemFactory delegate = new SwitchingFileSystemFactory(loader);
         delegate = new TracingFileSystemFactory(tracer, delegate);
+
+        // Enable leak detection if configured or if running in a CI environment
+        boolean trackingDetectionEnabled = getenv("CONTINUOUS_INTEGRATION") != null;
+        if (config.isTrackingEnabled() || trackingDetectionEnabled) {
+            delegate = new TrackingFileSystemFactory(delegate);
+        }
+
         if (!decorators.isEmpty()) {
             delegate = new DecoratingTrinoFileSystemFactory(delegate, decorators);
         }
