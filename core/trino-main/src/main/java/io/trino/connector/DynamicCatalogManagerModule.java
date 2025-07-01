6f1a5030491d4c3245bca4a39bcdb39da91955cf
Dain Sundstrom <dain@iq80.com>
2025-06-30 20:27:53 -0700

    Use separate http client for CatalogPruneTask
    (cherry picked from commit fb309b0f939f2f8da021b714fdddabc2515f1203)

--- core/trino-main/src/main/java/io/trino/connector/DynamicCatalogManagerModule.java
+++ core/trino-main/src/main/java/io/trino/connector/DynamicCatalogManagerModule.java
@@ -17,6 +17,7 @@ import com.google.inject.Binder;
 import com.google.inject.Inject;
 import com.google.inject.Scopes;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
+import io.airlift.units.Duration;
 import io.trino.connector.WorkerDynamicCatalogManager.NoOpWorkerCatalogManager;
 import io.trino.connector.system.GlobalSystemConnector;
 import io.trino.metadata.CatalogManager;
@@ -24,6 +25,8 @@ import io.trino.server.ServerConfig;
 import io.trino.spi.catalog.CatalogStore;
 
 import static io.airlift.configuration.ConfigBinder.configBinder;
+import static io.trino.server.InternalCommunicationHttpClientModule.internalHttpClientModule;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class DynamicCatalogManagerModule
         extends AbstractConfigurationAwareModule
@@ -44,6 +47,11 @@ public class DynamicCatalogManagerModule
 
             configBinder(binder).bindConfig(CatalogPruneTaskConfig.class);
             binder.bind(CatalogPruneTask.class).in(Scopes.SINGLETON);
+            install(internalHttpClientModule("catalog-prune", ForCatalogPrune.class)
+                    .withConfigDefaults(config -> {
+                        config.setIdleTimeout(new Duration(30, SECONDS));
+                        config.setRequestTimeout(new Duration(10, SECONDS));
+                    }).build());
         }
         else {
             binder.bind(CatalogManager.class).to(NoOpWorkerCatalogManager.class).in(Scopes.SINGLETON);
