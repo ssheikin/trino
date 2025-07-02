6ff324926524289d058a5cbcf846e74356dd344f
Dain Sundstrom <dain@iq80.com>
2025-07-02 11:33:28 -0700

    Add simple announce service for node inventory
    Nodes only announce their internal URI every 5 seconds, as opposed to a
    full discovery announcement. The announcement server simply keeps a list
    of known workers that times out after 30 seconds. The existing internal
    node manager code handles the rest, reaching out to the node to get
    state and any other critical information.
    
    The announcer client uses the same discovery.uri property name as
    before, and the new code supports multiple names if there are multiple
    coordinators to announce to.
    
    The system used is determined by the discovery.type property which
    currently can be announce or airlift_discovery.
    
    (cherry picked from commit 9235614dbf1ea1c08bc10a9ca82b639818964a2b)
    (cherry picked from commit cda27eb1f65d64bbf5911df657a05d123f6c8536)

--- core/trino-main/src/main/java/io/trino/server/testing/TestingTrinoServer.java
+++ core/trino-main/src/main/java/io/trino/server/testing/TestingTrinoServer.java
@@ -73,8 +73,8 @@ import io.trino.metadata.SessionPropertyManager;
 import io.trino.metadata.TablePropertyManager;
 import io.trino.node.Announcer;
 import io.trino.node.InternalNode;
-import io.trino.node.TestingInternalNodeManager;
-import io.trino.node.TestingNodeManagerModule;
+import io.trino.node.InternalNodeManager;
+import io.trino.node.NodeManagerModule;
 import io.trino.security.AccessControl;
 import io.trino.security.AccessControlConfig;
 import io.trino.security.AccessControlManager;
@@ -221,7 +221,7 @@ public class TestingTrinoServer
     private final NodePartitioningManager nodePartitioningManager;
     private final ClusterMemoryManager clusterMemoryManager;
     private final LocalMemoryManager localMemoryManager;
-    private final TestingInternalNodeManager nodeManager;
+    private final InternalNodeManager nodeManager;
     private final DispatchManager dispatchManager;
     private final SqlQueryManager queryManager;
     private final SqlTaskManager taskManager;
@@ -327,7 +327,7 @@ public class TestingTrinoServer
                 .add(new StarburstDataframeModule())
                 .add(new CatalogManagerModule())
                 .add(new TransactionManagerModule())
-                .add(new TestingNodeManagerModule())
+                .add(new NodeManagerModule(VERSION))
                 .add(new ServerMainModule(VERSION))
                 .add(new CacheManagerModule())
                 .add(new TestingWarningCollectorModule())
@@ -433,7 +433,7 @@ public class TestingTrinoServer
             clusterMemoryManager = injector.getInstance(ClusterMemoryManager.class);
             statsCalculator = injector.getInstance(StatsCalculator.class);
             injector.getInstance(CertificateAuthenticatorManager.class).useDefaultAuthenticator();
-            nodeManager = injector.getInstance(TestingInternalNodeManager.class);
+            nodeManager = injector.getInstance(InternalNodeManager.class);
         }
         else {
             dispatchManager = null;
@@ -752,13 +752,13 @@ public class TestingTrinoServer
     public void registerServer(InternalNode server)
     {
         checkState(coordinator, "Current server is not a coordinator");
-        nodeManager.addNodes(server);
+        nodeManager.refreshNodes(true);
     }
 
     public void unregisterServer(InternalNode worker)
     {
         checkState(coordinator, "Current server is not a coordinator");
-        nodeManager.removeNode(worker);
+        nodeManager.refreshNodes(true);
     }
 
     public int getWorkerCount()
--- testing/trino-testing/src/main/java/io/trino/testing/DistributedQueryRunner.java
+++ testing/trino-testing/src/main/java/io/trino/testing/DistributedQueryRunner.java
@@ -115,7 +115,7 @@ public final class DistributedQueryRunner
     private static final AtomicInteger unclosedInstances = new AtomicInteger();
 
     private TestingTrinoServer coordinator;
-    private Optional<TestingTrinoServer> backupCoordinator;
+    private Optional<TestingTrinoServer> backupCoordinator = Optional.empty();
     private Consumer<Map<String, String>> registerNewWorker;
     private final InMemorySpanExporter spanExporter = InMemorySpanExporter.create();
     private final List<TestingTrinoServer> servers = new CopyOnWriteArrayList<>();
@@ -270,7 +270,7 @@ public final class DistributedQueryRunner
 
     private TestingTrinoServer createServer(
             boolean coordinator,
-            Map<String, String> extraCoordinatorProperties,
+            Map<String, String> extraProperties,
             String environment,
             Module additionalModule,
             Optional<Path> baseDataDir,
@@ -281,9 +281,18 @@ public final class DistributedQueryRunner
             List<EventListener> eventListeners,
             Optional<ModelConnectionSpecsLoader> modelConnectionSpecsLoader)
     {
+        if (this.coordinator != null) {
+            String discoveryUri = this.coordinator.getCurrentNode().getInternalUri() +
+                    backupCoordinator.map(backup -> "," + backup.getCurrentNode().getInternalUri()).orElse("");
+            extraProperties = ImmutableMap.<String, String>builder()
+                    .putAll(extraProperties)
+                    .put("discovery.uri", discoveryUri)
+                    .buildOrThrow();
+        }
+
         TestingTrinoServer server = closer.register(createTestingTrinoServer(
                 coordinator,
-                extraCoordinatorProperties,
+                extraProperties,
                 environment,
                 additionalModule,
                 baseDataDir,
@@ -299,7 +308,8 @@ public final class DistributedQueryRunner
                 },
                 modelConnectionSpecsLoader));
         servers.add(server);
-        if (!coordinator) {
+
+        if (this.coordinator != null) {
             InternalNode currentNode = server.getCurrentNode();
             this.coordinator.registerServer(currentNode);
             this.backupCoordinator.ifPresent(backup -> backup.registerServer(currentNode));
