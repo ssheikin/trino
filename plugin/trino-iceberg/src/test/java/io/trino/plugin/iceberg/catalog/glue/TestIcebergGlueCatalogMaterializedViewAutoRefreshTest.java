fd1be07d7bcbb205418d0b0668fee36148365304
Konrad Dziedzic <konraddziedzic@gmail.com>
2025-07-25 14:53:14 +0200

    Move TestIcebergGlueCatalogMaterializedViewAutoRefreshTest to glue package

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/glue/TestIcebergGlueCatalogMaterializedViewAutoRefreshTest.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/glue/TestIcebergGlueCatalogMaterializedViewAutoRefreshTest.java
@@ -11,9 +11,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.trino.plugin.iceberg;
+package io.trino.plugin.iceberg.catalog.glue;
 
 import com.google.common.collect.ImmutableMap;
+import io.trino.plugin.iceberg.TestIcebergHiveCatalogMaterializedViewAutoRefreshTest;
 
 import java.util.Map;
 
