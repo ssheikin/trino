079ffbe210fae61d285d1c886735ba7669757319
Raunaq Morarka <raunaqmorarka@gmail.com>
2025-08-14 11:31:36 +0530

    Increase s3.max-error-retries to 20
    Helps with reducing failures of remove_orphan_files
    due to S3 throttling
    
    (cherry picked from commit e22c129e8f793a0b68cf415054166d6e03b65d4d)
    (cherry picked from commit 77c665e1ec1c19c2696424029b5758b990e6f649)

--- lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemConfig.java
+++ lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemConfig.java
@@ -180,7 +180,7 @@ public class S3FileSystemConfig
     private Set<String> nonProxyHosts = ImmutableSet.of();
     private ObjectCannedAcl objectCannedAcl = ObjectCannedAcl.NONE;
     private RetryMode retryMode = RetryMode.LEGACY;
-    private int maxErrorRetries = 10;
+    private int maxErrorRetries = 20;
     private boolean supportsExclusiveCreate = true;
     private boolean crossRegionAccessEnabled;
     private String applicationId = "Trino";
