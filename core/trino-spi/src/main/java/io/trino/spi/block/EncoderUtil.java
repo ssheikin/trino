da6b8eebc449bcd5ba9bcf2dd5b0c066d5c488ae
James Petty <pettja@amazon.com>
2025-08-18 13:11:51 -0400

    Mark EncoderUtil#encodeNullsAsBits(SliceOutput, Block) deprecated
    (cherry picked from commit 624542d89f0b36e70808ec3c74a5910f0c3070d3)
    (cherry picked from commit d1e150d8a0bc27249fa0e5e04686eeee6924cae9)

--- core/trino-spi/src/main/java/io/trino/spi/block/EncoderUtil.java
+++ core/trino-spi/src/main/java/io/trino/spi/block/EncoderUtil.java
@@ -71,7 +71,10 @@ public final class EncoderUtil
 
     /**
      * Append null values for the block as a stream of bits.
+     *
+     * @deprecated Use {@link EncoderUtil#encodeNullsAsBits(SliceOutput, boolean[], int, int)} instead
      */
+    @Deprecated(forRemoval = true)
     @SuppressWarnings({"NarrowingCompoundAssignment", "ImplicitNumericConversion"})
     public static void encodeNullsAsBits(SliceOutput sliceOutput, Block block)
     {
