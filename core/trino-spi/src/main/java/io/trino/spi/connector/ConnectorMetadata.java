9ba9d1ad9f04ee049f0bf8bb739f57044ebc66b3
David Phillips <david@acz.org>
2025-08-20 12:13:45 -0700

    Deprecate unused delegated materialized view refresh
    (cherry picked from commit cf1756bf879dbd0b6e52eb61734ee9f4a355447b)
    (cherry picked from commit 4ceea3ef0136da8f64a11e048e1063fbcc6c7651)

--- core/trino-spi/src/main/java/io/trino/spi/connector/ConnectorMetadata.java
+++ core/trino-spi/src/main/java/io/trino/spi/connector/ConnectorMetadata.java
@@ -788,6 +788,7 @@ public interface ConnectorMetadata
     /**
      * Returns true if materialized view refresh should be delegated to connector using {@link ConnectorMetadata#refreshMaterializedView}
      */
+    @Deprecated(since = "477", forRemoval = true)
     default boolean delegateMaterializedViewRefreshToConnector(ConnectorSession session, SchemaTableName viewName)
     {
         throw new TrinoException(NOT_SUPPORTED, "This connector does not support materialized views");
@@ -796,6 +797,7 @@ public interface ConnectorMetadata
     /**
      * Refresh materialized view
      */
+    @Deprecated(since = "477", forRemoval = true)
     default CompletableFuture<?> refreshMaterializedView(ConnectorSession session, SchemaTableName viewName)
     {
         throw new TrinoException(NOT_SUPPORTED, "This connector does not support materialized views");
