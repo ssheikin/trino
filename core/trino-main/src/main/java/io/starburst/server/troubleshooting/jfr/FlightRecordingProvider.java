afe67fb22e88f24422c2f099f7e2587038150555
Krzysztof Sobolewski <krzysztof.sobolewski@starburstdata.com>
2025-09-23 10:38:08 +0200

    [TROUBLESHOOTING] Remove obsolete method from FlightRecordingProvider
    It's been unused since commit 54a245a270b8528ca28de7be68a1dba3297d31e5.
    
    (cherry picked from commit 99109b9d3ae26500083c133e6b55f88e3c3d027d)

--- core/trino-main/src/main/java/io/starburst/server/troubleshooting/jfr/FlightRecordingProvider.java
+++ core/trino-main/src/main/java/io/starburst/server/troubleshooting/jfr/FlightRecordingProvider.java
@@ -10,41 +10,24 @@
 package io.starburst.server.troubleshooting.jfr;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.google.inject.Inject;
 import io.starburst.server.troubleshooting.TroubleshootingContext;
 import io.starburst.server.troubleshooting.providers.TroubleshootingProvider;
-import io.trino.execution.QueryInfo;
-import io.trino.execution.QueryManager;
-import io.trino.execution.StageInfo;
-import io.trino.execution.StagesInfo;
-import io.trino.execution.TaskInfo;
-import io.trino.execution.TaskStatus;
-import io.trino.spi.QueryId;
 
 import java.io.InputStream;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
-import static com.google.common.collect.ImmutableSet.toImmutableSet;
-import static io.trino.execution.StagesInfo.getAllStages;
 import static java.util.Objects.requireNonNull;
 
 public class FlightRecordingProvider
         implements TroubleshootingProvider
 {
     private final FlightRecordingFactory recordingFactory;
-    private final QueryManager queryManager;
 
     @Inject
-    public FlightRecordingProvider(FlightRecordingFactory recordingFactory, QueryManager queryManager)
+    public FlightRecordingProvider(FlightRecordingFactory recordingFactory)
     {
         this.recordingFactory = requireNonNull(recordingFactory, "recordingFactory is null");
-        this.queryManager = requireNonNull(queryManager, "queryManager is null");
     }
 
     @Override
@@ -59,30 +42,6 @@ public class FlightRecordingProvider
         FlightRecording recording = context.getOrThrow(FlightRecording.class);
         recording.retainForNodes(context.getJfrCollectedNodes());
         recording.finish();
-    }
-
-    private Set<String> getProcessingNodesForQuery(QueryId queryId)
-    {
-        try {
-            QueryInfo queryInfo = queryManager.getFullQueryInfo(queryId);
-            return queryInfo.getStages().map(this::getNodeIdsProcessingQuery).orElse(ImmutableSet.of());
-        }
-        catch (Exception e) {
-            return Set.of();
-        }
-    }
-
-    private Set<String> getNodeIdsProcessingQuery(StagesInfo outputStage)
-    {
-        List<TaskInfo> tasks = getAllStages(Optional.of(outputStage)).stream()
-                .map(StageInfo::getTasks)
-                .flatMap(Collection::stream)
-                .collect(toImmutableList());
-
-        return tasks.stream()
-                .map(TaskInfo::taskStatus)
-                .map(TaskStatus::getNodeId)
-                .collect(toImmutableSet());
     }
 
     @Override
