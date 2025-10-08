c29c896308bc1ae9c5a9475c929e8941de7a9bb1
Yuya Ebihara <ebyhry@gmail.com>
2025-10-09 05:54:49 +0900

    Disallow dropping sort column explicitly in Iceberg
    (cherry picked from commit ab7fdcefe5b4bf4a1845859b6e23d3982743fd89)
    (cherry picked from commit b30f726986d5bcd89d0725a5346870f0159dc6fe)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
@@ -3207,6 +3207,11 @@ public class IcebergMetadata
         if (isPartitionColumn) {
             throw new TrinoException(NOT_SUPPORTED, "Cannot drop partition field: " + name);
         }
+        boolean isSortColumn = icebergTable.sortOrder().fields().stream()
+                .anyMatch(field -> field.sourceId() == fieldId);
+        if (isSortColumn) {
+            throw new TrinoException(NOT_SUPPORTED, "Cannot drop sort field: " + name);
+        }
         int currentSpecId = icebergTable.spec().specId();
         boolean columnUsedInOlderPartitionSpecs = icebergTable.specs().entrySet().stream()
                 .filter(spec -> spec.getValue().specId() != currentSpecId)
