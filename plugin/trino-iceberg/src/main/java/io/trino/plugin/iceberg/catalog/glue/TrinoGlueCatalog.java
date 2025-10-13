6d9e7626f33831daa481ceee40293d56f9c742b7
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-10-13 21:03:28 +0200

    Use precompiled pattern for replacing a prefix
    (cherry picked from commit 0cae17a27bcd1054c80a754531d8f404feed4320)
    (cherry picked from commit 55535b39411fd187cbd1e9c0840754bff73f507a)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/glue/TrinoGlueCatalog.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/glue/TrinoGlueCatalog.java
@@ -99,6 +99,7 @@ import java.util.concurrent.Executor;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
@@ -171,6 +172,7 @@ public class TrinoGlueCatalog
     private static final Logger LOG = Logger.get(TrinoGlueCatalog.class);
 
     private static final int PER_QUERY_CACHES_SIZE = 1000;
+    private static final Pattern METADATA_PATTERN = Pattern.compile("/metadata/[^/]*$");
 
     private final String trinoVersion;
     private final boolean cacheTableMetadata;
@@ -737,7 +739,7 @@ public class TrinoGlueCatalog
         if (metadataLocation == null) {
             throw new TrinoException(ICEBERG_INVALID_METADATA, format("Table %s is missing [%s] property", schemaTableName, METADATA_LOCATION_PROP));
         }
-        String tableLocation = metadataLocation.replaceFirst("/metadata/[^/]*$", "");
+        String tableLocation = METADATA_PATTERN.matcher(metadataLocation).replaceFirst("");
         deleteTableDirectory(fileSystemFactory.create(session), schemaTableName, tableLocation);
         invalidateTableCache(schemaTableName);
     }
