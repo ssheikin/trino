49b688a63e59759857636a1777273f4ad99c7c24
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-10-14 16:13:53 +0200

    Disable Snowflake tests
    (cherry picked from commit d08101985db3a312f9239c3352e54973dc0f55e4)
    (cherry picked from commit f50fed25c97bd3bb26b02a7fd3443f2f7c5c989f)

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/snowflake/TestIcebergSnowflakeCatalogConnectorSmokeTest.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/snowflake/TestIcebergSnowflakeCatalogConnectorSmokeTest.java
@@ -23,6 +23,7 @@ import io.trino.testing.QueryRunner;
 import io.trino.testing.TestingConnectorBehavior;
 import io.trino.tpch.TpchTable;
 import org.apache.iceberg.FileFormat;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -46,6 +47,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @TestInstance(PER_CLASS)
+@Disabled
 public class TestIcebergSnowflakeCatalogConnectorSmokeTest
         extends BaseIcebergConnectorSmokeTest
 {
