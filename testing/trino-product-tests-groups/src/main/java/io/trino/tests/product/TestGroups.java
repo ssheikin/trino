4c698c0cbf5468c08ed60faf5e5c40f349db9d94
Yuya Ebihara <ebyhry@gmail.com>
2025-10-15 09:09:55 +0900

    Stop running product tests with Databricks 11.3 LTS
    (cherry picked from commit 57b16dad4c0b51d59565713c8d08c9c4dad841cc)
    (cherry picked from commit 164b1392fa5c2ffae5c3e6dd1ab9857d5ff96944)

--- testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
+++ testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
@@ -100,13 +100,11 @@ public final class TestGroups
     public static final String DELTA_LAKE_AZURE = "delta-lake-azure";
     public static final String DELTA_LAKE_GCS = "delta-lake-gcs";
     public static final String DELTA_LAKE_DATABRICKS = "delta-lake-databricks";
-    public static final String DELTA_LAKE_DATABRICKS_122 = "delta-lake-databricks-122";
     public static final String DELTA_LAKE_DATABRICKS_133 = "delta-lake-databricks-133";
     public static final String DELTA_LAKE_DATABRICKS_143 = "delta-lake-databricks-143";
     public static final String DELTA_LAKE_DATABRICKS_154 = "delta-lake-databricks-154";
     public static final String DELTA_LAKE_DATABRICKS_UNITY = "delta-lake-databricks-unity";
     public static final String DELTA_LAKE_DATABRICKS_UNITY_CREDENTIALS_VENDING = "delta-lake-databricks-unity-credentials-vending";
-    public static final String DELTA_LAKE_EXCLUDE_113 = "delta-lake-exclude-113";
     // TODO: Remove it once we support generatedColumns, particularly for writes in Delta Lake
     public static final String DELTA_LAKE_EXCLUDE_164 = "delta-lake-exclude-164";
     public static final String HIVE_DATABRICKS_UNITY = "hive-databricks-unity";
