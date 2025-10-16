70c83a2e69e816aa5071b70a0ca8df22c3a35a0c
Yuya Ebihara <ebyhry@gmail.com>
2025-10-16 09:09:27 +0900

    Replace deprecated NestedField.of with NestedField.optional
    (cherry picked from commit 07c32f53416c311daf8b1af7df9bedd7c75a87c9)
    (cherry picked from commit 1a6721e7af98a9a9b4e6a6d89668ac31dbc48ed2)

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergV2.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergV2.java
@@ -1483,7 +1483,7 @@ public class TestIcebergV2
         catalog.newCreateTableTransaction(
                         SESSION,
                         schemaTableName,
-                        new Schema(Types.NestedField.of(1, true, "x", Types.LongType.get())),
+                        new Schema(Types.NestedField.optional(1, "x", Types.LongType.get())),
                         PartitionSpec.unpartitioned(),
                         SortOrder.unsorted(),
                         Optional.ofNullable(catalog.defaultTableLocation(SESSION, schemaTableName)),
