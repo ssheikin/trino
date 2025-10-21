2bc01afe56ae5e022dba251119e6541d10ae0638
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-10-21 10:01:56 +0200

    Fix DROP CATALOG spurious failure if cleanupQuery implemented
    Before the change, DROP CATALOG could fail if the metadata of the
    catalog being dropped implements `cleanupQuery` and the `cleanupQuery`
    checks catalog session properties. This was because inspecting session
    properties requires catalog to exist and that could have been pruned
    away. This fixes coordinator catalog pruning to retain catalogs used in
    active transactions.
    
    (cherry picked from commit aee92a6373adb892e5a42fdda24271bebea40843)

--- core/trino-main/src/main/java/io/trino/connector/CatalogPruneTask.java
+++ core/trino-main/src/main/java/io/trino/connector/CatalogPruneTask.java
@@ -141,8 +141,8 @@ public class CatalogPruneTask
         List<CatalogHandle> activeCatalogs = getActiveCatalogs();
         pruneWorkerCatalogs(online, activeCatalogs);
 
-        // prune all inactive catalogs - we pass an empty set here because manager always retains active catalogs
-        connectorServicesProvider.pruneCatalogs(ImmutableSet.of());
+        // prune inactive catalogs locally
+        connectorServicesProvider.pruneCatalogs(ImmutableSet.copyOf(activeCatalogs));
     }
 
     void pruneWorkerCatalogs(Set<ServiceDescriptor> online, List<CatalogHandle> activeCatalogs)
