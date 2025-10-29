62e9bd72b0745b891895df569c5760a0dd561e4b
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-10-30 00:34:19 +0100

    Replace Optional<Integer> with OptionalInt to avoid boxing
    (cherry picked from commit bde3174f7f275e4342b8c7b8182b34e8792b3c3b)
    (cherry picked from commit d7f1339e528c07e1e5dd8269ac2409bcce72ed28)

--- core/trino-main/src/main/java/io/trino/operator/HashAggregationOperator.java
+++ core/trino-main/src/main/java/io/trino/operator/HashAggregationOperator.java
@@ -37,6 +37,7 @@ import io.trino.sql.planner.plan.PlanNodeId;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.OptionalLong;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -65,7 +66,7 @@ public class HashAggregationOperator
         private final Step step;
         private final boolean produceDefaultOutput;
         private final List<AggregatorFactory> aggregatorFactories;
-        private final Optional<Integer> groupIdChannel;
+        private final OptionalInt groupIdChannel;
 
         private final int expectedGroups;
         private final Optional<DataSize> maxPartialMemory;
@@ -87,7 +88,7 @@ public class HashAggregationOperator
                 List<Integer> globalAggregationGroupIds,
                 Step step,
                 List<AggregatorFactory> aggregatorFactories,
-                Optional<Integer> groupIdChannel,
+                OptionalInt groupIdChannel,
                 int expectedGroups,
                 Optional<DataSize> maxPartialMemory,
                 FlatHashStrategyCompiler hashStrategyCompiler,
@@ -123,7 +124,7 @@ public class HashAggregationOperator
                 Step step,
                 boolean produceDefaultOutput,
                 List<AggregatorFactory> aggregatorFactories,
-                Optional<Integer> groupIdChannel,
+                OptionalInt groupIdChannel,
                 int expectedGroups,
                 Optional<DataSize> maxPartialMemory,
                 boolean spillEnabled,
@@ -161,7 +162,7 @@ public class HashAggregationOperator
                 Step step,
                 boolean produceDefaultOutput,
                 List<AggregatorFactory> aggregatorFactories,
-                Optional<Integer> groupIdChannel,
+                OptionalInt groupIdChannel,
                 int expectedGroups,
                 Optional<DataSize> maxPartialMemory,
                 boolean spillEnabled,
@@ -254,7 +255,7 @@ public class HashAggregationOperator
     private final Step step;
     private final boolean produceDefaultOutput;
     private final List<AggregatorFactory> aggregatorFactories;
-    private final Optional<Integer> groupIdChannel;
+    private final OptionalInt groupIdChannel;
     private final int expectedGroups;
     private final Optional<DataSize> maxPartialMemory;
     private final boolean spillEnabled;
@@ -288,7 +289,7 @@ public class HashAggregationOperator
             Step step,
             boolean produceDefaultOutput,
             List<AggregatorFactory> aggregatorFactories,
-            Optional<Integer> groupIdChannel,
+            OptionalInt groupIdChannel,
             int expectedGroups,
             Optional<DataSize> maxPartialMemory,
             boolean spillEnabled,
--- core/trino-main/src/test/java/io/trino/execution/TestingRemoteTaskFactory.java
+++ core/trino-main/src/test/java/io/trino/execution/TestingRemoteTaskFactory.java
@@ -48,6 +48,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
@@ -187,7 +188,7 @@ public class TestingRemoteTaskFactory
                     DataSize.of(0, BYTE),
                     DataSize.of(0, BYTE),
                     DataSize.of(0, BYTE),
-                    Optional.empty(),
+                    OptionalInt.empty(),
                     DataSize.of(0, BYTE),
                     DataSize.of(0, BYTE),
                     DataSize.of(0, BYTE),
