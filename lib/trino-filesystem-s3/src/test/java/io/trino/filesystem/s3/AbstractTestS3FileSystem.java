101f3c364d1105763d449cd94c5732499a4d516d
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-11-16 21:25:08 +0100

    Disable existence checks in S3OutputFile.create
    Restore the behavior before as it was before support for conditional
    writes was introduced.  File existence checks are not implemented for
    multi-part upload code path (the `FileAlreadyExistsException` is throw
    only when multi-part did not take place). Furthermore, file existence
    checks can break file writes when the call is retried on the AWS SDK
    level. Fixing both these issues is possible, but not necessary: users of
    `TrinoOutputFile.create` cannot assume particular behavior and file
    system implementation is allowed to overwrite existing files.
    
    Atomic writes are necessary for Delta's connector transaction log
    synchronizer `S3ConditionalWriteLogSynchronizer`, but is explicitly
    invokes `TrinoOutputFile.createExclusive` API.
    
    (cherry picked from commit d3a7fd587b3b47bbe4ffbb621631c7f3caf7e4f4)
    (cherry picked from commit e2af42f3e38d0b86e6af6c3c5dda2e91c9ab9d16)

--- lib/trino-filesystem-s3/src/test/java/io/trino/filesystem/s3/AbstractTestS3FileSystem.java
+++ lib/trino-filesystem-s3/src/test/java/io/trino/filesystem/s3/AbstractTestS3FileSystem.java
@@ -103,6 +103,12 @@ public abstract class AbstractTestS3FileSystem
     }
 
     @Override
+    protected boolean isCreateExclusive()
+    {
+        return false;
+    }
+
+    @Override
     protected final boolean supportsRenameFile()
     {
         return false;
