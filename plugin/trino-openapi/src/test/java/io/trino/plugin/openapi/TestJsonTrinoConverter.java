c05859d71cf0e8fd9b329521978c51aa725875ae
Mathias Kools <mathias.kools@starburstdata.com>
2025-11-18 17:58:00 -0500

    Ensure imported openapi tests start with Test
    (cherry picked from commit 8ef08136a07a814a3055b15d1e606cafdcbcad05)

--- plugin/trino-openapi/src/test/java/io/trino/plugin/openapi/TestJsonTrinoConverter.java
+++ plugin/trino-openapi/src/test/java/io/trino/plugin/openapi/TestJsonTrinoConverter.java
@@ -21,7 +21,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class JsonTrinoConverterTest
+class TestJsonTrinoConverter
 {
     @Test
     void shouldConvertDifferentDateTimeFormatsIntoLong()
