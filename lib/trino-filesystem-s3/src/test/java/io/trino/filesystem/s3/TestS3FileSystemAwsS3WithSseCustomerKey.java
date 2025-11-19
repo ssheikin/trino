49138ac9a9e20900a22fc15e75e3fad2ab3ca59c
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-11-19 14:12:10 +0100

    Throw correctly from S3 createExclusive with larger data
    Before the change, the `S3OutputStream` configured to write exclusively
    (using S3 conditional writes), would detect object existence and throw
    `FileAlreadyExistsException` only in the case of single upload, but not
    when multi-part upload is used.
    
    (cherry picked from commit c0130d572dd36ab9acba046ac153f7fd93820d87)
    (cherry picked from commit 4b88601bc7de8d7172a48826ffd874ff5aee3cbc)

--- lib/trino-filesystem-s3/src/test/java/io/trino/filesystem/s3/TestS3FileSystemAwsS3WithSseCustomerKey.java
+++ lib/trino-filesystem-s3/src/test/java/io/trino/filesystem/s3/TestS3FileSystemAwsS3WithSseCustomerKey.java
@@ -36,6 +36,7 @@ import java.util.function.Function;
 
 import static io.trino.filesystem.s3.S3FileSystemConfig.S3SseType.CUSTOMER;
 import static io.trino.testing.SystemEnvironmentUtils.requireEnv;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestS3FileSystemAwsS3WithSseCustomerKey
         extends AbstractTestS3FileSystem
@@ -99,6 +100,9 @@ public class TestS3FileSystemAwsS3WithSseCustomerKey
     @Override
     protected S3FileSystemFactory createS3FileSystemFactory()
     {
+        DataSize streamingPartSize = DataSize.valueOf("5.5MB");
+        assertThat(streamingPartSize).describedAs("Configured part size should be less than test's larger file size")
+                .isLessThan(LARGER_FILE_DATA_SIZE);
         return new S3FileSystemFactory(
                 OpenTelemetry.noop(),
                 new S3FileSystemConfig()
@@ -107,7 +111,7 @@ public class TestS3FileSystemAwsS3WithSseCustomerKey
                         .setRegion(region)
                         .setSseType(CUSTOMER)
                         .setSseCustomerKey(s3SseCustomerKey.key())
-                        .setStreamingPartSize(DataSize.valueOf("5.5MB")),
+                        .setStreamingPartSize(streamingPartSize),
                 new S3FileSystemStats());
     }
 
