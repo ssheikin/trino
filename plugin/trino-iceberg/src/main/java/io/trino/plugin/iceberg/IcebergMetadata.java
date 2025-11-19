8ea2cdb7fcc064836aeaf0667cbc85bb6e78285a
Yuya Ebihara <ebyhry@gmail.com>
2025-11-19 11:52:39 +0900

    Add clean_expired_metadata option to Iceberg expire_snapshots
    (cherry picked from commit 5f5f506a22158b4a8328d5824d6fd06a904cb939)
    (cherry picked from commit b08e7ee8f6017bd2e07963f26dca6428b3b8fc4b)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergMetadata.java
@@ -1846,11 +1846,12 @@ public class IcebergMetadata
         int retainLast = firstNonNull(
                 (Integer) executeProperties.get("retain_last"),
                 propertyAsInt(icebergTable.properties(), MIN_SNAPSHOTS_TO_KEEP, MIN_SNAPSHOTS_TO_KEEP_DEFAULT));
+        boolean cleanExpiredMetadata = (boolean) executeProperties.get("clean_expired_metadata");
 
         return Optional.of(new IcebergTableExecuteHandle(
                 tableHandle.getSchemaTableName(),
                 EXPIRE_SNAPSHOTS,
-                new IcebergExpireSnapshotsHandle(retentionThreshold, retainLast),
+                new IcebergExpireSnapshotsHandle(retentionThreshold, retainLast, cleanExpiredMetadata),
                 icebergTable.location(),
                 icebergTable.io().properties(),
                 tableHandle.getFormatVersion()));
@@ -2619,6 +2620,7 @@ public class IcebergMetadata
             table.expireSnapshots()
                     .expireOlderThan(session.getStart().toEpochMilli() - retention.toMillis())
                     .retainLast(expireSnapshotsHandle.retainLast())
+                    .cleanExpiredMetadata(expireSnapshotsHandle.cleanExpiredMetadata())
                     .planWith(icebergScanExecutor)
                     .commit();
         }
