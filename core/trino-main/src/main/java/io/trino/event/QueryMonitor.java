1391b3083b09b15534d75bbbdd278c54993ed684
Raunaq Morarka <raunaqmorarka@gmail.com>
2025-11-21 00:20:40 +0530

    Fix recording of query completion event for query failures before execution
    queryImmediateFailureEvent can be called for queries which fail during
    analysis, resource waiting or planning.
    It is updated to populate the relevant metrics
    
    (cherry picked from commit f7824bfdd4f782df7fa42b7b848839924251bbf3)
    (cherry picked from commit c5137df988df0a432081bc89405c52fcc6a0654b)

--- core/trino-main/src/main/java/io/trino/event/QueryMonitor.java
+++ core/trino-main/src/main/java/io/trino/event/QueryMonitor.java
@@ -185,6 +185,7 @@ public class QueryMonitor
 
     public void queryImmediateFailureEvent(BasicQueryInfo queryInfo, ExecutionFailureInfo failure)
     {
+        BasicQueryStats queryStats = queryInfo.getQueryStats();
         eventListenerManager.queryCompleted(requiresAnonymizedPlan -> new QueryCompletedEvent(
                 new QueryMetadata(
                         queryInfo.getQueryId().toString(),
@@ -205,16 +206,16 @@ public class QueryMonitor
                 new QueryStatistics(
                         Duration.ZERO,
                         Duration.ZERO,
-                        Duration.ZERO,
-                        queryInfo.getQueryStats().getQueuedTime().toJavaTime(),
+                        queryStats.getElapsedTime().toJavaTime(),
+                        queryStats.getQueuedTime().toJavaTime(),
                         Optional.empty(),
                         Optional.empty(),
+                        Optional.of(queryStats.getResourceWaitingTime().toJavaTime()),
+                        Optional.of(queryStats.getAnalysisTime().toJavaTime()),
+                        Optional.of(queryStats.getPlanningTime().toJavaTime()),
                         Optional.empty(),
                         Optional.empty(),
-                        Optional.empty(),
-                        Optional.empty(),
-                        Optional.empty(),
-                        Optional.empty(),
+                        Optional.of(queryStats.getExecutionTime().toJavaTime()),
                         Optional.empty(),
                         Optional.empty(),
                         Optional.empty(),
