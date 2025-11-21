2b5add5c9646e0f449026520720d0cb04cf50b1d
Yuya Ebihara <ebyhry@gmail.com>
2025-11-21 12:37:29 +0900

    Remove redundant line after license in OpenAPI
    (cherry picked from commit 59542cf3c328bfce686c9d68c0b21684fe8788a2)

--- plugin/trino-openapi/src/test/java/io/trino/plugin/openapi/TestOpenApiSpec.java
+++ plugin/trino-openapi/src/test/java/io/trino/plugin/openapi/TestOpenApiSpec.java
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.trino.plugin.openapi;
 
 import io.swagger.v3.oas.models.PathItem;
