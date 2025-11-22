25c0140f94e432cb036607ddcf4a8c24e33a6b44
Yuya Ebihara <ebyhry@gmail.com>
2025-11-22 09:54:37 +0900

    Add binary varying mapping to Redshift connector
    (cherry picked from commit 9812cd568183218904e874eaac7782dda1c9f452)
    (cherry picked from commit e956e721e397c560e70ba72b6bbe063b1828dfce)

--- plugin/trino-redshift/src/main/java/io/trino/plugin/redshift/RedshiftClient.java
+++ plugin/trino-redshift/src/main/java/io/trino/plugin/redshift/RedshiftClient.java
@@ -646,6 +646,12 @@ public class RedshiftClient
                     RedshiftClient::readTime,
                     RedshiftClient::writeTime));
         }
+        if ("binary varying".equals(type.jdbcTypeName().orElse("")) || type.jdbcType() == Types.LONGVARBINARY) {
+            return Optional.of(ColumnMapping.sliceMapping(
+                    VARBINARY,
+                    varbinaryReadFunction(),
+                    varbinaryWriteFunction()));
+        }
 
         if ("binary varying".equals(type.jdbcTypeName().orElse("")) || type.jdbcType() == Types.LONGVARBINARY) {
             return Optional.of(ColumnMapping.sliceMapping(
@@ -710,12 +716,6 @@ public class RedshiftClient
                                 : createUnboundedVarcharType(),
                         true));
             }
-
-            case Types.LONGVARBINARY:
-                return Optional.of(ColumnMapping.sliceMapping(
-                        VARBINARY,
-                        varbinaryReadFunction(),
-                        varbinaryWriteFunction()));
 
             case Types.DATE:
                 return Optional.of(ColumnMapping.longMapping(
