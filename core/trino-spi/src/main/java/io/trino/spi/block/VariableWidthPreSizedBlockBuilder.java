b45e9db0a098d940273f069402aae3ddd563b211
Raunaq Morarka <raunaqmorarka@gmail.com>
2025-11-26 18:53:02 +0530

    Avoid creating Slice in VariableWidthPreSizedBlockBuilder#append
    (cherry picked from commit 9dc80b094f9d56d0ca22e34e992ccad57c0b7b7c)

--- core/trino-spi/src/main/java/io/trino/spi/block/VariableWidthPreSizedBlockBuilder.java
+++ core/trino-spi/src/main/java/io/trino/spi/block/VariableWidthPreSizedBlockBuilder.java
@@ -16,12 +16,13 @@ package io.trino.spi.block;
 import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import static io.airlift.slice.Slices.EMPTY_SLICE;
+import static io.trino.spi.block.BlockUtil.calculateNewArraySize;
+import static io.trino.spi.block.PageBuilderStatus.DEFAULT_MAX_PAGE_SIZE_IN_BYTES;
 import static io.trino.spi.block.PreSizedBlockBuilder.checkArgument;
-import static java.lang.Math.addExact;
+import static java.lang.Math.min;
 
 public class VariableWidthPreSizedBlockBuilder
         implements PreSizedBlockBuilder
@@ -30,18 +31,24 @@ public class VariableWidthPreSizedBlockBuilder
 
     private final boolean[] isNull;
     private final int[] offsets;
-    private final List<Slice> values;
 
+    private byte[] bytes;
     private int positionCount;
     private boolean hasNullValue;
     private boolean hasNonNullValue;
 
     public VariableWidthPreSizedBlockBuilder(int expectedEntries)
     {
+        this(expectedEntries, expectedEntries * 8);
+    }
+
+    private VariableWidthPreSizedBlockBuilder(int expectedEntries, int expectedBytes)
+    {
         checkArgument(expectedEntries >= 0, "expectedEntries %s must be positive", expectedEntries);
+        checkArgument(expectedBytes >= 0, "expectedBytes %s must be positive", expectedBytes);
         this.isNull = new boolean[expectedEntries];
         this.offsets = new int[expectedEntries + 1];
-        this.values = new ArrayList<>(expectedEntries);
+        this.bytes = new byte[expectedBytes];
     }
 
     @Override
@@ -55,23 +62,43 @@ public class VariableWidthPreSizedBlockBuilder
 
     public void writeEntry(Slice source)
     {
-        addNonNullEntry(source, source.length());
+        int bytesWritten = source.length();
+        ensureFreeSpace(bytesWritten);
+
+        source.getBytes(0, bytes, offsets[positionCount], bytesWritten);
+        offsets[positionCount + 1] = offsets[positionCount] + bytesWritten;
+
+        positionCount++;
+        hasNonNullValue = true;
     }
 
     @Override
     public void append(ValueBlock block, int position)
     {
         VariableWidthBlock variableWidthBlock = (VariableWidthBlock) block;
+        int bytesWritten = 0;
         if (variableWidthBlock.isNull(position)) {
             isNull[positionCount] = true;
-            offsets[positionCount + 1] = offsets[positionCount];
-            positionCount++;
             hasNullValue = true;
         }
         else {
-            Slice value = variableWidthBlock.getSlice(position);
-            addNonNullEntry(value, value.length());
+            int rawArrayBase = variableWidthBlock.getRawArrayBase();
+            int[] rawOffsets = variableWidthBlock.getRawOffsets();
+            int startValueOffset = rawOffsets[rawArrayBase + position];
+            int endValueOffset = rawOffsets[rawArrayBase + position + 1];
+            int length = endValueOffset - startValueOffset;
+            ensureFreeSpace(length);
+
+            Slice rawSlice = variableWidthBlock.getRawSlice();
+            byte[] rawByteArray = rawSlice.byteArray();
+            int byteArrayOffset = rawSlice.byteArrayOffset();
+
+            System.arraycopy(rawByteArray, byteArrayOffset + startValueOffset, bytes, offsets[positionCount], length);
+            bytesWritten = length;
+            hasNonNullValue = true;
         }
+        offsets[positionCount + 1] = offsets[positionCount] + bytesWritten;
+        positionCount++;
     }
 
     @Override
@@ -80,33 +107,28 @@ public class VariableWidthPreSizedBlockBuilder
         if (!hasNonNullValue) {
             return RunLengthEncodedBlock.create(NULL_VALUE_BLOCK, positionCount);
         }
-        return new VariableWidthBlock(0, positionCount, asSlice(), offsets, hasNullValue ? isNull : null);
+        return new VariableWidthBlock(0, positionCount, Slices.wrappedBuffer(bytes, 0, offsets[positionCount]), offsets, hasNullValue ? isNull : null);
     }
 
     @Override
     public PreSizedBlockBuilder newBlockBuilderLike(int expectedEntries)
     {
-        return new VariableWidthPreSizedBlockBuilder(expectedEntries);
-    }
-
-    private void addNonNullEntry(Slice value, int bytesWritten)
-    {
-        values.add(value);
-        offsets[positionCount + 1] = addExact(offsets[positionCount], bytesWritten);
-
-        positionCount++;
-        hasNonNullValue = true;
-    }
-
-    private Slice asSlice()
-    {
-        int totalSize = offsets[positionCount];
-        Slice slice = Slices.allocate(totalSize);
-        int offset = 0;
-        for (Slice value : values) {
-            slice.setBytes(offset, value);
-            offset += value.length();
+        if (positionCount == 0) {
+            return new VariableWidthPreSizedBlockBuilder(expectedEntries);
         }
-        return slice;
+        double bytesPerEntry = (double) offsets[positionCount] / positionCount;
+        int expectedBytes = (int) min(bytesPerEntry * expectedEntries, DEFAULT_MAX_PAGE_SIZE_IN_BYTES);
+        return new VariableWidthPreSizedBlockBuilder(expectedEntries, expectedBytes);
+    }
+
+    private void ensureFreeSpace(int extraBytesCapacity)
+    {
+        int requiredSize = offsets[positionCount] + extraBytesCapacity;
+        if (bytes.length >= requiredSize) {
+            return;
+        }
+
+        int newSize = calculateNewArraySize(requiredSize, bytes.length);
+        bytes = Arrays.copyOf(bytes, newSize);
     }
 }
