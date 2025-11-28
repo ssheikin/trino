e5d88f159bb2ad89adf5e07c629ea53ccc7c6fdd
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-11-28 12:08:10 +0100

    Move inner enum to top
    It was initially added next to other enums, but now they are gone. Move
    to a better place.
    
    (cherry picked from commit 74acad35cfeecb536a24d03824bf06573a93b4a0)
    (cherry picked from commit a4e4413b58fbd0bba1c77f42413c7a312c3b45a4)

--- core/trino-main/src/main/java/io/trino/FeaturesConfig.java
+++ core/trino-main/src/main/java/io/trino/FeaturesConfig.java
@@ -82,6 +82,14 @@ import static io.trino.sql.analyzer.RegexLibrary.JONI;
 })
 public class FeaturesConfig
 {
+    public enum DataIntegrityVerification
+    {
+        NONE,
+        ABORT,
+        RETRY,
+        /**/;
+    }
+
     @VisibleForTesting
     public static final String SPILLER_SPILL_PATH = "spiller-spill-path";
 
@@ -133,14 +141,6 @@ public class FeaturesConfig
     private boolean legacyArithmeticDecimalOperators;
 
     private boolean parallelizeLookupOuterOperator = true;
-
-    public enum DataIntegrityVerification
-    {
-        NONE,
-        ABORT,
-        RETRY,
-        /**/;
-    }
 
     public boolean isOmitDateTimeTypePrecision()
     {
