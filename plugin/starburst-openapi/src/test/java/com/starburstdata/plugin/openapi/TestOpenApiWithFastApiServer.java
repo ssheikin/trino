2f1c4e86bf2e0df06404144c14d9074b1d658974
Mathias Kools <mathias.kools@starburstdata.com>
2025-12-04 15:07:22 -0500

    Split TestOpenApiQueries into 3 catalog tests
    When we test every detail of these openapi specs,
    this one testing file will be too cramped.
    This also enables us to simplify the query runner.
    
    (cherry picked from commit be82477991a3bb00d7e715d2339fde5dfcc78de5)

--- plugin/starburst-openapi/src/test/java/com/starburstdata/plugin/openapi/TestOpenApiWithFastApiServer.java
+++ plugin/starburst-openapi/src/test/java/com/starburstdata/plugin/openapi/TestOpenApiWithFastApiServer.java
@@ -28,85 +28,21 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-final class TestOpenApiQueries
+final class TestOpenApiWithFastApiServer
         extends AbstractTestQueryFramework
 {
     @Override
     protected QueryRunner createQueryRunner()
             throws Exception
     {
-        KeycloakServer keycloakServer = closeAfterClass(new KeycloakServer());
-        PetStoreServer petStoreServer = closeAfterClass(new PetStoreServer(keycloakServer));
         FastApiServer fastApiServer = closeAfterClass(new FastApiServer());
-
-        Map<String, String> petStoreProperties = ImmutableMap.<String, String>builder()
-                .put("openapi.spec-location", petStoreServer.getSpecUrl())
-                .put("openapi.base-uri", petStoreServer.getApiUrl())
-                .put("openapi.authentication.type", "oauth")
-                .put("openapi.authentication.scheme", "basic")
-                .put("openapi.authentication.username", "user")
-                .put("openapi.authentication.password", "user")
-                .put("openapi.authentication.api-key-name", "api_key")
-                .put("openapi.authentication.api-key-value", "special-key")
-                .put("openapi.authentication.client-id", "sample-client-id")
-                .put("openapi.authentication.client-secret", "secret")
-                .buildOrThrow();
 
         Map<String, String> fastApiProperties = ImmutableMap.<String, String>builder()
                 .put("openapi.spec-location", fastApiServer.getSpecUrl())
                 .put("openapi.base-uri", fastApiServer.getApiUrl())
                 .buildOrThrow();
 
-        return OpenApiQueryRunner.builder(Map.of(
-                "openmeteo", Map.of(
-                        "openapi.spec-location", "https://raw.githubusercontent.com/open-meteo/open-meteo/main/openapi.yml",
-                        "openapi.base-uri", "https://api.open-meteo.com"),
-                "petstore", petStoreProperties,
-                "fastapi", fastApiProperties))
-                .build();
-    }
-
-    @Test
-    void testShowPetStoreTables()
-    {
-        assertQuery("SHOW SCHEMAS FROM petstore",
-                "VALUES 'default', 'information_schema'");
-        assertQuery("SHOW TABLES FROM petstore.default",
-                "VALUES 'pet_find_by_status', 'pet_find_by_tags', 'store_inventory', 'store_order', 'pet', 'user', 'user_create_with_list', 'user_login', 'pet_upload_image'");
-    }
-
-    @Test
-    void testSelectFromPetTable()
-    {
-        assertQuery("SELECT name FROM petstore.default.pet_find_by_status WHERE status = 'available' AND id != 100",
-                "VALUES ('Cat 1'), ('Cat 2'), ('Dog 1'), ('Lion 1'), ('Lion 2'), ('Lion 3'), ('Rabbit 1')");
-        assertQuery("SELECT name FROM petstore.default.pet WHERE pet_id = 1",
-                "VALUES ('Cat 1')");
-    }
-
-    @Test
-    void testInsertPet()
-    {
-        assertQueryReturnsEmptyResult("SELECT name FROM petstore.default.pet WHERE pet_id = 100");
-        assertQuerySucceeds("INSERT INTO petstore.default.pet (id, name, photo_urls, status) VALUES (100, 'Cat X', ARRAY[], 'available')");
-        assertQuery("SELECT name FROM petstore.default.pet WHERE pet_id = 100",
-                "VALUES ('Cat X')");
-        assertUpdate("UPDATE petstore.default.pet SET name = 'Cat Y' WHERE pet_id = 100", 1);
-        assertQuery("SELECT name FROM petstore.default.pet WHERE pet_id = 100",
-                "VALUES ('Cat Y')");
-        assertUpdate("DELETE FROM petstore.default.pet WHERE pet_id = 100", 1);
-        assertQueryReturnsEmptyResult("SELECT name FROM petstore.default.pet WHERE pet_id = 100");
-    }
-
-    @Test
-    void testSelectFromForecastTable()
-    {
-        assertQuery("SELECT elevation, timezone, current_weather.temperature BETWEEN -50 AND 100 AS is_livable " +
-                        "FROM openmeteo.default.v1_forecast WHERE latitude_req = 53.1325 AND longitude_req = 23.1688",
-                "VALUES (135.0, 'GMT', null)");
-        assertQuery("SELECT elevation, timezone, current_weather.temperature BETWEEN -50 AND 100 AS is_livable " +
-                        "FROM openmeteo.default.v1_forecast WHERE latitude_req = 53.1325 AND longitude_req = 23.1688 AND current_weather_req = true",
-                "VALUES (135.0, 'GMT', true)");
+        return OpenApiQueryRunner.builder(Map.of("fastapi", fastApiProperties)).build();
     }
 
     @Test
---
+++ plugin/starburst-openapi/src/test/java/com/starburstdata/plugin/openapi/TestOpenApiWithPetstore.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.starburstdata.plugin.openapi;
+
+import com.google.common.collect.ImmutableMap;
+import io.trino.testing.AbstractTestQueryFramework;
+import io.trino.testing.QueryRunner;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+final class TestOpenApiWithPetstore
+        extends AbstractTestQueryFramework
+{
+    @Override
+    protected QueryRunner createQueryRunner()
+            throws Exception
+    {
+        KeycloakServer keycloakServer = closeAfterClass(new KeycloakServer());
+        PetStoreServer petStoreServer = closeAfterClass(new PetStoreServer(keycloakServer));
+
+        Map<String, String> petStoreProperties = ImmutableMap.<String, String>builder()
+                .put("openapi.spec-location", petStoreServer.getSpecUrl())
+                .put("openapi.base-uri", petStoreServer.getApiUrl())
+                .put("openapi.authentication.type", "oauth")
+                .put("openapi.authentication.scheme", "basic")
+                .put("openapi.authentication.username", "user")
+                .put("openapi.authentication.password", "user")
+                .put("openapi.authentication.api-key-name", "api_key")
+                .put("openapi.authentication.api-key-value", "special-key")
+                .put("openapi.authentication.client-id", "sample-client-id")
+                .put("openapi.authentication.client-secret", "secret")
+                .buildOrThrow();
+
+        return OpenApiQueryRunner.builder(Map.of("petstore", petStoreProperties)).build();
+    }
+
+    @Test
+    void testShowPetStoreTables()
+    {
+        assertQuery("SHOW SCHEMAS FROM petstore",
+                "VALUES 'default', 'information_schema'");
+        assertQuery("SHOW TABLES FROM petstore.default",
+                "VALUES 'pet_find_by_status', 'pet_find_by_tags', 'store_inventory', 'store_order', 'pet', 'user', 'user_create_with_list', 'user_login', 'pet_upload_image'");
+    }
+
+    @Test
+    void testSelectFromPetTable()
+    {
+        assertQuery("SELECT name FROM petstore.default.pet_find_by_status WHERE status = 'available' AND id != 100",
+                "VALUES ('Cat 1'), ('Cat 2'), ('Dog 1'), ('Lion 1'), ('Lion 2'), ('Lion 3'), ('Rabbit 1')");
+        assertQuery("SELECT name FROM petstore.default.pet WHERE pet_id = 1",
+                "VALUES ('Cat 1')");
+    }
+
+    @Test
+    void testInsertPet()
+    {
+        assertQueryReturnsEmptyResult("SELECT name FROM petstore.default.pet WHERE pet_id = 100");
+        assertQuerySucceeds("INSERT INTO petstore.default.pet (id, name, photo_urls, status) VALUES (100, 'Cat X', ARRAY[], 'available')");
+        assertQuery("SELECT name FROM petstore.default.pet WHERE pet_id = 100",
+                "VALUES ('Cat X')");
+        assertUpdate("UPDATE petstore.default.pet SET name = 'Cat Y' WHERE pet_id = 100", 1);
+        assertQuery("SELECT name FROM petstore.default.pet WHERE pet_id = 100",
+                "VALUES ('Cat Y')");
+        assertUpdate("DELETE FROM petstore.default.pet WHERE pet_id = 100", 1);
+        assertQueryReturnsEmptyResult("SELECT name FROM petstore.default.pet WHERE pet_id = 100");
+    }
+}
