362ddecc55fc1fbc3994ff648a435f5caa01eb17
Yuya Ebihara <ebyhry@gmail.com>
2025-12-21 21:49:59 +0900

    Add delete_after_commit_enabled max_previous_versions Iceberg properties
    (cherry picked from commit f081dee265304da8f3c3c8f5a7908777102d17b8)
    (cherry picked from commit 94e0fc4c8179ed97d30c83e21f4561ef211a705a)

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/rest/TestIcebergPolarisCatalogConnectorSmokeTest.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/rest/TestIcebergPolarisCatalogConnectorSmokeTest.java
@@ -84,7 +84,6 @@ final class TestIcebergPolarisCatalogConnectorSmokeTest
                 .addIcebergProperty("iceberg.file-format", format.name())
                 .addIcebergProperty("iceberg.register-table-procedure.enabled", "true")
                 .addIcebergProperty("iceberg.writer-sort-buffer-size", "1MB")
-                .addIcebergProperty("iceberg.allowed-extra-properties", "write.metadata.delete-after-commit.enabled,write.metadata.previous-versions-max")
                 .addIcebergProperty("iceberg.catalog.type", "rest")
                 .addIcebergProperty("iceberg.rest-catalog.nested-namespace-enabled", "true")
                 .addIcebergProperty("iceberg.rest-catalog.uri", catalogUri)
