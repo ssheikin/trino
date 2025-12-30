9e60e976f0f980556616b95e733b6fed16d5b112
Piotr Findeisen <piotr.findeisen@gmail.com>
2025-12-30 20:54:43 +0100

    Fix test thread pool naming pattern
    It's good practice to be able to distinguish threads by name.
    
    (cherry picked from commit 5d24b539fab5192dfbbe2591c1217731f747fcfd)
    (cherry picked from commit bd928de4370c5552f7710d92d2a75c7ab58ea047)

--- plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/BaseJdbcConnectorTest.java
+++ plugin/trino-base-jdbc/src/test/java/io/trino/plugin/jdbc/BaseJdbcConnectorTest.java
@@ -134,7 +134,7 @@ public abstract class BaseJdbcConnectorTest
 {
     private static final Logger log = Logger.get(BaseJdbcConnectorTest.class);
 
-    private final ExecutorService executor = newCachedThreadPool(daemonThreadsNamed(getClass().getName()));
+    private final ExecutorService executor = newCachedThreadPool(daemonThreadsNamed(getClass().getName() + "-%s"));
 
     protected static final String CREATE_CATALOG_SQL_TEMPLATE = """
                 CREATE CATALOG %s USING %s
