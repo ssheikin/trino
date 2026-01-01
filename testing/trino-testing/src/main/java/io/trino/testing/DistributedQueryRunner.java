f5a383e9c1d248b983900c54e316405de507c54c
Łukasz Osipiuk <lukasz@osipiuk.net>
2026-01-01 16:51:39 +0100

    Expose backup trino client
    (cherry picked from commit 1edd767f375f2467fcc149ce6b6a1330647c6464)

--- testing/trino-testing/src/main/java/io/trino/testing/DistributedQueryRunner.java
+++ testing/trino-testing/src/main/java/io/trino/testing/DistributedQueryRunner.java
@@ -128,6 +128,7 @@ public final class DistributedQueryRunner
     private final Closer closer = Closer.create();
 
     private TestingTrinoClient trinoClient;
+    private Optional<TestingTrinoClient> backupTrinoClient = Optional.empty();
 
     private final ReadWriteLock lock = new ReentrantReadWriteLock();
     private final AtomicInteger concurrentQueries = new AtomicInteger();
@@ -258,6 +259,9 @@ public final class DistributedQueryRunner
                 defaultSession.getExchangeEncryptionKey());
 
         this.trinoClient = closer.register(testingTrinoClientFactory.create(coordinator, defaultSession));
+        if (backupCoordinator.isPresent()) {
+            this.backupTrinoClient = Optional.of(closer.register(testingTrinoClientFactory.create(backupCoordinator.get(), defaultSession)));
+        }
 
         log.info("Created DistributedQueryRunner in %s (unclosed instances = %s)", nanosSince(start), unclosedInstances.incrementAndGet());
     }
@@ -462,6 +466,12 @@ public final class DistributedQueryRunner
     public TestingTrinoClient getClient()
     {
         return trinoClient;
+    }
+
+    public TestingTrinoClient getBackupClient()
+    {
+        checkState(backupTrinoClient.isPresent(), "Backup client not present");
+        return backupTrinoClient.get();
     }
 
     @Override
