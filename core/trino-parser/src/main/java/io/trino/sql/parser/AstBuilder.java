ab10c4f48ea105c429027f05edf87ecc54ef97cf
Max-Cheng <aaron9shire@gmail.com>
2026-01-25 16:48:30 +0800

    Support `DESCRIBE OUTPUT (subquery)` syntax
    (cherry picked from commit 6b2c279485f5983ff151132bec00732fd45aaf1c)
    (cherry picked from commit a761fdd90de43edb1869b0bbf7f3be4e3faecbbf)

--- core/trino-parser/src/main/java/io/trino/sql/parser/AstBuilder.java
+++ core/trino-parser/src/main/java/io/trino/sql/parser/AstBuilder.java
@@ -1206,6 +1206,14 @@ class AstBuilder
     }
 
     @Override
+    public Node visitDescribeQueryOutput(SqlBaseParser.DescribeQueryOutputContext context)
+    {
+        return new DescribeOutput(
+                getLocation(context),
+                (Query) visit(context.rootQuery()));
+    }
+
+    @Override
     public Node visitDescribeInput(SqlBaseParser.DescribeInputContext context)
     {
         return new DescribeInput(
