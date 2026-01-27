270e778e91b3b9f2ae089a32a5ba8ba5fea8ce54
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-01-27 13:13:56 +0100

    Drop unused SqlTask methods return values
    (cherry picked from commit 2e9ccd9ce6ba7f548dae8bbf5a973f85de3ff619)
    (cherry picked from commit 91300358f012f7f087a073b0c9963856750c9c71)

--- core/trino-main/src/main/java/io/trino/execution/SqlTask.java
+++ core/trino-main/src/main/java/io/trino/execution/SqlTask.java
@@ -300,10 +300,10 @@ public class SqlTask
         return Optional.ofNullable(catalogs.get());
     }
 
-    public boolean setCatalogs(Set<CatalogHandle> catalogs)
+    public void setCatalogs(Set<CatalogHandle> catalogs)
     {
         requireNonNull(catalogs, "catalogs is null");
-        return this.catalogs.compareAndSet(null, requireNonNull(catalogs, "catalogs is null"));
+        this.catalogs.compareAndSet(null, requireNonNull(catalogs, "catalogs is null"));
     }
 
     public boolean catalogsLoaded()
@@ -311,9 +311,9 @@ public class SqlTask
         return catalogsLoaded.get();
     }
 
-    public boolean setCatalogsLoaded()
+    public void setCatalogsLoaded()
     {
-        return catalogsLoaded.compareAndSet(false, true);
+        catalogsLoaded.set(true);
     }
 
     public VersionedDynamicFilterDomains acknowledgeAndGetNewDynamicFilterDomains(long callersDynamicFiltersVersion)
