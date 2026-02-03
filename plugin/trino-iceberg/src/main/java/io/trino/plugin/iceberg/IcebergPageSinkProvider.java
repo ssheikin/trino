b902cd55cb6168c4e6edd28e0a1b12dea7bfd646
Raunaq Morarka <raunaqmorarka@gmail.com>
2026-02-03 10:26:53 +0530

    Rename io.trino.plugin.iceberg.procedure.IcebergOptimizeHandle#sortOrder
    (cherry picked from commit fe246e7cba736c3790431d5c68dc78654a80b1d0)
    (cherry picked from commit d9d7ec56d145ea2fbc06de5cc9b43fc094c1539d)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergPageSinkProvider.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergPageSinkProvider.java
@@ -182,7 +182,7 @@ public class IcebergPageSinkProvider
                         optimizeHandle.fileFormat(),
                         optimizeHandle.tableStorageProperties(),
                         maxPartitionsPerWriter(session),
-                        optimizeHandle.sortOrder(),
+                        optimizeHandle.sortFields(),
                         sortingFileWriterBufferSize,
                         sortingFileWriterMaxOpenFiles,
                         sortingFileWriterLocalStagingPath,
