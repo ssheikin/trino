3dfb9b9cfe5b1e97ad54bee5ef9be12bd979c885
Raunaq Morarka <raunaqmorarka@gmail.com>
2026-02-10 23:52:35 +0530

    Remove redundant run of RemoveUnsupportedDynamicFilters in optimizer
    (cherry picked from commit c61357aa510a6fdbbb2b23ea4b5efc33a1f28327)

--- core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
@@ -979,7 +979,6 @@ public class PlanOptimizers
         // Remove unsupported dynamic filters introduced by PredicatePushdown. Also, cleanup dynamic filters removed by
         // PushPredicateIntoTableScan and RemoveRedundantPredicateAboveTableScan due to those rules replacing table scans with empty ValuesNode
         builder.add(new RemoveUnsupportedDynamicFilters(plannerContext));
-        builder.add(new RemoveUnsupportedDynamicFilters(plannerContext)); // Remove unsupported dynamic filters introduced by PredicatePushdown
         builder.add(new StatsRecordingPlanOptimizer(optimizerStats, new DeterminePreferredDynamicFilterTimeout(statsCalculator))); // Run after PredicatePushDown
         builder.add(inlineProjections);
         builder.add(new UnaliasSymbolReferences()); // Run unalias after merging projections to simplify projections more efficiently
