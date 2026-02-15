707f327cb3b67d25d55af14d2cffe610760eccdf
Piotr Rzysko <piotr.rzysko@gmail.com>
2026-02-15 10:05:37 +0100

    Add test for external writes for Glue-based Iceberg catalog
    This test is intended to verify that the connector properly invalidates
    caches shared across queries when external modifications occur. For now,
    such shared caches are non-existent, so the test documents the current
    behavior where stale reads are expected. Once shared caching is
    implemented, this test will ensure proper cache invalidation.
    
    (cherry picked from commit 4a41c94bc7b17ea3282e99fa982ff430f2a42c01)

---
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/catalog/glue/TestIcebergGlueCatalogExternalWriteTest.java
@@ -0,0 +1,86 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.iceberg.catalog.glue;
+
+import com.google.common.collect.ImmutableMap;
+import io.trino.plugin.iceberg.BaseIcebergExternalWriteTest;
+import org.junit.jupiter.api.AfterAll;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static io.trino.testing.SystemEnvironmentUtils.requireEnv;
+import static io.trino.testing.TestingNames.randomNameSuffix;
+import static org.apache.iceberg.FileFormat.PARQUET;
+
+/*
+ * The test currently uses AWS Default Credential Provider Chain,
+ * See https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/credentials-chain.html#credentials-default
+ * on ways to set your AWS credentials which will be needed to run this test.
+ */
+final class TestIcebergGlueCatalogExternalWriteTest
+        extends BaseIcebergExternalWriteTest
+{
+    private final String bucketName = requireEnv("S3_BUCKET");
+    private final String testPrefix = "test_iceberg_glue_external_write_" + randomNameSuffix();
+
+    @AfterAll
+    public void cleanupResources()
+    {
+        // Clean up S3 objects created during tests
+        try (S3Client s3 = S3Client.create()) {
+            ListObjectsV2Request listRequest = ListObjectsV2Request.builder()
+                    .bucket(bucketName)
+                    .prefix(testPrefix)
+                    .build();
+            s3.listObjectsV2Paginator(listRequest).stream()
+                    .forEach(response -> {
+                        List<String> keys = response.contents().stream()
+                                .map(S3Object::key)
+                                .collect(toImmutableList());
+                        if (!keys.isEmpty()) {
+                            DeleteObjectsRequest deleteRequest = DeleteObjectsRequest.builder()
+                                    .bucket(bucketName)
+                                    .delete(builder -> builder.objects(keys.stream()
+                                            .map(key -> ObjectIdentifier.builder().key(key).build())
+                                            .toList()).quiet(true))
+                                    .build();
+                            s3.deleteObjects(deleteRequest);
+                        }
+                    });
+        }
+    }
+
+    @Override
+    protected Map<String, String> getIcebergProperties()
+    {
+        return ImmutableMap.<String, String>builder()
+                .put("iceberg.catalog.type", "glue")
+                .put("hive.metastore.glue.default-warehouse-dir", "s3://%s/%s".formatted(bucketName, testPrefix))
+                .put("iceberg.file-format", PARQUET.name())
+                .buildOrThrow();
+    }
+
+    @Override
+    protected String createSchemaSql(String schemaName)
+    {
+        return "CREATE SCHEMA %s WITH (location = 's3://%s/%s/%s')".formatted(schemaName, bucketName, testPrefix, schemaName);
+    }
+}
