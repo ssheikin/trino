f59abb842ca3feb02943d6f5a0800cdfaed651f2
Raunaq Morarka <raunaqmorarka@gmail.com>
2026-02-19 11:03:25 +0530

    Remove unnecessary usages of parquet_writer_block_size in tests
    (cherry picked from commit b6e443a33e908c9d91dcb17941d2db86e697bd5f)
    (cherry picked from commit 6202aaf55f198d1a9dd626f6e074861e7e2fec8f)

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergSortedWriting.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergSortedWriting.java
@@ -95,17 +95,11 @@ public class TestIcebergSortedWriting
     private void testSortedWritingWithLocalStaging(FileFormat format)
     {
         // Using a larger table forces buffered data to be written to disk
-        Session withSmallRowGroups = Session.builder(getSession())
-                .setCatalogSessionProperty("iceberg", "orc_writer_max_stripe_rows", "200")
-                .setCatalogSessionProperty("iceberg", "parquet_writer_block_size", "20kB")
-                .setCatalogSessionProperty("iceberg", "parquet_writer_batch_size", "200")
-                .build();
         try (TestTable table = new TestTable(
                 getQueryRunner()::execute,
                 "test_sorted_lineitem_table",
                 "WITH (sorted_by = ARRAY['comment'], format = '" + format.name() + "') AS TABLE tpch.tiny.lineitem WITH NO DATA")) {
             assertUpdate(
-                    withSmallRowGroups,
                     "INSERT INTO " + table.getName() + " TABLE tpch.tiny.lineitem",
                     "VALUES 60175");
             for (Object filePath : computeActual("SELECT file_path from \"" + table.getName() + "$files\"").getOnlyColumnAsSet()) {
