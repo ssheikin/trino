pettyjamesm/prestosql

Add vectorized null bit packing
Adds support for using the java vector API for encoding and decoding the
valueIsNull boolean arrays to and from their corresponding bitmask
representations during serialization and deserialization. This is
enabled on all x86_64 platforms with at least 256-bit SIMD register
support and all ARM platforms with SVE or SVE2.

core/trino-main/src/main/java/io/trino/metadata/BlockEncodingManager.java
@@ -49,17 +49,17 @@ public final class BlockEncodingManager
     {
         // add the built-in BlockEncodings
         SimdSupport simdSupport = blockEncodingSimdSupport.getSimdSupport();
-        addBlockEncoding(new VariableWidthBlockEncoding());
-        addBlockEncoding(new ByteArrayBlockEncoding(simdSupport.compressByte(), simdSupport.expandByte()));
-        addBlockEncoding(new ShortArrayBlockEncoding(simdSupport.compressShort(), simdSupport.expandShort()));
-        addBlockEncoding(new IntArrayBlockEncoding(simdSupport.compressInt(), simdSupport.expandInt()));
-        addBlockEncoding(new LongArrayBlockEncoding(simdSupport.compressLong(), simdSupport.expandLong()));
-        addBlockEncoding(new Fixed12BlockEncoding());
-        addBlockEncoding(new Int128ArrayBlockEncoding());
+        addBlockEncoding(new VariableWidthBlockEncoding(simdSupport.packIsNullBits()));
+        addBlockEncoding(new ByteArrayBlockEncoding(simdSupport.packIsNullBits(), simdSupport.compressByte(), simdSupport.expandByte()));
+        addBlockEncoding(new ShortArrayBlockEncoding(simdSupport.packIsNullBits(), simdSupport.compressShort(), simdSupport.expandShort()));
+        addBlockEncoding(new IntArrayBlockEncoding(simdSupport.packIsNullBits(), simdSupport.compressInt(), simdSupport.expandInt()));
+        addBlockEncoding(new LongArrayBlockEncoding(simdSupport.packIsNullBits(), simdSupport.compressLong(), simdSupport.expandLong()));
+        addBlockEncoding(new Fixed12BlockEncoding(simdSupport.packIsNullBits()));
+        addBlockEncoding(new Int128ArrayBlockEncoding(simdSupport.packIsNullBits()));
         addBlockEncoding(new DictionaryBlockEncoding());
-        addBlockEncoding(new ArrayBlockEncoding());
-        addBlockEncoding(new MapBlockEncoding());
-        addBlockEncoding(new RowBlockEncoding());
+        addBlockEncoding(new ArrayBlockEncoding(simdSupport.packIsNullBits()));
+        addBlockEncoding(new MapBlockEncoding(simdSupport.packIsNullBits()));
+        addBlockEncoding(new RowBlockEncoding(simdSupport.packIsNullBits()));
         addBlockEncoding(new RunLengthBlockEncoding());
     }
 

ssheikin/trino

(cherry picked from commit 959a4c1fbd6a3a559a70a84e5adc6618f142aa97)
(cherry picked from commit b115ebd3a64b53b6f0a48d070901ed563c06d25e)

core/trino-main/src/main/java/io/trino/metadata/BlockEncodingManager.java
@@ -55,13 +55,13 @@ public final class BlockEncodingManager
     {
         // add the built-in BlockEncodings
         SimdSupport simdSupport = blockEncodingSimdSupport.getSimdSupport();
-        addBlockEncoding(new ByteArrayBlockEncoding(simdSupport.compressByte(), simdSupport.expandByte()));
-        addBlockEncoding(new ShortArrayBlockEncoding(simdSupport.compressShort(), simdSupport.expandShort()));
-        addBlockEncoding(new Fixed12BlockEncoding());
-        addBlockEncoding(new Int128ArrayBlockEncoding());
-        addBlockEncoding(new ArrayBlockEncoding());
-        addBlockEncoding(new MapBlockEncoding());
-        addBlockEncoding(new RowBlockEncoding());
+        addBlockEncoding(new ByteArrayBlockEncoding(simdSupport.vectorizeNullBitPacking(), simdSupport.compressByte(), simdSupport.expandByte()));
+        addBlockEncoding(new ShortArrayBlockEncoding(simdSupport.vectorizeNullBitPacking(), simdSupport.compressShort(), simdSupport.expandShort()));
+        addBlockEncoding(new Fixed12BlockEncoding(simdSupport.vectorizeNullBitPacking()));
+        addBlockEncoding(new Int128ArrayBlockEncoding(simdSupport.vectorizeNullBitPacking()));
+        addBlockEncoding(new ArrayBlockEncoding(simdSupport.vectorizeNullBitPacking()));
+        addBlockEncoding(new MapBlockEncoding(simdSupport.vectorizeNullBitPacking()));
+        addBlockEncoding(new RowBlockEncoding(simdSupport.vectorizeNullBitPacking()));
         addBlockEncoding(new RunLengthBlockEncoding());
 
         if (config.isExchangeAdaptiveBlockEncodingEnabled()) {
@@ -71,10 +71,10 @@ public final class BlockEncodingManager
             addBlockEncoding(new VariableWidthAdaptiveBlockEncoding());
         }
         else {
-            addBlockEncoding(new IntArrayBlockEncoding(simdSupport.compressInt(), simdSupport.expandInt()));
-            addBlockEncoding(new LongArrayBlockEncoding(simdSupport.compressLong(), simdSupport.expandLong()));
+            addBlockEncoding(new IntArrayBlockEncoding(simdSupport.vectorizeNullBitPacking(), simdSupport.compressInt(), simdSupport.expandInt()));
+            addBlockEncoding(new LongArrayBlockEncoding(simdSupport.vectorizeNullBitPacking(), simdSupport.compressLong(), simdSupport.expandLong()));
             addBlockEncoding(new DictionaryBlockEncoding());
-            addBlockEncoding(new VariableWidthBlockEncoding());
+            addBlockEncoding(new VariableWidthBlockEncoding(simdSupport.vectorizeNullBitPacking()));
         }
     }
 