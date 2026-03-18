2e124c5550fe4dd77df28fd03cf9b9a496cbf467
Mathias Kools <mathias.kools@starburstdata.com>
2026-03-18 13:19:27 -0400

    Use connector factory to test openAPI schemas
    This becomes important for later work where OpenApiSpec
    depends on guice injected parameters.
    That's a lot harder to do with how the code was.
    
    (cherry picked from commit 0d78780eaf1f8a124d80ea272cdd2928fde0ca18)

--- plugin/starburst-openapi/src/test/java/com/starburstdata/plugin/openapi/TestOpenApiConnectorFactory.java
+++ plugin/starburst-openapi/src/test/java/com/starburstdata/plugin/openapi/TestOpenApiConnectorFactory.java
@@ -13,86 +13,65 @@
  */
 package com.starburstdata.plugin.openapi;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.CreationException;
+import com.google.inject.spi.Message;
 import com.starburstdata.plugin.openapi.OpenApiValidationExceptions.AmbiguousTableFunctionPath;
 import com.starburstdata.plugin.openapi.OpenApiValidationExceptions.BadPathItem;
 import com.starburstdata.plugin.openapi.OpenApiValidationExceptions.BadResponseReference;
 import com.starburstdata.plugin.openapi.OpenApiValidationExceptions.FailedValidation;
-import io.trino.spi.StandardErrorCode;
+import io.trino.spi.TrinoException;
+import io.trino.spi.connector.Connector;
 import io.trino.spi.function.table.ConnectorTableFunction;
 import io.trino.spi.function.table.ReturnTypeSpecification.DescribedTable;
+import io.trino.testing.TestingConnectorContext;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.net.URL;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.starburstdata.plugin.openapi.OpenApiSpec.SCHEMA_NAME;
-import static io.trino.testing.assertions.TrinoExceptionAssert.assertTrinoExceptionThrownBy;
+import static io.trino.spi.StandardErrorCode.CONFIGURATION_INVALID;
 import static java.util.Objects.requireNonNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.Fail.fail;
 import static org.assertj.core.api.InstanceOfAssertFactories.list;
+import static org.assertj.core.api.InstanceOfAssertFactories.throwable;
 import static org.assertj.core.api.InstanceOfAssertFactories.type;
 
-final class TestOpenApiSpec
+final class TestOpenApiConnectorFactory
 {
-    @Test
-    public void testLoadsGithub()
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "github.json",
+            "github-patched.json",
+            "jira.json",
+            "galaxy.json",
+            "petstore.yaml",
+            "datadog.yaml",
+            "cloudflare.json",
+            "openmeteo.yml"
+    })
+    public void testLoadsSpecification(String specification)
     {
-        assertThatNoException().isThrownBy(() -> loadSpec("github.json"));
-    }
-
-    @Test
-    public void testLoadsGithubPatched()
-    {
-        assertThatNoException().isThrownBy(() -> loadSpec("github-patched.json"));
-    }
-
-    @Test
-    public void testLoadsJira()
-    {
-        assertThatNoException().isThrownBy(() -> loadSpec("jira.json"));
-    }
-
-    @Test
-    public void testLoadsGalaxy()
-    {
-        assertThatNoException().isThrownBy(() -> loadSpec("galaxy.json"));
-    }
-
-    @Test
-    public void testLoadsPetstore()
-    {
-        assertThatNoException().isThrownBy(() -> loadSpec("petstore.yaml"));
-    }
-
-    @Test
-    public void testLoadsDatadog()
-    {
-        assertThatNoException().isThrownBy(() -> loadSpec("datadog.yaml"));
-    }
-
-    @Test
-    public void testLoadsCloudflare()
-    {
-        assertThatNoException().isThrownBy(() -> loadSpec("cloudflare.json"));
-    }
-
-    @Test
-    public void testLoadsOpenMeteo()
-    {
-        assertThatNoException().isThrownBy(() -> loadSpec("openmeteo.yml"));
+        assertThatNoException().isThrownBy(() -> createConnector(specification).shutdown());
     }
 
     @Test
     void testTableFunctions()
     {
-        OpenApiSpec spec = loadSpec("petstore.yaml");
-        assertThat(spec.getTableFunctions())
+        Connector connector = createConnector("petstore.yaml");
+        assertThat(connector.getTableFunctions())
                 .extracting(ConnectorTableFunction::getSchema)
                 .containsOnly(SCHEMA_NAME);
-        assertThat(spec.getTableFunctions())
+        assertThat(connector.getTableFunctions())
                 .extracting(ConnectorTableFunction::getName)
                 .containsExactlyInAnyOrder(
                         "pet_find_by_status",
@@ -102,16 +81,16 @@ final class TestOpenApiSpec
                         "store_order_order_id",
                         "user_login",
                         "user_username");
-        assertThat(spec.getTableFunctions())
+        assertThat(connector.getTableFunctions())
                 .extracting(ConnectorTableFunction::getReturnTypeSpecification)
                 .allMatch(rt -> rt instanceof DescribedTable);
+        connector.shutdown();
     }
 
     @Test
     void testAmbiguousTableFunctions()
     {
-        assertTrinoExceptionThrownBy(() -> loadSpec("ambiguouspaths.json"))
-                .hasErrorCode(StandardErrorCode.CONFIGURATION_INVALID)
+        assertThat(getConfigurationThrowable("ambiguouspaths.json"))
                 .cause()
                 .asInstanceOf(type(OpenApiValidationExceptions.class))
                 .extracting(OpenApiValidationExceptions::getFailedValidations)
@@ -137,8 +116,7 @@ final class TestOpenApiSpec
     public void testResponses()
     {
         Map<String, String> badResponseReferences =
-                assertTrinoExceptionThrownBy(() -> loadSpec("responses.json"))
-                .hasErrorCode(StandardErrorCode.CONFIGURATION_INVALID)
+                assertThat(getConfigurationThrowable("responses.json"))
                 .cause()
                 .asInstanceOf(type(OpenApiValidationExceptions.class))
                 .extracting(OpenApiValidationExceptions::getFailedValidations)
@@ -159,19 +137,17 @@ final class TestOpenApiSpec
     @Test
     public void testPaths()
     {
-        Map<String, String> badPaths =
-                assertTrinoExceptionThrownBy(() -> loadSpec("paths.json"))
-                        .hasErrorCode(StandardErrorCode.CONFIGURATION_INVALID)
-                        .cause()
-                        .asInstanceOf(type(OpenApiValidationExceptions.class))
-                        .extracting(OpenApiValidationExceptions::getFailedValidations)
-                        .asInstanceOf(list(FailedValidation.class))
-                        .asInstanceOf(list(BadPathItem.class))
-                        .actual()
-                        .stream()
-                        .collect(toImmutableMap(
-                                BadPathItem::path,
-                                BadPathItem::error));
+        Map<String, String> badPaths = assertThat(getConfigurationThrowable("paths.json"))
+                .cause()
+                .asInstanceOf(type(OpenApiValidationExceptions.class))
+                .extracting(OpenApiValidationExceptions::getFailedValidations)
+                .asInstanceOf(list(FailedValidation.class))
+                .asInstanceOf(list(BadPathItem.class))
+                .actual()
+                .stream()
+                .collect(toImmutableMap(
+                        BadPathItem::path,
+                        BadPathItem::error));
         assertThat(badPaths).containsOnlyKeys("/circular", "/circularSTART", "/circularEND", "/badref");
         assertThat(badPaths.get("/circular"))
                 .isEqualTo("Path references form a cycle");
@@ -181,11 +157,28 @@ final class TestOpenApiSpec
                 .isEqualTo("Path references path that doesn't exist: notreal");
     }
 
-    private OpenApiSpec loadSpec(String name)
+    private Connector createConnector(String location)
     {
-        OpenApiConfig config = new OpenApiConfig();
-        URL specResource = requireNonNull(getClass().getClassLoader().getResource(name));
-        config.setSpecLocation(specResource.getFile());
-        return new OpenApiSpec(config);
+        URL specResource = requireNonNull(getClass().getClassLoader().getResource(location));
+        Map<String, String> config = ImmutableMap.<String, String>builder()
+                .put("bootstrap.quiet", "true")
+                .put("openapi.spec-location", specResource.getFile())
+                .put("openapi.base-uri", "https://starburst.io")
+                .buildOrThrow();
+        return new OpenApiConnectorFactory().create("openapi", config, new TestingConnectorContext());
+    }
+
+    private Throwable getConfigurationThrowable(String location)
+    {
+        Collection<Message> creationMessages = assertThatThrownBy(() -> createConnector(location))
+                .asInstanceOf(throwable(CreationException.class))
+                .extracting(CreationException::getErrorMessages)
+                .actual();
+        Optional<Throwable> exception = creationMessages.stream()
+                .flatMap(e -> Optional.ofNullable(e.getCause()).stream())
+                .filter(e -> e instanceof TrinoException trinoException &&
+                        trinoException.getErrorCode().equals(CONFIGURATION_INVALID.toErrorCode()))
+                .findFirst();
+        return assertThat(exception).isPresent().get().actual();
     }
 }
