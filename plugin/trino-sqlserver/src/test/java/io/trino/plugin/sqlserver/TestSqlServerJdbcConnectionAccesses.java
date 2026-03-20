becd0f03ad163f85ebec2c4b6599f8b26fe75041
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-03-20 13:18:21 +0100

    Run SQL Server type mapping tests against latest too
    Refactor SQL Server type mapping tests so they run against both: oldest
    supported and latest tested SQL Server versions.  Both are important. The
    oldest supported is key for testing writes. The latest supported is key
    for testing for potential metadata reporting changes and for negative
    tests (asserting remote system does not [yet] support a given thing).
    
    (cherry picked from commit 87c82efafa99f6bacb6bb9324654228a184e3163)
    (cherry picked from commit 2481285c59dfbda21a366a7267f0aa18bcb12e4b)

--- plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestSqlServerJdbcConnectionAccesses.java
+++ plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestSqlServerJdbcConnectionAccesses.java
@@ -97,7 +97,7 @@ public class TestSqlServerJdbcConnectionAccesses
         assertJdbcConnections("SELECT * FROM nation JOIN region USING(regionkey)", 10, Optional.empty());
         assertJdbcConnections("SELECT * FROM information_schema.schemata", 1, Optional.empty());
         assertJdbcConnections("SELECT * FROM information_schema.tables", 1, Optional.empty());
-        assertJdbcConnections("SELECT * FROM information_schema.columns", 1179, Optional.empty());
+        assertJdbcConnections("SELECT * FROM information_schema.columns", 1041, Optional.empty());
         assertJdbcConnections("SELECT * FROM nation", 3, Optional.empty());
         assertJdbcConnections("SELECT * FROM TABLE (system.query(query => 'SELECT * FROM dbo.nation'))", 2, Optional.empty());
         assertJdbcConnections("CREATE TABLE copy_of_nation AS SELECT * FROM nation", 15, Optional.empty());
@@ -109,7 +109,7 @@ public class TestSqlServerJdbcConnectionAccesses
         assertJdbcConnections("SHOW SCHEMAS", 1, Optional.empty());
         assertJdbcConnections("SHOW TABLES", 2, Optional.empty());
         assertJdbcConnections("SHOW STATS FOR nation", 4, Optional.empty());
-        assertJdbcConnections("SELECT * FROM system.jdbc.columns WHERE table_cat = 'counting_sqlserver'", 1179, Optional.empty());
+        assertJdbcConnections("SELECT * FROM system.jdbc.columns WHERE table_cat = 'counting_sqlserver'", 1041, Optional.empty());
     }
 
     private static final class TestingSqlServerModule
--- plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestingSqlServer.java
+++ plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/TestingSqlServer.java
@@ -65,7 +65,8 @@ public final class TestingSqlServer
             .build();
 
     private static final DockerImageName IMAGE_NAME = DockerImageName.parse("mcr.microsoft.com/mssql/server");
-    public static final String LATEST_VERSION = "2022-latest";
+    public static final String DEFAULT_VERSION = "2019-CU28-ubuntu-20.04";
+    public static final String LATEST_VERSION = "2025-CU3-ubuntu-24.04";
 
     private final MSSQLServerContainer container;
     private final String databaseName;
@@ -73,7 +74,7 @@ public final class TestingSqlServer
 
     public TestingSqlServer()
     {
-        this(LATEST_VERSION, DEFAULT_DATABASE_SETUP);
+        this(DEFAULT_VERSION);
     }
 
     public TestingSqlServer(String version)
