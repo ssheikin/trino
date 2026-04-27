692a9a5cdaad9308268cbd18d4288820ec552913
David Phillips <david@acz.org>
2026-04-27 00:35:35 -0700

    Fix UseEnhancedSwitch in core
    (cherry picked from commit 5b28d33eee248837d50a4cef2723c186b0448f2b)
    (cherry picked from commit 7ff4149d224d1cc9fa8b654b2a972c6317fdeaa4)

--- core/trino-main/src/main/java/io/trino/execution/scheduler/PipelinedStageExecution.java
+++ core/trino-main/src/main/java/io/trino/execution/scheduler/PipelinedStageExecution.java
@@ -545,8 +545,7 @@ public class PipelinedStageExecution
         TaskState taskState = taskStatus.state();
 
         switch (taskState) {
-            case FAILING:
-            case FAILED:
+            case FAILING, FAILED -> {
                 RuntimeException failure = taskStatus.failures().stream()
                         .findFirst()
                         .map(this::rewriteTransportFailure)
@@ -554,18 +553,13 @@ public class PipelinedStageExecution
                         // task is failed or failing, so we need to create a synthetic exception to fail the stage now
                         .orElseGet(() -> new TrinoException(GENERIC_INTERNAL_ERROR, format("Task %s failed for an unknown reason", taskStatus.taskId())));
                 fail(failure);
-                break;
-            case CANCELING:
-            case CANCELED:
-            case ABORTING:
-            case ABORTED:
+            }
+            case CANCELING, CANCELED, ABORTING, ABORTED -> {
                 // A task should only be in the aborting, aborted, canceling, or canceled state if the STAGE is done (ABORTED or FAILED)
                 fail(new TrinoException(GENERIC_INTERNAL_ERROR, format("Task %s is in the %s state but stage %s is %s", taskStatus.taskId(), taskState, stateMachine.getStageId(), stateMachine.getState())));
-                break;
-            case FLUSHING:
-                newFlushingOrFinishedTaskObserved = addFlushingTask(taskStatus.taskId());
-                break;
-            case FINISHED:
+            }
+            case FLUSHING -> newFlushingOrFinishedTaskObserved = addFlushingTask(taskStatus.taskId());
+            case FINISHED -> {
                 if (hasSpoolingExchangeOutput()) {
                     ExchangeSinkHandle exchangeSinkHandle = exchangeSinkHandles.get(taskStatus.taskId());
                     getOutputSpoolingExchange().sinkFinished(exchangeSinkHandle, taskStatus.taskId().attemptId());
@@ -575,9 +569,8 @@ public class PipelinedStageExecution
                 else {
                     newFlushingOrFinishedTaskObserved = addFinishedTask(taskStatus.taskId());
                 }
-
-                break;
-            default:
+            }
+                default -> {}
         }
 
         // Only allow stage state to transition to RUNNING, FLUSHING or FINISHED state
