e1333df7b7d81841dfe14cc6f223c3f0c83a6b8a
David Phillips <david@acz.org>
2026-05-03 20:27:20 -0700

    Fix text block alignment in plugin tests
    (cherry picked from commit 0073d826686f83580320eecda25dbee3cdda514b)
    (cherry picked from commit 0a98384e06c272bec04dbcf99b86fae20d4470df)

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergV2.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergV2.java
@@ -1838,11 +1838,11 @@ public class TestIcebergV2
                 .commitTransaction();
 
         String expectedStats =
-        """
-        VALUES
-        ('x', 0e0, 0e0, 1e0, NULL, NULL, NULL),
-        (NULL, NULL, NULL, NULL, 0e0, NULL, NULL)
-        """;
+                """
+                VALUES
+                ('x', 0e0, 0e0, 1e0, NULL, NULL, NULL),
+                (NULL, NULL, NULL, NULL, 0e0, NULL, NULL)
+                """;
 
         assertThat(query("SHOW STATS FOR " + table))
                 .skippingTypesCheck()
