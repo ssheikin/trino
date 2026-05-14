15fd4f276cfbafcff63eb4fef1317038da56a7dd
Martin Traverso <mtraverso@gmail.com>
2026-05-14 01:16:40 +0000

    Merge PushTopN with ExtractSpatialJoins
    PushTopN (TopN pushdown rules) and ExtractSpatialJoins (spatial-join extraction with surrounding cleanup) ran in sequence after the ReorderJoins/PredicatePushDown stage. PushTopN's rules match TopN-rooted patterns; ExtractSpatialJoins matches Filter-over-Join and Join. The two rule sets target disjoint node-type patterns, and the per-rule iteration order produces the same result as sequential application.
    
    Combine into a single IterativeOptimizer named Phase6.
    
    (cherry picked from commit a6cbeaa7822a70060bdf23b805376c26bc0a33cd)
    (cherry picked from commit bb84c13a41c269f6b8bac2a79790a6df100b9712)

--- testing/trino-tests/src/test/java/io/trino/execution/TestEventListenerBasic.java
+++ testing/trino-tests/src/test/java/io/trino/execution/TestEventListenerBasic.java
@@ -1544,14 +1544,14 @@ public class TestEventListenerBasic
                         ImmutableList.of(),
                         ImmutableList.of(new PlanNodeStatsAndCostSummary(10., 90., 0., 0., 0.)),
                         ImmutableList.of(new JsonRenderedNode(
-                                "100",
+                                "82",
                                 "Limit",
                                 ImmutableMap.of("count", "10", "withTies", "", "inputPreSortedBy", "[]"),
                                 ImmutableList.of(new Symbol(DOUBLE, "symbol_1")),
                                 ImmutableList.of(),
                                 ImmutableList.of(new PlanNodeStatsAndCostSummary(10., 90., 90., 0., 0.)),
                                 ImmutableList.of(new JsonRenderedNode(
-                                        "173",
+                                        "143",
                                         "LocalExchange",
                                         ImmutableMap.of(
                                                 "partitioning", "[connectorHandleType = SystemPartitioningHandle, partitioning = SINGLE, function = SINGLE]",
@@ -1561,7 +1561,7 @@ public class TestEventListenerBasic
                                         ImmutableList.of(),
                                         ImmutableList.of(new PlanNodeStatsAndCostSummary(10., 90., 0., 0., 0.)),
                                         ImmutableList.of(new JsonRenderedNode(
-                                                "140",
+                                                "114",
                                                 "RemoteSource",
                                                 ImmutableMap.of("sourceFragmentIds", "[1]"),
                                                 ImmutableList.of(new Symbol(DOUBLE, "symbol_1")),
@@ -1569,7 +1569,7 @@ public class TestEventListenerBasic
                                                 ImmutableList.of(),
                                                 ImmutableList.of()))))))),
                 "1", new JsonRenderedNode(
-                        "139",
+                        "113",
                         "LimitPartial",
                         ImmutableMap.of(
                                 "count", "10",
