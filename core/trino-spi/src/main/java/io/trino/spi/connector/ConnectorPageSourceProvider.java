255525d39125bf8aa9d1c0e49c26bf3982e3b545
Marius Grama <findinpath@gmail.com>
2026-05-20 09:28:38 +0200

    Remove deprecated method from ConnectorPageSourceProvider
    (cherry picked from commit 4a861c8e3c379c7b81308206f56bd4fc60550fe7)
    (cherry picked from commit 3558d1691cac675aa3b4444e8b2d3089ccbeaebe)

--- core/trino-spi/src/main/java/io/trino/spi/connector/ConnectorPageSourceProvider.java
+++ core/trino-spi/src/main/java/io/trino/spi/connector/ConnectorPageSourceProvider.java
@@ -13,15 +13,12 @@
  */
 package io.trino.spi.connector;
 
-import io.trino.spi.TrinoException;
 import io.trino.spi.gpu.ConnectorGpuMemoryContext;
 import io.trino.spi.gpu.ConnectorGpuPageSource;
 import io.trino.spi.predicate.TupleDomain;
 
 import java.util.List;
 import java.util.Optional;
-
-import static io.trino.spi.StandardErrorCode.NOT_SUPPORTED;
 
 public interface ConnectorPageSourceProvider
 {
@@ -51,34 +48,14 @@ public interface ConnectorPageSourceProvider
      * @param columns columns that should show up in the output page, in this order
      * @param dynamicFilter optionally remove rows that don't satisfy this predicate
      */
-    default ConnectorPageSource createPageSource(
+    ConnectorPageSource createPageSource(
             ConnectorTransactionHandle transaction,
             ConnectorSession session,
             ConnectorSplit split,
             ConnectorTableHandle table,
             Optional<ConnectorTableCredentials> tableCredentials,
             List<ColumnHandle> columns,
-            DynamicFilter dynamicFilter)
-    {
-        return createPageSource(transaction, session, split, table, columns, dynamicFilter);
-    }
-
-    /**
-     * @param columns columns that should show up in the output page, in this order
-     * @param dynamicFilter optionally remove rows that don't satisfy this predicate
-     * @deprecated Implement {@link #createPageSource(ConnectorTransactionHandle, ConnectorSession, ConnectorSplit, ConnectorTableHandle, Optional, List, DynamicFilter)} instead.
-     */
-    @Deprecated(forRemoval = true)
-    default ConnectorPageSource createPageSource(
-            ConnectorTransactionHandle transaction,
-            ConnectorSession session,
-            ConnectorSplit split,
-            ConnectorTableHandle table,
-            List<ColumnHandle> columns,
-            DynamicFilter dynamicFilter)
-    {
-        throw new TrinoException(NOT_SUPPORTED, "This connector does not support reading tables");
-    }
+            DynamicFilter dynamicFilter);
 
     /**
      * Returns unenforced (effective) predicate that {@link ConnectorPageSource} would use to filter split data.
