2ae6cee1c67e16328818b6a32ea016ed4a6d73df
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-05-27 13:57:52 +0200

    Disable TestStarburstOdbc
    Temporarily disable the test, as it always fails apparently due to test
    infrastructure issues (a docker container startup error).
    
    (cherry picked from commit 147f7882326befc2d080f77e42c952d24e2ba965)

--- testing/starburst-tests-odbc/src/test/java/io/starburst/tests/odbc/TestStarburstOdbc.java
+++ testing/starburst-tests-odbc/src/test/java/io/starburst/tests/odbc/TestStarburstOdbc.java
@@ -15,6 +15,7 @@ import io.starburst.tests.odbc.protocol.StarburstProtocolModule;
 import io.trino.testing.DistributedQueryRunner;
 import io.trino.testing.containers.junit.ReportLeakedContainers;
 import io.trino.tests.tpch.TpchQueryRunnerBuilder;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.GenericContainer;
@@ -25,6 +26,7 @@ import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Disabled // TODO https://starburstdata.atlassian.net/browse/ENG-16449 this test is currently broken
 public class TestStarburstOdbc
 {
     private final Logger log = Logger.get(TestStarburstOdbc.class);
