01450780908106d9e85822a8a2b02be799497d35
Yuya Ebihara <ebyhry@gmail.com>
2026-05-29 18:58:04 +0900

    Simplify column name builder in KDB
    (cherry picked from commit ae1ebb11620c0659e414ec58a9f0686e58622b3b)

--- plugin/starburst-kdb/src/main/java/com/starburstdata/plugin/kdb/KdbQueryBuilder.java
+++ plugin/starburst-kdb/src/main/java/com/starburstdata/plugin/kdb/KdbQueryBuilder.java
@@ -17,6 +17,7 @@ import io.trino.spi.connector.SchemaTableName;
 
 import java.util.List;
 import java.util.OptionalLong;
+import java.util.stream.Collectors;
 
 import static com.starburstdata.plugin.kdb.KdbClient.getFullTableName;
 import static com.starburstdata.plugin.kdb.KdbClient.validateIdentifier;
@@ -42,15 +43,10 @@ public final class KdbQueryBuilder
 
         // Use functional select form to safely handle reserved column names
         // ?[table; (); 0b; cols!cols; limit] projects named columns with optional limit
-        StringBuilder cols = new StringBuilder();
-        for (int i = 0; i < columns.size(); i++) {
-            if (i > 0) {
-                cols.append("`");
-            }
-            String columnName = columns.get(i).columnName();
-            validateIdentifier(columnName, "column");
-            cols.append(columnName);
-        }
+        String cols = columns.stream()
+                .peek(column -> validateIdentifier(column.columnName(), "column"))
+                .map(KdbColumnHandle::columnName)
+                .collect(Collectors.joining("`"));
         String colList = columns.size() == 1 ? "enlist[`" + cols + "]" : "`" + cols;
         String limitParam = limit.isPresent() ? "; %d".formatted(limit.getAsLong()) : "";
         return "?[%s; (); 0b; %s!%s%s]".formatted(fullTableName, colList, colList, limitParam);
