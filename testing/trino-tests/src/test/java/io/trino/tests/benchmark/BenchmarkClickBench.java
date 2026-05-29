fda4da2edec9f1c47cfde834183046d7875408c6
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-05-29 12:54:34 +0200

    Revert "Fix remote ClickBench data location"
    This reverts commit ddeb3739dcd464d100fe416cb8fc8e1b1113ee48.
    
    (cherry picked from commit 43d6f979e1b23746a25cf0b610a66ba0905520c2)

--- testing/trino-tests/src/test/java/io/trino/tests/benchmark/BenchmarkClickBench.java
+++ testing/trino-tests/src/test/java/io/trino/tests/benchmark/BenchmarkClickBench.java
@@ -305,7 +305,7 @@ public final class BenchmarkClickBench
 
         queryRunner.execute("CREATE SCHEMA IF NOT EXISTS hive.clickbench");
         String hitsLocation = isRemote(dataLocation)
-                ? dataLocation
+                ? dataLocation + "/hits"
                 : Path.of(dataLocation, "hits").toUri().toString();
         log.info("Creating hive.clickbench.hits at %s", hitsLocation);
         queryRunner.execute(format(
