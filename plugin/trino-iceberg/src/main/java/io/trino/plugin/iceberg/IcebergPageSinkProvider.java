b8c3d88090b1ab4fa835bbd3c1a7778e17491e18
Marius Grama <findinpath@gmail.com>
2026-05-29 19:48:50 +0200

    Require table credentials in IcebergPageSinkProvider
    (cherry picked from commit 0e967c468f98f9399e68f3eb2ef3f82cbe5d5858)
    (cherry picked from commit 8fcd3d75a03a0ef15bb65ed7a9cd671d06c0b511)

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergPageSinkProvider.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/IcebergPageSinkProvider.java
@@ -119,6 +119,7 @@ public class IcebergPageSinkProvider
     @Override
     public ConnectorPageSink createPageSink(ConnectorTransactionHandle transactionHandle, ConnectorSession session, ConnectorOutputTableHandle outputTableHandle, Optional<ConnectorTableCredentials> tableCredentials, ConnectorPageSinkId pageSinkId)
     {
+        verify(tableCredentials.isPresent(), "tableCredentials must be present");
         IcebergWritableTableHandle tableHandle = (IcebergWritableTableHandle) outputTableHandle;
         return createPageSink(session, tableHandle, getFileIoProperties(tableCredentials));
     }
@@ -126,6 +127,7 @@ public class IcebergPageSinkProvider
     @Override
     public ConnectorPageSink createPageSink(ConnectorTransactionHandle transactionHandle, ConnectorSession session, ConnectorInsertTableHandle insertTableHandle, Optional<ConnectorTableCredentials> tableCredentials, ConnectorPageSinkId pageSinkId)
     {
+        verify(tableCredentials.isPresent(), "tableCredentials must be present");
         IcebergWritableTableHandle tableHandle = (IcebergWritableTableHandle) insertTableHandle;
         return createPageSink(session, tableHandle, getFileIoProperties(tableCredentials));
     }
