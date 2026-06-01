32450cdb50619e7ee6623f4b858172e407c94f22
praveenkrishna.d <praveenkrishna@tutanota.com>
2026-06-01 18:43:51 +0530

    Push partial aggregation with projection through exchange
    (cherry picked from commit dbda9489624758345f7e00a0e27e3b7930a91430)
    (cherry picked from commit 16dcb5c779475027aa2fd7ec498b68db08223f68)

--- core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
+++ core/trino-main/src/main/java/io/trino/sql/planner/PlanOptimizers.java
@@ -1082,8 +1082,8 @@ public class PlanOptimizers
                 costCalculator,
                 ImmutableSet.<Rule<?>>builder()
                         .addAll(new PushPartialAggregationThroughJoin().rules())
-                        .add(new PushPartialAggregationThroughExchange(plannerContext),
-                                new PruneJoinColumns(),
+                        .addAll(new PushPartialAggregationThroughExchange(plannerContext).rules())
+                        .add(new PruneJoinColumns(),
                                 new PruneJoinChildrenColumns(),
                                 new RemoveRedundantIdentityProjections())
                         .build()));
