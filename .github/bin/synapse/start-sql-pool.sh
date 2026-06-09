3d621773d5b23865b828f68c8d4f89929bd88332
Lukas Peraza <lukas.peraza@starburstdata.com>
2026-06-09 16:52:34 -0400

    Add more context tags to Synapse SQL pools
    (cherry picked from commit aa7ba6d64062620977568433ec14e8720deb4705)

--- .github/bin/synapse/start-sql-pool.sh
+++ .github/bin/synapse/start-sql-pool.sh
@@ -8,6 +8,8 @@
 #     SYNAPSE_WORKSPACE (required)
 #     SYNAPSE_SQL_POOL (optional) if not specified, one will be generated
 #     SYNAPSE_POOL_TTL (optional) minimum hours to keep pool up, default 2
+#     SYNAPSE_POOL_TAGS (optional) JSON object containing additional tags
+#           to apply to the pool
 #
 
 set -euo pipefail
@@ -18,9 +20,20 @@ az account show --output none
 ttl_seconds=$(( ${SYNAPSE_POOL_TTL:-2} * 3600 ))
 now=$(date -u +"%s")
 sql_pool_expiration=$(( now + ttl_seconds ))
-diff=$(( sql_pool_expiration - now ))
 
 pool_name="${SYNAPSE_SQL_POOL:-POOL_$(openssl rand -hex 4)}"
+
+additional_tags=()
+if [[ -n ${SYNAPSE_POOL_TAGS:-} ]]; then
+    if command -v jq &> /dev/null; then
+        while read -r tag; do
+            additional_tags+=( "$tag" )
+        done < <(echo "$SYNAPSE_POOL_TAGS" |
+                    jq -r 'to_entries | .[] | "\(.key)=\(.value)"')
+    else
+        echo "jq is not installed; ignoring additional tags" >&2
+    fi
+fi
 
 # Add expiration tag so the pool can be cleaned up later if the
 # workflow fails before the cleanup job runs
@@ -29,7 +42,6 @@ az synapse sql pool create \
     --performance-level "DW300c" \
     --resource-group "$SYNAPSE_RESOURCE_GROUP" \
     --workspace-name "$SYNAPSE_WORKSPACE" \
-    --tags "expiration=$sql_pool_expiration" "environment=test" "team=connectors-team" \
-    --output none
+    --tags "expiration=$sql_pool_expiration" "environment=test" "team=connectors-team" "${additional_tags[@]}"
 
 echo "$pool_name"
