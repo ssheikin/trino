fa875ba9472d1c91e6bd27a3507aa632ef9231c3
Marius Grama <findinpath@gmail.com>
2026-06-10 00:24:33 +0200

    Inline temporary credentials conversion logic
    (cherry picked from commit 377bff7ecb5b10077a80f8317819f486119da224)

--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/metastore/BaseVendedFileSystemCredentials.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/metastore/BaseVendedFileSystemCredentials.java
@@ -13,16 +13,8 @@
  */
 package io.trino.plugin.deltalake.metastore;
 
-import io.trino.spi.TrinoException;
-import io.unitycatalog.client.model.AwsCredentials;
-import io.unitycatalog.client.model.AzureUserDelegationSAS;
-import io.unitycatalog.client.model.GcpOauthToken;
-import io.unitycatalog.client.model.TemporaryCredentials;
-
 import java.time.Instant;
-import java.util.Optional;
 
-import static io.trino.spi.StandardErrorCode.NOT_SUPPORTED;
 import static java.util.Objects.requireNonNull;
 
 public abstract class BaseVendedFileSystemCredentials
@@ -48,33 +40,5 @@ public abstract class BaseVendedFileSystemCredentials
     public Instant expireAt()
     {
         return expireAt;
-    }
-
-    public static FileSystemCredentials fromTemporaryCredentials(TemporaryCredentials credentials)
-    {
-        AzureUserDelegationSAS azureUserDelegationSas = credentials.getAzureUserDelegationSas();
-        if (azureUserDelegationSas != null) {
-            // TODO: support azure vended credentials https://starburstdata.atlassian.net/browse/SEP-18169
-            throw new TrinoException(NOT_SUPPORTED, "Azure vended credentials are not supported yet");
-        }
-
-        AwsCredentials awsTempCredentials = credentials.getAwsTempCredentials();
-        Instant expireAt = Optional.ofNullable(credentials.getExpirationTime()).map(Instant::ofEpochMilli).orElse(Instant.MAX);
-        if (awsTempCredentials != null) {
-            return new AwsVendedCredentials(
-                    awsTempCredentials.getAccessKeyId(),
-                    awsTempCredentials.getSecretAccessKey(),
-                    awsTempCredentials.getSessionToken(),
-                    expireAt);
-        }
-
-        GcpOauthToken gcpOauthToken = credentials.getGcpOauthToken();
-        if (gcpOauthToken != null) {
-            return new GcsVendedCredentials(
-                    gcpOauthToken.getOauthToken(),
-                    expireAt);
-        }
-
-        throw new TrinoException(NOT_SUPPORTED, "No supported cloud credentials returned from Unity Catalog");
     }
 }
--- plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/metastore/unity/UnityVendedCredentialsProvider.java
+++ plugin/trino-delta-lake/src/main/java/io/trino/plugin/deltalake/metastore/unity/UnityVendedCredentialsProvider.java
@@ -14,19 +14,26 @@
 package io.trino.plugin.deltalake.metastore.unity;
 
 import com.google.inject.Inject;
-import io.trino.plugin.deltalake.metastore.BaseVendedFileSystemCredentials;
+import io.trino.plugin.deltalake.metastore.AwsVendedCredentials;
 import io.trino.plugin.deltalake.metastore.FileSystemCredentials;
+import io.trino.plugin.deltalake.metastore.GcsVendedCredentials;
 import io.trino.plugin.deltalake.metastore.VendedCredentialsHandle;
 import io.trino.plugin.deltalake.metastore.VendedCredentialsProvider;
 import io.trino.plugin.hive.metastore.unity.UnityHiveMetastoreFactory;
 import io.trino.plugin.hive.metastore.unity.UnityMetastore;
+import io.trino.spi.TrinoException;
+import io.unitycatalog.client.model.AwsCredentials;
+import io.unitycatalog.client.model.AzureUserDelegationSAS;
+import io.unitycatalog.client.model.GcpOauthToken;
 import io.unitycatalog.client.model.PathOperation;
 import io.unitycatalog.client.model.TableOperation;
 import io.unitycatalog.client.model.TemporaryCredentials;
 
+import java.time.Instant;
 import java.util.Optional;
 
 import static com.google.common.base.Verify.verify;
+import static io.trino.spi.StandardErrorCode.NOT_SUPPORTED;
 import static java.util.Objects.requireNonNull;
 
 public class UnityVendedCredentialsProvider
@@ -59,8 +66,36 @@ public class UnityVendedCredentialsProvider
             temporaryCredentials = unityMetastore.getTemporaryPathCredentials(handle.tableLocation(), PathOperation.PATH_READ_WRITE);
         }
 
-        FileSystemCredentials credentials = BaseVendedFileSystemCredentials.fromTemporaryCredentials(temporaryCredentials);
+        FileSystemCredentials credentials = fromTemporaryCredentials(temporaryCredentials);
         verify(credentials.isValid(), "vended credentials is not valid");
         return handle.withVendedCredentials(credentials);
+    }
+
+    private static FileSystemCredentials fromTemporaryCredentials(TemporaryCredentials credentials)
+    {
+        AzureUserDelegationSAS azureUserDelegationSas = credentials.getAzureUserDelegationSas();
+        if (azureUserDelegationSas != null) {
+            // TODO: support azure vended credentials https://starburstdata.atlassian.net/browse/SEP-18169
+            throw new TrinoException(NOT_SUPPORTED, "Azure vended credentials are not supported yet");
+        }
+
+        AwsCredentials awsTempCredentials = credentials.getAwsTempCredentials();
+        Instant expireAt = Optional.ofNullable(credentials.getExpirationTime()).map(Instant::ofEpochMilli).orElse(Instant.MAX);
+        if (awsTempCredentials != null) {
+            return new AwsVendedCredentials(
+                    awsTempCredentials.getAccessKeyId(),
+                    awsTempCredentials.getSecretAccessKey(),
+                    awsTempCredentials.getSessionToken(),
+                    expireAt);
+        }
+
+        GcpOauthToken gcpOauthToken = credentials.getGcpOauthToken();
+        if (gcpOauthToken != null) {
+            return new GcsVendedCredentials(
+                    gcpOauthToken.getOauthToken(),
+                    expireAt);
+        }
+
+        throw new TrinoException(NOT_SUPPORTED, "No supported cloud credentials returned from Unity Catalog");
     }
 }
