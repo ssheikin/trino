7cf3c444aa4a5e18fdd52ee6d3cbf08a66af86dd
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-06-12 18:21:30 +0200

    Annotate CHAR type operators fallibility
    (cherry picked from commit fffa5a9b6c59f93a4e2396dd26e2521c36045cbd)
    (cherry picked from commit 2a5499c8d24c6106b364326d0df15028e0b3a0a2)

--- core/trino-spi/src/main/java/io/trino/spi/type/CharType.java
+++ core/trino-spi/src/main/java/io/trino/spi/type/CharType.java
@@ -238,7 +238,7 @@ public final class CharType
         return true;
     }
 
-    @ScalarOperator(COMPARISON_UNORDERED_LAST)
+    @ScalarOperator(value = COMPARISON_UNORDERED_LAST, neverFails = true)
     private static long comparisonOperator(Slice left, Slice right)
     {
         return compareChars(left, right);
