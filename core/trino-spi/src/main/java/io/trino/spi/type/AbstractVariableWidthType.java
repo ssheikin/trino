56aafd329399da94c72edcd5a37cc5fb11dfe04a
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-06-12 18:21:30 +0200

    Annotate AbstractVariableWidthType operators fallibility
    (cherry picked from commit 79be8b4ff5cc7aa298783595fc2ad5edcbdc9bca)
    (cherry picked from commit f8713971982eaed5bc2a5e1d24fa3d706e5e7573)

--- core/trino-spi/src/main/java/io/trino/spi/type/AbstractVariableWidthType.java
+++ core/trino-spi/src/main/java/io/trino/spi/type/AbstractVariableWidthType.java
@@ -259,13 +259,13 @@ public abstract class AbstractVariableWidthType
 
     private static class DefaultComparableOperators
     {
-        @ScalarOperator(EQUAL)
+        @ScalarOperator(value = EQUAL, neverFails = true)
         private static boolean equalOperator(Slice left, Slice right)
         {
             return left.equals(right);
         }
 
-        @ScalarOperator(EQUAL)
+        @ScalarOperator(value = EQUAL, neverFails = true)
         private static boolean equalOperator(@BlockPosition VariableWidthBlock leftBlock, @BlockIndex int leftPosition, @BlockPosition VariableWidthBlock rightBlock, @BlockIndex int rightPosition)
         {
             Slice leftRawSlice = leftBlock.getRawSlice();
@@ -279,13 +279,13 @@ public abstract class AbstractVariableWidthType
             return leftRawSlice.equals(leftRawSliceOffset, leftLength, rightRawSlice, rightRawSliceOffset, rightLength);
         }
 
-        @ScalarOperator(EQUAL)
+        @ScalarOperator(value = EQUAL, neverFails = true)
         private static boolean equalOperator(Slice left, @BlockPosition VariableWidthBlock rightBlock, @BlockIndex int rightPosition)
         {
             return equalOperator(rightBlock, rightPosition, left);
         }
 
-        @ScalarOperator(EQUAL)
+        @ScalarOperator(value = EQUAL, neverFails = true)
         private static boolean equalOperator(@BlockPosition VariableWidthBlock leftBlock, @BlockIndex int leftPosition, Slice right)
         {
             Slice leftRawSlice = leftBlock.getRawSlice();
@@ -374,13 +374,13 @@ public abstract class AbstractVariableWidthType
             return rightRawSlice.equals(rightRawSliceOffset, rightLength, leftBytes, leftOffset, leftLength);
         }
 
-        @ScalarOperator(XX_HASH_64)
+        @ScalarOperator(value = XX_HASH_64, neverFails = true)
         private static long xxHash64Operator(Slice value)
         {
             return XxHash64.hash(value);
         }
 
-        @ScalarOperator(XX_HASH_64)
+        @ScalarOperator(value = XX_HASH_64, neverFails = true)
         private static long xxHash64Operator(@BlockPosition VariableWidthBlock block, @BlockIndex int position)
         {
             return XxHash64.hash(block.getRawSlice(), block.getRawSliceOffset(position), block.getSliceLength(position));
@@ -410,13 +410,13 @@ public abstract class AbstractVariableWidthType
 
     private static class DefaultOrderingOperators
     {
-        @ScalarOperator(COMPARISON_UNORDERED_LAST)
+        @ScalarOperator(value = COMPARISON_UNORDERED_LAST, neverFails = true)
         private static long comparisonOperator(Slice left, Slice right)
         {
             return left.compareTo(right);
         }
 
-        @ScalarOperator(COMPARISON_UNORDERED_LAST)
+        @ScalarOperator(value = COMPARISON_UNORDERED_LAST, neverFails = true)
         private static long comparisonOperator(@BlockPosition VariableWidthBlock leftBlock, @BlockIndex int leftPosition, @BlockPosition VariableWidthBlock rightBlock, @BlockIndex int rightPosition)
         {
             Slice leftRawSlice = leftBlock.getRawSlice();
@@ -430,7 +430,7 @@ public abstract class AbstractVariableWidthType
             return leftRawSlice.compareTo(leftRawSliceOffset, leftLength, rightRawSlice, rightRawSliceOffset, rightLength);
         }
 
-        @ScalarOperator(COMPARISON_UNORDERED_LAST)
+        @ScalarOperator(value = COMPARISON_UNORDERED_LAST, neverFails = true)
         private static long comparisonOperator(@BlockPosition VariableWidthBlock leftBlock, @BlockIndex int leftPosition, Slice right)
         {
             Slice leftRawSlice = leftBlock.getRawSlice();
@@ -440,7 +440,7 @@ public abstract class AbstractVariableWidthType
             return leftRawSlice.compareTo(leftRawSliceOffset, leftLength, right, 0, right.length());
         }
 
-        @ScalarOperator(COMPARISON_UNORDERED_LAST)
+        @ScalarOperator(value = COMPARISON_UNORDERED_LAST, neverFails = true)
         private static long comparisonOperator(Slice left, @BlockPosition VariableWidthBlock rightBlock, @BlockIndex int rightPosition)
         {
             Slice rightRawSlice = rightBlock.getRawSlice();
