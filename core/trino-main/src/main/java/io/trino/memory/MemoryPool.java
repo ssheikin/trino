05d48739b121b7f4edfc41823ae91d6f56724e31
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-06-18 21:18:33 +0200

    Ensure MemoryPool.getInfo does not leak internal state
    `getInfo` is synchronized, but it could expose `queryMemoryReservations`
    if `MemoryPoolInfo` constructor did not make a defensive copy. A class
    internal consistency should not depend on some other class.
    
    (cherry picked from commit 1e1bd669848862a8646ded8f049723446afd997c)
    (cherry picked from commit 05bb6474783c6e2ef85700dfbdd56b1f819ae80f)

--- core/trino-main/src/main/java/io/trino/memory/MemoryPool.java
+++ core/trino-main/src/main/java/io/trino/memory/MemoryPool.java
@@ -109,7 +109,7 @@ public class MemoryPool
                 maxBytes,
                 reservedBytes,
                 reservedRevocableBytes,
-                queryMemoryReservations,
+                ImmutableMap.copyOf(queryMemoryReservations),
                 memoryAllocations,
                 stringKeyedTaskMemoryReservations,
                 stringKeyedTaskRevocableMemoryReservations);
