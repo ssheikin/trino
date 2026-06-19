e4539af704bb16a673255883047c85fd0c9af24d
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-06-19 13:18:09 +0200

    Simplify context use in tests
    `OperatorContext` provides access to operator's predefined
    `LocalMemoryContext`, no need to reach through `MemoryTrackingContext`.
    
    (cherry picked from commit 15ac7964aadb3f023bdf20d43d1c65a01d9dc869)
    (cherry picked from commit bb4133e5316c25b3f80ac7cd8d2fd65baf904751)

--- core/trino-main/src/test/java/io/trino/operator/join/TestHashJoinOperator.java
+++ core/trino-main/src/test/java/io/trino/operator/join/TestHashJoinOperator.java
@@ -1241,7 +1241,7 @@ public class TestHashJoinOperator
             }
 
             // occupy the whole memory pool with another operator so finish() has to wait
-            anotherOperatorContext.getOperatorMemoryContext().localUserMemoryContext().setBytes(SMALL_MEMORY_POOL_BYTES);
+            anotherOperatorContext.localUserMemoryContext().setBytes(SMALL_MEMORY_POOL_BYTES);
             operator.finish();
             assertThat(operator.getState()).isEqualTo(HashBuilderOperator.State.CONSUMING_INPUT);
             assertThat(operator.isFinished()).isFalse();
@@ -1264,7 +1264,7 @@ public class TestHashJoinOperator
             }
 
             // free memory and let finish() proceed
-            anotherOperatorContext.getOperatorMemoryContext().localUserMemoryContext().setBytes(0);
+            anotherOperatorContext.localUserMemoryContext().setBytes(0);
             operator.finish();
             assertThat(operator.getState()).isEqualTo(HashBuilderOperator.State.LOOKUP_SOURCE_BUILT);
             assertThat(operator.isFinished()).isFalse();
@@ -1329,14 +1329,14 @@ public class TestHashJoinOperator
             assertThat(operator.getState()).isEqualTo(HashBuilderOperator.State.INPUT_UNSPILLING);
 
             // block memory so unspilling cannot reserve memory
-            anotherOperatorContext.getOperatorMemoryContext().localUserMemoryContext().setBytes(SMALL_MEMORY_POOL_BYTES);
+            anotherOperatorContext.localUserMemoryContext().setBytes(SMALL_MEMORY_POOL_BYTES);
             operator.finish();
             assertThat(operator.getState()).isEqualTo(HashBuilderOperator.State.INPUT_UNSPILLING);
             assertThat(operatorContext.isWaitingForMemory()).isNotDone();
             assertThat(lookupSourceFuture).isNotDone();
 
             // release memory and continue
-            anotherOperatorContext.getOperatorMemoryContext().localUserMemoryContext().setBytes(0);
+            anotherOperatorContext.localUserMemoryContext().setBytes(0);
             operator.finish();
             assertThat(operator.getState()).isEqualTo(HashBuilderOperator.State.INPUT_UNSPILLED_AND_BUILT);
 
--- core/trino-main/src/test/java/io/trino/operator/join/nonspilling/TestHashBuilderOperator.java
+++ core/trino-main/src/test/java/io/trino/operator/join/nonspilling/TestHashBuilderOperator.java
@@ -130,7 +130,7 @@ public class TestHashBuilderOperator
             assertThat(operator.isFinished()).isFalse();
             assertThat(operator.getState()).isEqualTo(CONSUMING_INPUT);
 
-            anotherOperatorContext.getOperatorMemoryContext().localUserMemoryContext().setBytes(memoryPoolSizeInBytes);
+            anotherOperatorContext.localUserMemoryContext().setBytes(memoryPoolSizeInBytes);
 
             operator.finish();
 
@@ -147,7 +147,7 @@ public class TestHashBuilderOperator
             assertThat(whenBuildFinishes).isNotDone();
             assertThat(operatorContext.isWaitingForMemory()).isNotDone();
 
-            anotherOperatorContext.getOperatorMemoryContext().localUserMemoryContext().setBytes(0);
+            anotherOperatorContext.localUserMemoryContext().setBytes(0);
 
             operator.finish();
 
