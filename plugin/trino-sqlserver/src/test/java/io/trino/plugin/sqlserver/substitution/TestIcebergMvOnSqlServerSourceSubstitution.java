89a12384372d3aa5967ef0e97cc8668f500bb965
lukasz-stec <lukasz.s.stec@gmail.com>
2026-07-24 10:33:46 +0200

    Test MV substitution with SQL Server source
    Add a connector-module test covering SQL Server source substitution.
    
    (cherry picked from commit b4b6d81a977563aa1324fee6178132eee51791d5)

---
+++ plugin/trino-sqlserver/src/test/java/io/trino/plugin/sqlserver/substitution/TestIcebergMvOnSqlServerSourceSubstitution.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.sqlserver.substitution;
+
+import com.google.common.collect.ImmutableMap;
+import io.trino.Session;
+import io.trino.plugin.geospatial.GeoPlugin;
+import io.trino.plugin.iceberg.substitution.AbstractIcebergMvSubstitutionTest;
+import io.trino.plugin.sqlserver.SqlServerPlugin;
+import io.trino.plugin.sqlserver.TestingSqlServer;
+import io.trino.spi.connector.CatalogSchemaName;
+import io.trino.testing.DistributedQueryRunner;
+import io.trino.testing.QueryRunner;
+import org.junit.jupiter.api.parallel.Execution;
+
+import static io.trino.plugin.base.util.Closables.closeAllSuppress;
+import static org.junit.jupiter.api.parallel.ExecutionMode.SAME_THREAD;
+
+@Execution(SAME_THREAD)
+public class TestIcebergMvOnSqlServerSourceSubstitution
+        extends AbstractIcebergMvSubstitutionTest
+{
+    @Override
+    protected QueryRunner createSourceQueryRunner(Session defaultSession, CatalogSchemaName sourceSchema)
+            throws Exception
+    {
+        TestingSqlServer sqlServer = closeAfterClass(new TestingSqlServer());
+        QueryRunner queryRunner = DistributedQueryRunner.builder(defaultSession)
+                .addExtraProperty("materialized-view-substitution.support.enabled", "true")
+                .build();
+        try {
+            queryRunner.installPlugin(new GeoPlugin());
+            queryRunner.installPlugin(new SqlServerPlugin());
+            queryRunner.createCatalog(sourceSchema.getCatalogName(), "sqlserver", ImmutableMap.of(
+                    "connection-url", sqlServer.getJdbcUrl(),
+                    "connection-user", sqlServer.getUsername(),
+                    "connection-password", sqlServer.getPassword()));
+        }
+        catch (Throwable e) {
+            closeAllSuppress(e, queryRunner);
+            throw e;
+        }
+        return queryRunner;
+    }
+}
