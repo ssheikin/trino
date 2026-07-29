4a8bb4edece3934c5ece19cd11b49520a543ebd1
Mathias Kools <mathias.kools@starburstdata.com>
2026-07-29 18:37:10 -0400

    Drop snowflake jdbc product tests
    (cherry picked from commit 197e1beceac12d88762cb04552fd9ada4f1471c3)

--- testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
+++ testing/trino-product-tests-groups/src/main/java/io/trino/tests/product/TestGroups.java
@@ -69,7 +69,6 @@ public final class TestGroups
     public static final String POSTGRESQL = "postgresql";
     public static final String POSTGRESQL_POSTGIS = "postgresql_postgis";
     public static final String SQLSERVER = "sqlserver";
-    public static final String STARBURST_SNOWFLAKE = "starburst_snowflake";
     public static final String SYNAPSE = "synapse";
     public static final String SAP_HANA = "sap-hana";
     public static final String LDAP = "ldap";
