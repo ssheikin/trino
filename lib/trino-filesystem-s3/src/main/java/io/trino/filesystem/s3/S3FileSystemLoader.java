3bcbddfcaac33078382fa68b3d193e38417cd9c3
Marius Grama <findinpath@gmail.com>
2026-08-02 16:32:57 +0200

    Add s3.expect-continue-enabled config property
    Some S3-compatible storage gateways reject PUT requests that use the
    HTTP expect-continue handshake, which the AWS SDK's ApacheHttpClient
    enables by default. Allow disabling it via configuration so writes
    work against those backends.
    
    (cherry picked from commit 3a86d17b9457d9e01d9d92f3d8343bf6a3792c8e)
    (cherry picked from commit 8a3e2a290262f96822cafc9f08f62c848c2e98e2)

--- lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemLoader.java
+++ lib/trino-filesystem-s3/src/main/java/io/trino/filesystem/s3/S3FileSystemLoader.java
@@ -254,7 +254,8 @@ public final class S3FileSystemLoader
     {
         ApacheHttpClient.Builder client = ApacheHttpClient.builder()
                 .maxConnections(config.getMaxConnections())
-                .tcpKeepAlive(config.getTcpKeepAlive());
+                .tcpKeepAlive(config.getTcpKeepAlive())
+                .expectContinueEnabled(config.isExpectContinueEnabled());
 
         config.getConnectionTtl().ifPresent(ttl -> client.connectionTimeToLive(ttl.toJavaTime()));
         config.getConnectionMaxIdleTime().ifPresent(time -> client.connectionMaxIdleTime(time.toJavaTime()));
