21a8ae5ca4b201a889cd6f873de949e8bd6fd2a5
Grzegorz Wal <grzegorz.wal@starburstdata.com>
2026-08-07 11:29:59 +0200

    Revert "Pin cache-subquery test placement and disable cache revocation"
    This reverts commit ca8499017bb88e80f597039be09c08424f63805b.
    
    (cherry picked from commit 28d0782aac0ec8afc0a233ad7a66534c51b55e3c)

--- plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergCacheSubqueriesTest.java
+++ plugin/trino-iceberg/src/test/java/io/trino/plugin/iceberg/TestIcebergCacheSubqueriesTest.java
@@ -47,7 +47,6 @@ public class TestIcebergCacheSubqueriesTest
         return IcebergQueryRunner.builder()
                 .setWorkerCount(1)
                 .setExtraProperties(EXTRA_PROPERTIES)
-                .addExtraProperty("node-scheduler.include-coordinator", "false")
                 .setInitialTables(REQUIRED_TABLES)
                 .build();
     }
--- testing/trino-testing/src/main/java/io/trino/testing/BaseCacheSubqueriesTest.java
+++ testing/trino-testing/src/main/java/io/trino/testing/BaseCacheSubqueriesTest.java
@@ -114,9 +114,7 @@ public abstract class BaseCacheSubqueriesTest
 {
     protected static final Set<TpchTable<?>> REQUIRED_TABLES = ImmutableSet.of(NATION, LINE_ITEM, ORDERS, CUSTOMER);
     protected static final Map<String, String> EXTRA_PROPERTIES = ImmutableMap.of(
-            "subquery-cache.enabled", "true",
-            "subquery-cache.revoking-threshold", "1.0",
-            "subquery-cache.revoking-target", "1.0",
+            "cache.enabled", "true",
             "dynamic-filtering.bloom-filter.max-distinct-values-per-driver", "1000",
             "dynamic-filtering.partitioned-bloom-filter.max-distinct-values-per-driver", "100",
             "dynamic-filtering.max-distinct-values-per-driver", "100",
